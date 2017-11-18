df5145d338123fedd74bbf63688d045b23730d62
Wenbo Yu <wyu@box.com>
2017-11-17 18:33:57 -0800

    code review

--- src/api/uploads/UploadReachability.js
+++ src/api/uploads/UploadReachability.js
@@ -4,10 +4,11 @@
  * @author Box
  */
 
+import 'regenerator-runtime/runtime';
+
 import Base from '../Base';
 import LocalStore from '../../util/LocalStore';
 import { DEFAULT_HOSTNAME_UPLOAD } from '../../constants';
-import '../../util/regenerator-runtime';
 
 const CACHED_RESULTS_LOCAL_STORE_KEY = 'uploads-reachability-cached-results';
 
@@ -19,74 +20,94 @@ class UploadsReachability extends Base {
      *
      * @param {Object} [options]
      * @param {string} [options.token] - Auth token
-     * @param {string} [options.uploadHost] - Upload host name
+     * @param {string} [options.apiHost] - Api host
      * @return {void}
      */
     constructor(options: Object) {
-        super(options);
+        super({
+            id: 'folder_0',
+            ...options
+        });
         this.localStore = new LocalStore();
     }
 
     /**
      * Gets a reachable host for upload
+     * 
+     * @return {Promise}
      */
     async getReachableUploadHost() {
-        const cachedResults = this.localStore.getItem(CACHED_RESULTS_LOCAL_STORE_KEY) || {};
-        // Check if reachability for host is cached in local storage
-        if (this.uploadHost in cachedResults) {
-            return cachedResults[this.uploadHost] ? this.uploadHost : DEFAULT_HOSTNAME_UPLOAD;
+        const uploadHost = await this.getUploadHost();
+        // The default upload host is always reachable
+        if (uploadHost === DEFAULT_HOSTNAME_UPLOAD) {
+            return uploadHost;
         }
-        const isReachable = await this.isUploadHostReachable();
-        cachedResults[this.uploadHost] = isReachable;
-        this.localStore.setItem(CACHED_RESULTS_LOCAL_STORE_KEY, cachedResults);
+
+        const cachedHostReachability = this.localStore.getItem(CACHED_RESULTS_LOCAL_STORE_KEY) || {};
+        // Check if reachability for host is cached in local storage
+        if (uploadHost in cachedHostReachability) {
+            return cachedHostReachability[uploadHost] ? uploadHost : DEFAULT_HOSTNAME_UPLOAD;
+        }
+
+        const isReachable = await this.isUploadHostReachable(uploadHost);
+
+        // Update the host reachability in local store
+        cachedHostReachability[uploadHost] = isReachable;
+        this.localStore.setItem(CACHED_RESULTS_LOCAL_STORE_KEY, cachedHostReachability);
+
         // If the custom host is not reachable, return default upload host
-        return isReachable ? this.uploadHost : DEFAULT_HOSTNAME_UPLOAD;
+        return isReachable ? uploadHost : DEFAULT_HOSTNAME_UPLOAD;
     }
 
     /**
      * Checks if a host is reachable
+     * 
+     * @private
+     * @param {string} uploadHost
+     * @return {Promise}
      */
-    async isUploadHostReachable() {
-        const customUploadHost = await this.getUploadHost();
-        // the default upload host is always reachable
-        if (customUploadHost === DEFAULT_HOSTNAME_UPLOAD) {
+    async isUploadHostReachable(uploadHost: string) {
+        // The default upload host is always reachable
+        if (uploadHost === DEFAULT_HOSTNAME_UPLOAD) {
             return true;
         }
 
-        const xhr = new XMLHttpRequest();
-        xhr.open('GET', `${customUploadHost}/reachability-test-foo`, false);
         try {
-            xhr.send();
+            await fetch(`${uploadHost}/reachability-test-foo`);
         } catch (error) {
             return false;
         }
-        // as long as no exceptions were thrown when making the request, the host is reachable
+
+        // As long as no exceptions were thrown when making the request, the host is reachable
         return true;
     }
 
     /**
      * Get upload host name from the preflight request response
+     * 
+     * @private
+     * @return {Promise}
      */
     async getUploadHost() {
-        if (this.uploadHost === DEFAULT_HOSTNAME_UPLOAD) {
-            return DEFAULT_HOSTNAME_UPLOAD;
-        }
+        let preflightResponse;
 
-        const preflightResponse = await this.xhr.options({
+        await this.xhr.options({
             url: `${this.getBaseUrl()}/files/content`,
             data: {
                 name: 'test_name',
                 parent: { id: '0' },
                 size: '10'
             },
-            successHandler: () => {},
+            successHandler: (response) => {
+                preflightResponse = response;
+            },
             errorHandler: () => {}
         });
 
         if (preflightResponse) {
-            const { upload_url: uploadUrl } = preflightResponse;
+            const { upload_url } = preflightResponse;
 
-            const splitUrl = uploadUrl.split('/');
+            const splitUrl = upload_url.split('/');
             return `${splitUrl[0]}//${splitUrl[2]}`;
         }
 
--- src/util/regenerator-runtime.js
+++
@@ -1,6 +0,0 @@
-import regeneratorRuntime from 'regenerator-runtime'; // Puts regeneratorRuntime into global scope for async/await
-
-// Add regeneratorRuntime to global scope for async/await support
-if (!window.regeneratorRuntime) {
-    window.regeneratorRuntime = regeneratorRuntime;
-}
