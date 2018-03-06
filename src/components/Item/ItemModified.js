f5956b99e0c6204614f319fdf8ccee6e36bd495f
Sandip Ghosh <sandip.ghosh@resilient-networks.com>
2018-03-06 13:24:43 -0800

    New: Address Code Review Comment (#193)
    - Use FormattedMessage with arguments for localizing the Modified column text
    - Removed unnecessary spaces
    - Added flow comment for ItemModified.js and modifiedCellRenderer.js
    - removed unnecessary div from modifiedCellRenderer
    - made the Modified column flex 1 so it grows if the Content Explorer container is resized

--- src/components/Item/ItemModified.js
+++ src/components/Item/ItemModified.js
@@ -1,28 +1,40 @@
+/**
+ * @flow
+ * @file Component for the Modified column for the item
+ * @author sghosh326
+ */
+
 import React from 'react';
-import { injectIntl } from 'react-intl';
+import { injectIntl, FormattedMessage } from 'react-intl';
 import messages from '../messages';
 import Datefield from '../Date';
 import { VIEW_RECENTS } from '../../constants';
 import type { BoxItem, View } from '../../flowTypes';
+import './ModifiedCell.scss';
 
 type Props = {
     item: BoxItem,
     view: View,
-    intl: any
+    intl: any,
 };
 
 const ItemModified = ({ view, item, intl }: Props) => {
-    const { modified_at = '', interacted_at = '', modified_by }: BoxItem = item;
-    const isRecents: boolean = view === VIEW_RECENTS;
-    const date: string = isRecents ? interacted_at || modified_at : modified_at;
-    const modified_by_name: string = modified_by.name;
-    const byLabel = intl.formatMessage(messages.by);
+  const { modified_at = '', interacted_at = '', modified_by }: BoxItem = item;
+  const isRecents: boolean = view === VIEW_RECENTS;
+  const date: string = isRecents ? interacted_at || modified_at : modified_at;
+  const modifiedByName: string = modified_by.name;
 
-    return (
-   		<span className='be-item-label'>
-            <Datefield date={date} />&nbsp;{byLabel}&nbsp;{modified_by_name}
+  return (
+        <span className="be-item-modified">
+            <FormattedMessage
+                {...messages.modifiedColumnValue}
+                values={{
+                    date: <Datefield date={date} />,
+                    name: modifiedByName,
+                }}
+            />
         </span>
-    );
+  );
 };
 
 export default injectIntl(ItemModified);
--- src/components/Item/modifiedCellRenderer.js
+++ src/components/Item/modifiedCellRenderer.js
@@ -1,17 +1,13 @@
+/**
+ * @flow
+ * @file Function to render the Modified table cell
+ * @author sghosh326
+ */
+
 import React from 'react';
 import ItemModified from './ItemModified';
-import { VIEW_SEARCH } from '../../constants';
 import type { View, BoxItem } from '../../flowTypes';
-import './ModifiedCell.scss';
 
-export default (
-    rootId: string,
-    view: View,
-) => ({ rowData }: { rowData: BoxItem }) => (
-    <div className='be-item-modified'>
-        <ItemModified
-            item={rowData}
-            view={view}
-        />
-    </div>
+export default (rootId: string, view: View) => ({ rowData }: { rowData: BoxItem }) => (
+    <ItemModified item={rowData} view={view} />
 );
