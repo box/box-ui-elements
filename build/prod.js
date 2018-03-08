a4b6089e1cfb00a897042c66fd09bc04054c9f5b
Priyajeet Hora <priyajeet@box.com>
2018-03-07 17:11:47 -0800

    Chore: Package upgrades
    Upgrade to webpack 4
    Output source maps all the time
    Get rid of uglifyjs plugin
    Switches to babel-preset-env

--- build/prod.js
+++ build/prod.js
@@ -1,6 +1,6 @@
 /* eslint-disable */
 const workerFarm = require('worker-farm');
-const locales = require('./locales');
+const locales = require('box-locales');
 const numCPUs = require('os').cpus().length;
 const execSync = require('child_process').execSync;
 const path = require('path');
