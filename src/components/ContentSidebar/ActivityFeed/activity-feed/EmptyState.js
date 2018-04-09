9a6fc45d478653c0238deef009a20d777183e790
Justin Holdstock <jholdstock@box.com>
2018-04-09 16:55:33 -0600

    Chore: Formatted EmptyState

--- src/components/ContentSidebar/ActivityFeed/activity-feed/EmptyState.js
+++ src/components/ContentSidebar/ActivityFeed/activity-feed/EmptyState.js
@@ -23,14 +23,8 @@ const EmptyState = ({ isLoading, showCommentMessage }: Props): ReactNode => (
             <LoadingIndicator />
         ) : (
             <div className='bcs-empty-state-cta'>
-                <b>
-                    <FormattedMessage {...messages.noActivity} />
-                </b>
-                {showCommentMessage ? (
-                    <aside>
-                        <FormattedMessage {...messages.noActivityCommentPrompt} />
-                    </aside>
-                ) : null}
+                <FormattedMessage {...messages.noActivity} />
+                {showCommentMessage ? <FormattedMessage {...messages.noActivityCommentPrompt} /> : null}
             </div>
         )}
     </div>
