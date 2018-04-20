c7941ebc7eb0d79c0de94a748e050f2d19d32157
Daniel DeMicco <ddemicco@box.com>
2018-04-20 16:02:20 -0700

    Upgrade: preview to 1.0.40

--- src/constants.js
+++ src/constants.js
@@ -101,7 +101,7 @@ export const DELIMITER_SLASH: 'slash' = 'slash';
 export const DELIMITER_CARET: 'caret' = 'caret';
 
 /* ---------------------- Defaults -------------------------- */
-export const DEFAULT_PREVIEW_VERSION = '1.39.2';
+export const DEFAULT_PREVIEW_VERSION = '1.40.0';
 export const DEFAULT_PREVIEW_LOCALE = 'en-US';
 export const DEFAULT_PATH_STATIC = 'platform/elements';
 export const DEFAULT_PATH_STATIC_PREVIEW = 'platform/preview';
