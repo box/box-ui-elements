2739833a045b86ff10170881342c3323f77ea6cd
Priyajeet Hora <priyajeet@users.noreply.github.com>
2018-04-20 16:21:17 -0700

    Update: Modify token service to account for multiple tokens (#270)
    Can return a read and write token, preview should only be handed the read token.
    Token generator function should either return a simple string token, or should return an object that may have read and write token.
    When requesting multiple tokens per file, token generator should return a structure like
    
    {
      file_1: {
        read: 'token',
        write: 'token'
      },
      file_2: {
        read: 'token',
        write: 'token'
      },
      ...
    }

--- src/util/__tests__/TokenService-test.js
+++ src/util/__tests__/TokenService-test.js
@@ -1,15 +1,12 @@
 import Tokenservice from '../TokenService';
 
-const tokenMapGenerator = (id) => Promise.resolve({ [id]: 'token' });
+const readWriteTokenGenerator = () => Promise.resolve({ read: 'read_token', write: 'write_token' });
+const readTokenGenerator = () => Promise.resolve({ read: 'read_token' });
+const writeTokenGenerator = () => Promise.resolve({ write: 'write_token' });
 const tokenGenerator = () => Promise.resolve('token');
 const junkTokenGenerator = () => Promise.resolve(123);
 const nullTokenGenerator = () => Promise.resolve(null);
 const undefinedTokenGenerator = () => Promise.resolve();
-
-const tokensMapGenerator = (ids) => Promise.resolve({ [ids[0]]: 'token1', [ids[1]]: 'token2' });
-const tokensGenerator = (ids) => Promise.resolve({ [ids[0]]: 'token1', [ids[1]]: 'token2' });
-const nullTokensGenerator = (ids) => Promise.resolve({ [ids[0]]: null, [ids[1]]: null });
-const undefinedTokensGenerator = (ids) => Promise.resolve({ [ids[0]]: undefined, [ids[1]]: undefined });
 
 describe('util/Tokenservice', () => {
     describe('getToken()', () => {
@@ -31,7 +28,10 @@ describe('util/Tokenservice', () => {
             expect(Tokenservice.getToken('file_123', tokenGenerator)).resolves.toBe('token'));
 
         test('should return proper token with generator function that returns a token map', () =>
-            expect(Tokenservice.getToken('file_123', tokenMapGenerator)).resolves.toBe('token'));
+            expect(Tokenservice.getToken('file_123', readWriteTokenGenerator)).resolves.toEqual({
+                read: 'read_token',
+                write: 'write_token'
+            }));
 
         test('should reject when not given a typed id', () =>
             expect(Tokenservice.getToken('123')).rejects.toThrow(/Bad id or auth token/));
@@ -43,72 +43,99 @@ describe('util/Tokenservice', () => {
             expect(Tokenservice.getToken('file_123', junkTokenGenerator)).rejects.toThrow(/Bad id or auth token/));
     });
 
-    describe('getTokens()', () => {
+    describe('getWriteToken()', () => {
         test('should return null for a null token', () => {
-            expect(Tokenservice.getTokens(['file_123', 'folder_123'], null)).resolves.toEqual({
-                file_123: null,
-                folder_123: null
-            });
+            expect(Tokenservice.getWriteToken('file_123', null)).resolves.toBeNull();
         });
 
         test('should return undefined for a undefined token', () => {
-            expect(Tokenservice.getTokens(['file_123', 'folder_123'])).resolves.toEqual({
-                file_123: undefined,
-                folder_123: undefined
-            });
+            expect(Tokenservice.getWriteToken('file_123')).resolves.toBeUndefined();
+        });
+
+        test('should return a string token', () => {
+            expect(Tokenservice.getWriteToken('file_123', 'string_token')).resolves.toBe('string_token');
         });
 
         test('should return null for a null token generator', () =>
-            expect(Tokenservice.getTokens(['file_123', 'folder_123'], nullTokenGenerator)).resolves.toEqual({
-                file_123: null,
-                folder_123: null
-            }));
+            expect(Tokenservice.getWriteToken('file_123', nullTokenGenerator)).resolves.toBeNull());
 
         test('should return undefined for a undefined token generator', () =>
-            expect(Tokenservice.getTokens(['file_123', 'folder_123'], undefinedTokenGenerator)).resolves.toEqual({
-                file_123: undefined,
-                folder_123: undefined
-            }));
-
-        test('should return null for a null tokens generator', () =>
-            expect(Tokenservice.getTokens(['file_123', 'folder_123'], nullTokensGenerator)).resolves.toEqual({
-                file_123: null,
-                folder_123: null
-            }));
-
-        test('should return undefined for a undefined tokens generator', () =>
-            expect(Tokenservice.getTokens(['file_123', 'folder_123'], undefinedTokensGenerator)).resolves.toEqual({
-                file_123: undefined,
-                folder_123: undefined
-            }));
+            expect(Tokenservice.getWriteToken('file_123', undefinedTokenGenerator)).resolves.toBeUndefined());
 
         test('should return proper token with generator function that returns a string token', () =>
-            expect(Tokenservice.getTokens(['file_123', 'folder_123'], tokenGenerator)).resolves.toEqual({
-                file_123: 'token',
-                folder_123: 'token'
-            }));
+            expect(Tokenservice.getWriteToken('file_123', tokenGenerator)).resolves.toBe('token'));
 
-        test('should return proper token with generator function that returns a string tokens', () =>
-            expect(Tokenservice.getTokens(['file_123', 'folder_123'], tokensGenerator)).resolves.toEqual({
-                file_123: 'token1',
-                folder_123: 'token2'
-            }));
+        test('should return read token with generator function that returns a token map without write token', () =>
+            expect(Tokenservice.getWriteToken('file_123', readTokenGenerator)).resolves.toBe('read_token'));
 
-        test('should return proper token with generator function that returns a token map', () =>
-            expect(Tokenservice.getTokens(['file_123', 'folder_123'], tokensMapGenerator)).resolves.toEqual({
-                file_123: 'token1',
-                folder_123: 'token2'
-            }));
+        test('should return write token with generator function that returns a token map', () =>
+            expect(Tokenservice.getWriteToken('file_123', writeTokenGenerator)).resolves.toBe('write_token'));
+
+        test('should return write token with generator function that returns a both read and write tokens', () =>
+            expect(Tokenservice.getWriteToken('file_123', readWriteTokenGenerator)).resolves.toBe('write_token'));
 
         test('should reject when not given a typed id', () =>
-            expect(Tokenservice.getTokens(['123', 'folder_123'])).rejects.toThrow(/Bad id or auth token/));
+            expect(Tokenservice.getWriteToken('123')).rejects.toThrow(/Bad id or auth token/));
 
         test('should reject when not given proper token function', () =>
-            expect(Tokenservice.getTokens(['file_123', 'folder_123'], {})).rejects.toThrow(/Bad id or auth token/));
+            expect(Tokenservice.getWriteToken('file_123', {})).rejects.toThrow(/Bad id or auth token/));
 
         test('should reject when token generator returns junk', () =>
-            expect(Tokenservice.getTokens(['file_123', 'folder_123'], junkTokenGenerator)).rejects.toThrow(
-                /Bad id or auth token/
-            ));
+            expect(Tokenservice.getWriteToken('file_123', junkTokenGenerator)).rejects.toThrow(/Bad id or auth token/));
+    });
+
+    describe('getReadToken()', () => {
+        test('should return null for a null token', () => {
+            expect(Tokenservice.getReadToken('file_123', null)).resolves.toBeNull();
+        });
+
+        test('should return undefined for a undefined token', () => {
+            expect(Tokenservice.getReadToken('file_123')).resolves.toBeUndefined();
+        });
+
+        test('should return null for a null token generator', () =>
+            expect(Tokenservice.getReadToken('file_123', nullTokenGenerator)).resolves.toBeNull());
+
+        test('should return a string token', () => {
+            expect(Tokenservice.getReadToken('file_123', 'string_token')).resolves.toBe('string_token');
+        });
+
+        test('should return undefined for a undefined token generator', () =>
+            expect(Tokenservice.getReadToken('file_123', undefinedTokenGenerator)).resolves.toBeUndefined());
+
+        test('should return proper token with generator function that returns a string token', () =>
+            expect(Tokenservice.getReadToken('file_123', tokenGenerator)).resolves.toBe('token'));
+
+        test('should return read token with generator function that returns a token map without write token', () =>
+            expect(Tokenservice.getReadToken('file_123', readTokenGenerator)).resolves.toBe('read_token'));
+
+        test('should return undefined with generator function that returns a token map without read token', () =>
+            expect(Tokenservice.getReadToken('file_123', writeTokenGenerator)).resolves.toBeUndefined());
+
+        test('should return read token with generator function that returns a both read and write tokens', () =>
+            expect(Tokenservice.getReadToken('file_123', readWriteTokenGenerator)).resolves.toBe('read_token'));
+
+        test('should reject when not given a typed id', () =>
+            expect(Tokenservice.getReadToken('123')).rejects.toThrow(/Bad id or auth token/));
+
+        test('should reject when not given proper token function', () =>
+            expect(Tokenservice.getReadToken('file_123', {})).rejects.toThrow(/Bad id or auth token/));
+
+        test('should reject when token generator returns junk', () =>
+            expect(Tokenservice.getReadToken('file_123', junkTokenGenerator)).rejects.toThrow(/Bad id or auth token/));
+    });
+
+    describe('cacheTokens()', () => {
+        test('should call the token generator function', async () => {
+            const generator = jest.fn();
+            await Tokenservice.cacheTokens(['file_123', 'folder_123'], generator);
+            expect(generator).toHaveBeenCalledWith(['file_123', 'folder_123']);
+        });
+
+        test('should reject when not given a typed id', () =>
+            expect(Tokenservice.cacheTokens(['123', 'folder_123'])).rejects.toThrow(/Bad id or auth token/));
+
+        test('should reject when not given proper token function', () =>
+            expect(Tokenservice.cacheTokens(['file_123', 'folder_123'], {})).rejects.toThrow(/Bad id or auth token/));
     });
 });
