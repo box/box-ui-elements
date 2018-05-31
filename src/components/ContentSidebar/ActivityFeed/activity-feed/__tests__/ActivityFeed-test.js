f8fef57799062237d1dc606206ac0931e3c26ddc
Justin Holdstock <justinholdstock91@gmail.com>
2018-05-31 15:51:16 -0600

    Handle create success, and pending state, for comment (#332)

--- src/components/ContentSidebar/ActivityFeed/activity-feed/__tests__/ActivityFeed-test.js
+++ src/components/ContentSidebar/ActivityFeed/activity-feed/__tests__/ActivityFeed-test.js
@@ -1,9 +1,10 @@
 import * as React from 'react';
-import { mount, shallow } from 'enzyme';
+import { shallow } from 'enzyme';
 
 import ActivityFeed from '../ActivityFeed';
 
 jest.mock('../../Avatar', () => () => 'Avatar');
+jest.mock('lodash/uniqueId', () => () => 'uniqueId');
 const comments = {
     total_count: 1,
     entries: [
@@ -116,14 +117,14 @@ describe('components/ContentSidebar/ActivityFeed/activity-feed/ActivityFeed', ()
     });
 
     test('should call create comment handler and close input on valid comment submit', () => {
-        const createCommentSpy = jest.fn();
+        const createCommentSpy = jest.fn().mockReturnValue(Promise.resolve({}));
         allHandlers.comments.create = createCommentSpy;
         const file = {
             permissions: {
                 can_comment: true
             }
         };
-        const wrapper = mount(<ActivityFeed file={file} currentUser={currentUser} handlers={allHandlers} />);
+        const wrapper = shallow(<ActivityFeed file={file} currentUser={currentUser} handlers={allHandlers} />);
 
         const instance = wrapper.instance();
         const approvalCommentForm = wrapper.find('ApprovalCommentForm').at(0);
@@ -144,7 +145,7 @@ describe('components/ContentSidebar/ActivityFeed/activity-feed/ActivityFeed', ()
                 can_comment: true
             }
         };
-        const wrapper = mount(<ActivityFeed file={file} currentUser={currentUser} handlers={allHandlers} />);
+        const wrapper = shallow(<ActivityFeed file={file} currentUser={currentUser} handlers={allHandlers} />);
 
         const instance = wrapper.instance();
         const approvalCommentForm = wrapper.find('ApprovalCommentForm').at(0);
@@ -166,6 +167,53 @@ describe('components/ContentSidebar/ActivityFeed/activity-feed/ActivityFeed', ()
             }
         });
         expect(stopPropagationSpy).toHaveBeenCalled();
+    });
+
+    describe('updateFeedItems()', () => {
+        let wrapper;
+        let instance;
+        const file = {
+            id: '12345'
+        };
+        beforeEach(() => {
+            wrapper = shallow(<ActivityFeed currentUser={currentUser} file={file} />);
+            instance = wrapper.instance();
+            instance.sortFeedItems = jest.fn();
+        });
+
+        it('should not invoke sortFeedItems() if the feed item types required are missing', () => {
+            instance.updateFeedItems(); // no params means missing items
+
+            expect(instance.sortFeedItems).not.toBeCalled();
+        });
+
+        it('should not invoke sortFeedItems() if the current feed has not been emptied', () => {
+            instance.setState({ feedItems: [...comments.entries, ...tasks.entries] });
+            instance.clearFeedItems = jest.fn().mockReturnValue(false);
+            instance.shouldSortFeedItems = jest.fn().mockReturnValue(true);
+            instance.updateFeedItems(comments, tasks, versions);
+
+            expect(instance.sortFeedItems).not.toBeCalled();
+        });
+
+        it('should not invoke sortFeedItems() if there are items in the feed', () => {
+            instance.setState({ feedItems: [...comments.entries, ...tasks.entries] });
+            instance.clearFeedItems = jest.fn().mockReturnValue(false);
+            instance.shouldSortFeedItems = jest.fn().mockReturnValue(true);
+
+            instance.updateFeedItems(comments, tasks, versions);
+
+            expect(instance.sortFeedItems).not.toBeCalled();
+        });
+
+        it('should invoke sortFeedItems() if all conditions are met', () => {
+            instance.clearFeedItems = jest.fn().mockReturnValue(true);
+            instance.shouldSortFeedItems = jest.fn().mockReturnValue(true);
+
+            instance.updateFeedItems(comments, tasks, versions);
+
+            expect(instance.sortFeedItems).toBeCalledWith(comments, tasks, versions);
+        });
     });
 
     describe('shouldSortFeedItems()', () => {
@@ -215,12 +263,26 @@ describe('components/ContentSidebar/ActivityFeed/activity-feed/ActivityFeed', ()
             expect(feedItems.length).toBe(comments.entries.length);
         });
 
+        it('should return false if feedItems was not emptied', () => {
+            instance.setState({ feedItems: [...comments.entries] });
+            const wasEmptied = instance.clearFeedItems(file);
+
+            expect(wasEmptied).toBe(false);
+        });
+
         it('should clear feed items if a new file', () => {
             instance.setState({ feedItems: [...comments.entries] });
             instance.clearFeedItems({ id: 'abcdef' });
 
             const { feedItems } = instance.state;
             expect(feedItems.length).toBe(0);
+        });
+
+        it('should return true if feedItems was emptied', () => {
+            instance.setState({ feedItems: [...comments.entries] });
+            const wasEmptied = instance.clearFeedItems({ id: 'abcdef' });
+
+            expect(wasEmptied).toBe(true);
         });
     });
 
@@ -261,7 +323,7 @@ describe('components/ContentSidebar/ActivityFeed/activity-feed/ActivityFeed', ()
     });
 
     describe('sortFeedItems()', () => {
-        it('should sort items based on date', () => {
+        test('should sort items based on date', () => {
             const wrapper = shallow(<ActivityFeed currentUser={currentUser} />);
             const instance = wrapper.instance();
 
@@ -270,6 +332,233 @@ describe('components/ContentSidebar/ActivityFeed/activity-feed/ActivityFeed', ()
             const { feedItems } = instance.state;
             expect(feedItems[0].id).toEqual(comments.entries[0].id);
             expect(feedItems[1].id).toEqual(tasks.entries[0].id);
+        });
+    });
+
+    describe('addPendingItem()', () => {
+        let wrapper;
+        let instance;
+        beforeEach(() => {
+            wrapper = shallow(<ActivityFeed currentUser={currentUser} />);
+            instance = wrapper.instance();
+        });
+
+        test('should create an item and add it to the feed', () => {
+            const itemBase = {
+                my_prop: 'yay'
+            };
+
+            instance.addPendingItem(itemBase);
+            const { feedItems } = instance.state;
+            expect(feedItems.length).toEqual(1);
+        });
+
+        test('should create pending item with correct properties', () => {
+            const itemBase = {};
+
+            instance.addPendingItem(itemBase);
+            const { feedItems } = instance.state;
+            const item = feedItems[0];
+            expect(typeof item.created_at).toBe('string');
+            expect(typeof item.created_by).toBe('object');
+            expect(typeof item.modified_at).toBe('string');
+            expect(typeof item.isPending).toBe('boolean');
+        });
+
+        test('should add base properties into item added to feed', () => {
+            const itemBase = {
+                my_prop: 'supercalifragilisticexpialidocious'
+            };
+
+            instance.addPendingItem(itemBase);
+            const { feedItems } = instance.state;
+            const item = feedItems[0];
+            expect(item.my_prop).toBe(itemBase.my_prop);
+        });
+    });
+
+    describe('updateFeedItem()', () => {
+        let wrapper;
+        let instance;
+        beforeEach(() => {
+            wrapper = shallow(<ActivityFeed currentUser={currentUser} />);
+            instance = wrapper.instance();
+        });
+
+        test('should replace the item with matching uuid in feedItems', () => {
+            const uuid = 'a1b2c3d4e5f6';
+            const message = 'This is missing in the pending item';
+            const oldFeedItems = [
+                {
+                    id: uuid
+                }
+            ];
+            const item = {
+                message
+            };
+            instance.setState({ feedItems: oldFeedItems });
+
+            // New item details
+            instance.updateFeedItem(item, uuid);
+
+            const { feedItems } = instance.state;
+            expect(feedItems[0].message).toBe(message);
+        });
+
+        test('should mark the item with matching item as longer pending', () => {
+            const uuid = 'a1b2c3d4e5f6';
+            const message = 'This is missing in the pending item';
+            const oldFeedItems = [
+                {
+                    id: uuid
+                }
+            ];
+            const item = {
+                message
+            };
+            instance.setState({ feedItems: oldFeedItems });
+
+            // New item details
+            instance.updateFeedItem(item, uuid);
+
+            const { feedItems } = instance.state;
+            expect(feedItems[0].isPending).toBe(false);
+        });
+
+        test('should do nothing if it can\'t find an item with matching uuid', () => {
+            const uuid = 'a1b2c3d4e5f6';
+            const message = 'This is missing in the pending item';
+            const oldFeedItems = [
+                {
+                    id: uuid
+                }
+            ];
+            const item = {
+                message
+            };
+            instance.setState({ feedItems: oldFeedItems });
+
+            // New item details
+            instance.updateFeedItem(item, 'some_other_id');
+
+            const { feedItems } = instance.state;
+            expect(feedItems[0].message).not.toBe(message);
+        });
+    });
+
+    describe('createCommentSuccessCallback()', () => {
+        let wrapper;
+        let instance;
+        beforeEach(() => {
+            wrapper = shallow(<ActivityFeed currentUser={currentUser} />);
+            instance = wrapper.instance();
+            instance.updateFeedItem = jest.fn();
+        });
+
+        it('should invoke updateFeedItem() with the new API returned comment, and the id to replace', () => {
+            const text = 'yay';
+            const id = '0987654321';
+            instance.createCommentSuccessCallback({ tagged_message: text }, id);
+
+            expect(instance.updateFeedItem).toBeCalledWith({ tagged_message: text }, id);
+        });
+
+        it('should assign tagged_message of the comment with tagged_message value if it exists', (done) => {
+            const text = 'yay';
+            const id = '0987654321';
+            instance.updateFeedItem = (comment) => {
+                const { tagged_message } = comment;
+                expect(tagged_message).toEqual(text);
+                done();
+            };
+            instance.createCommentSuccessCallback({ tagged_message: text }, id);
+        });
+
+        it('should assign tagged_message of the comment with message value if it exists', (done) => {
+            const text = 'yay';
+            const id = '0987654321';
+            instance.updateFeedItem = (comment) => {
+                const { tagged_message } = comment;
+                expect(tagged_message).toEqual(text);
+                done();
+            };
+            instance.createCommentSuccessCallback({ message: text }, id);
+        });
+    });
+
+    describe('createComment()', () => {
+        let wrapper;
+        let instance;
+        const create = jest.fn();
+        const message = 'message';
+        const handlers = {
+            comments: {
+                create
+            }
+        };
+        beforeEach(() => {
+            wrapper = shallow(<ActivityFeed currentUser={currentUser} handlers={handlers} />);
+            instance = wrapper.instance();
+            instance.addPendingItem = jest.fn();
+            instance.updateFeedItem = jest.fn();
+        });
+
+        test('should creating a pending item', () => {
+            instance.createComment({ text: message });
+
+            expect(instance.addPendingItem).toBeCalledWith({
+                id: 'uniqueId',
+                tagged_message: 'message',
+                type: 'comment'
+            });
+        });
+
+        test('should invoke the handler prop create() function with data to create comment and callbacks', () => {
+            const hasMention = true;
+            instance.createComment({ text: message, hasMention });
+
+            expect(create).toBeCalledWith(message, hasMention, expect.any(Function), expect.any(Function));
+        });
+
+        test('should invoke createCommentSuccessCallback() with new comment and id on success creating', () => {
+            const createComment = (text, hasMention, onSuccess) => {
+                const comment = {
+                    message,
+                    hasMention
+                };
+                onSuccess(comment);
+            };
+            const newHandlers = {
+                comments: {
+                    create: createComment
+                }
+            };
+            wrapper.setProps({ handlers: newHandlers });
+            instance.createCommentSuccessCallback = jest.fn();
+
+            const hasMention = true;
+            instance.createComment({ text: message, hasMention });
+
+            // Should be called with new comment and the 'uniqueId' returned from lodash/uniqueId
+            expect(instance.createCommentSuccessCallback).toBeCalledWith({ message, hasMention }, 'uniqueId');
+        });
+
+        test('should invoke createCommentErrorCallback() with error and id on fail to create', () => {
+            const createComment = (text, hasMention, onSuccess, onFail) => {
+                onFail(new Error('You fail!'));
+            };
+            const newHandlers = {
+                comments: {
+                    create: createComment
+                }
+            };
+            wrapper.setProps({ handlers: newHandlers });
+            instance.createCommentErrorCallback = jest.fn();
+
+            instance.createComment({ text: message });
+
+            // Should be called with new comment and the 'uniqueId' returned from lodash/uniqueId
+            expect(instance.createCommentErrorCallback).toBeCalledWith(expect.any(Error), 'uniqueId');
         });
     });
 });
