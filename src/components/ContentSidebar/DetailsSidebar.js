ca0357a19f8dfb67e75f011098a3cce5e5a17a46
Daniel DeMicco <ddemicco@box.com>
2018-07-25 16:33:21 -0700

    Chore: revert a proptype

--- src/components/ContentSidebar/DetailsSidebar.js
+++ src/components/ContentSidebar/DetailsSidebar.js
@@ -37,7 +37,7 @@ export type Props = {
     file: BoxItem,
     onDescriptionChange: Function,
     versions?: FileVersions,
-    accessStatsError?: Errors | MessageDescriptor,
+    accessStatsError?: Errors,
     fileError?: Errors,
     onVersionHistoryClick?: Function,
     isFileLoading?: boolean
