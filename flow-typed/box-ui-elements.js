16a8d068c739223184b791fe4531677a354548a3
Jeremy Press <jpress@box.com>
2018-09-04 09:52:35 -0700

    Fix: Fix flow types

--- flow-typed/box-ui-elements.js
+++ flow-typed/box-ui-elements.js
@@ -544,8 +544,8 @@ type Translations = {
 
 type OpenWithIntegrations = {
     default_app_integration?: AppIntegrationMiniItem,
-    disabled_reasons?: Array<String>,
-    icon?: String,
+    disabled_reasons?: Array<string>,
+    icon?: string,
     is_disabled?: boolean,
     items: Array<OpenWithIntegrationItem>,
     should_show_consent_popup?: boolean
@@ -553,9 +553,9 @@ type OpenWithIntegrations = {
 
 type OpenWithIntegrationItem = {
     app_integration: AppIntegrationMiniItem | AppIntegrationItem,
-    disabled_reasons: Array<String>,
+    disabled_reasons: Array<string>,
     display_order: number,
-    icon: String,
+    icon: string,
     is_disabled: boolean,
     should_show_consent_popup: boolean
 }
