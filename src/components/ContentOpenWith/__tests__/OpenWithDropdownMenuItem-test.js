b2b97834891680d455d9619285cc56e7b9a4c0c2
Jeremy Press <jpress@box.com>
2018-10-19 14:19:43 -0700

    Chore: Test OpenWith dropdown

---
+++ src/components/ContentOpenWith/__tests__/OpenWithDropdownMenuItem-test.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import OpenWithDropdownMenuItem from '../OpenWithDropdownMenuItem';
+
+describe('components/ContentOpenWith/OpenWithMenu', () => {
+    const getWrapper = props =>
+        shallow(<OpenWithDropdownMenuItem {...props} />);
+
+    it('should render the description and correct icon', () => {
+        const props = {
+            integration: {
+                displayName: 'Adobe Sign',
+                displayDescription: 'Open With Adobe',
+                appIntegrationId: '1',
+            },
+        };
+        const wrapper = getWrapper(props);
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    it('should use the default icon when no mapping can be found to an existing icon', () => {
+        const props = {
+            integration: {
+                displayName: 'A new integration',
+                displayDescription: 'Open With the new integration',
+                appIntegrationId: '22',
+            },
+        };
+        const wrapper = getWrapper(props);
+        expect(wrapper).toMatchSnapshot();
+    });
+});
