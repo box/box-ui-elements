82c5d737f92f506783e008902fff7453755623d5
Alex Krolick <akrolick@box.com>
2018-12-13 16:15:07 -0800

    chore(feat-flip): remove unneeded template string

--- src/components/FeatureChecking/util.js
+++ src/components/FeatureChecking/util.js
@@ -3,11 +3,11 @@ import get from 'lodash/get';
 import * as types from './flowTypes';
 
 function isFeatureEnabled(features: types.FeatureConfig, featureName: string) {
-    return !!get(features, `${featureName}`, false);
+    return !!get(features, featureName, false);
 }
 
 function getFeatureConfig(features: types.FeatureConfig, featureName: string) {
-    return get(features, `${featureName}`, {});
+    return get(features, featureName, {});
 }
 
 export { isFeatureEnabled, getFeatureConfig };
