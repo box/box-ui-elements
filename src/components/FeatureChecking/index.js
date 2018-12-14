0692701740d1a813081a5e7b97b7e16e8429f73c
Alex Krolick <alexkrolick@users.noreply.github.com>
2018-12-13 16:42:57 -0800

    feat: feature flag component (#709)
    * chore: add feature flag component
    
    * chore: fix eslint glob
    
    * chore(feat-flip): take node as child instead of function
    
    * chore(feat-flip): split features.js into files
    
    * chore(feat-flip): fix dupe types
    
    * chore(feat-flip): remove unneeded template string

---
+++ src/components/FeatureChecking/index.js
@@ -0,0 +1,6 @@
+export { default as FeatureContext } from './FeatureContext';
+export { default as FeatureProvider } from './FeatureProvider';
+export { default as FeatureConsumer } from './FeatureConsumer';
+export { default as FeatureFlag } from './FeatureFlag';
+export { isFeatureEnabled, getFeatureConfig } from './util';
+export * from './flowTypes';
