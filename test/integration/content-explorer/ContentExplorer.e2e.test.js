0abede8aece19c77b1720a0cdd9a55c2515e4a57
Jared Stoffan <jstoffan@box.com>
2019-01-04 12:29:02 -0800

    chore: Remove cypress-testing-library and rename test

--- test/integration/content-explorer/ContentExplorer.e2e.test.js
+++ test/integration/content-explorer/ContentExplorer.e2e.test.js
@@ -12,25 +12,25 @@ describe('ContentExplorer', () => {
 
         it('Navigation within a collection keeps sidebar open', () => {
             // Sidebar should be open by default
-            cy.queryByTestId('bcs-content').should('exist');
+            cy.getByTestId('bcs-content').should('exist');
 
             // Navigating between files in a collection should retain the prior open state
             cy.getByTitle('Next File').click();
-            cy.queryByTestId('bcs-content').should('exist');
+            cy.getByTestId('bcs-content').should('exist');
             cy.getByTitle('Previous File').click();
-            cy.queryByTestId('bcs-content').should('exist');
+            cy.getByTestId('bcs-content').should('exist');
         });
 
         it('Navigation within a collection keeps sidebar closed', () => {
             // Manually closing the sidebar should remove its content
-            cy.queryByTestId('sidebaractivity').click();
-            cy.queryByTestId('bcs-content').should('not.exist');
+            cy.getByTestId('sidebaractivity').click();
+            cy.getByTestId('bcs-content').should('not.exist');
 
             // Navigating between files in a collection should retain the prior closed state
             cy.getByTitle('Next File').click();
-            cy.queryByTestId('bcs-content').should('not.exist');
+            cy.getByTestId('bcs-content').should('not.exist');
             cy.getByTitle('Previous File').click();
-            cy.queryByTestId('bcs-content').should('not.exist');
+            cy.getByTestId('bcs-content').should('not.exist');
         });
     });
 });
