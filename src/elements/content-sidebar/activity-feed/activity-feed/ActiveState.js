4dfd58afca0236aece407baed92ed129240648ee
Alex Krolick <akrolick@box.com>
2019-02-01 17:58:37 -0800

    feat(tasks): new task card layout

--- src/elements/content-sidebar/activity-feed/activity-feed/ActiveState.js
+++ src/elements/content-sidebar/activity-feed/activity-feed/ActiveState.js
@@ -4,8 +4,10 @@
  */
 import * as React from 'react';
 import getProp from 'lodash/get';
+import { FeatureFlag } from 'elements/common/feature-checking';
 import Comment from '../comment';
 import Task from '../task';
+import TaskNew from '../task-new';
 import Version, { CollapsedVersion } from '../version';
 import Keywords from '../keywords';
 import withErrorHandling from '../../withErrorHandling';
@@ -62,27 +64,54 @@ const ActiveState = ({
                         </li>
                     );
                 case 'task':
-                    return item.task_assignment_collection.total_count ? (
-                        <li key={type + id} className="bcs-activity-feed-task">
-                            <Task
-                                {...item}
-                                currentUser={currentUser}
-                                getAvatarUrl={getAvatarUrl}
-                                getMentionWithQuery={getMentionWithQuery}
-                                getUserProfileUrl={getUserProfileUrl}
-                                mentionSelectorContacts={mentionSelectorContacts}
-                                onAssignmentUpdate={onTaskAssignmentUpdate}
-                                onDelete={onTaskDelete}
-                                onEdit={onTaskEdit}
-                                permissions={{
-                                    can_delete: true,
-                                    can_edit: true,
-                                }}
-                                // permissions are not part of task API so hard code to true
-                                translations={translations}
-                            />
-                        </li>
-                    ) : null;
+                    return (
+                        <div>
+                            <FeatureFlag not feature="activityFeed.tasks.avatars">
+                                {item.task_assignment_collection.total_count && (
+                                    <li key={type + id} className="bcs-activity-feed-task">
+                                        <Task
+                                            {...item}
+                                            currentUser={currentUser}
+                                            getAvatarUrl={getAvatarUrl}
+                                            getMentionWithQuery={getMentionWithQuery}
+                                            getUserProfileUrl={getUserProfileUrl}
+                                            mentionSelectorContacts={mentionSelectorContacts}
+                                            onAssignmentUpdate={onTaskAssignmentUpdate}
+                                            onDelete={onTaskDelete}
+                                            onEdit={onTaskEdit}
+                                            // permissions are not part of task API so hard code to true
+                                            permissions={{
+                                                can_delete: true,
+                                                can_edit: true,
+                                            }}
+                                            translations={translations}
+                                        />
+                                    </li>
+                                )}
+                            </FeatureFlag>
+                            <FeatureFlag feature="activityFeed.tasks.avatars">
+                                <li key={type + id} className="bcs-activity-feed-task-new">
+                                    <TaskNew
+                                        {...item}
+                                        currentUser={currentUser}
+                                        getAvatarUrl={getAvatarUrl}
+                                        getMentionWithQuery={getMentionWithQuery}
+                                        getUserProfileUrl={getUserProfileUrl}
+                                        mentionSelectorContacts={mentionSelectorContacts}
+                                        onAssignmentUpdate={onTaskAssignmentUpdate}
+                                        onDelete={onTaskDelete}
+                                        onEdit={onTaskEdit}
+                                        // permissions are not part of task API so hard code to true
+                                        permissions={{
+                                            can_delete: true,
+                                            can_edit: true,
+                                        }}
+                                        translations={translations}
+                                    />
+                                </li>
+                            </FeatureFlag>
+                        </div>
+                    );
                 case 'file_version':
                     return (
                         <li key={type + id} className="bcs-version-item">
---
+++ src/elements/content-sidebar/activity-feed/task-new/Task.js
@@ -0,0 +1,158 @@
+/**
+ * @flow
+ * @file Tasks component
+ */
+
+import * as React from 'react';
+import noop from 'lodash/noop';
+import { FormattedTime, FormattedMessage } from 'react-intl';
+import classNames from 'classnames';
+import messages from 'elements/common/messages';
+import { fillUserPlaceholder } from 'utils/fields';
+import Comment from '../comment';
+import AssigneeStatus from './AssigneeStatus';
+import PendingAssignment from './PendingAssignment';
+import { TASK_APPROVED, TASK_REJECTED, TASK_INCOMPLETE } from '../../../../constants';
+import './Task.scss';
+
+type Props = {
+    created_at: number | string,
+    created_by: User,
+    currentUser?: User,
+    due_at: any,
+    error?: ActionItemError,
+    getAvatarUrl: string => Promise<?string>,
+    getMentionWithQuery?: Function,
+    getUserProfileUrl?: string => Promise<string>,
+    id: string,
+    isDisabled?: boolean,
+    isPending?: boolean,
+    mentionSelectorContacts?: SelectorItems,
+    message: string,
+    onAssignmentUpdate: Function,
+    onDelete?: Function,
+    onEdit?: Function,
+    permissions?: BoxItemPermission,
+    task_assignment_collection: TaskAssignments | SelectorItems,
+    translatedTaggedMessage?: string,
+    translations?: Translations,
+};
+
+const MAX_AVATARS = 3;
+
+// eslint-disable-next-line
+class Task extends React.Component<Props> {
+    render(): React.Node {
+        const {
+            task_assignment_collection,
+            created_at,
+            created_by,
+            currentUser,
+            due_at,
+            error,
+            id,
+            isPending,
+            onDelete,
+            onEdit,
+            onAssignmentUpdate = noop,
+            message,
+            translatedTaggedMessage,
+            translations,
+            getAvatarUrl,
+            getUserProfileUrl,
+            getMentionWithQuery,
+            mentionSelectorContacts,
+        } = this.props;
+
+        const isCommentAuthor = currentUser && created_by && created_by.id === currentUser.id;
+
+        const taskPermissions = {
+            can_edit: isCommentAuthor,
+            can_delete: isCommentAuthor,
+        };
+
+        // // find assignment for current user if permitted
+
+        const currentUserAssignment =
+            task_assignment_collection && task_assignment_collection.entries
+                ? task_assignment_collection.entries
+                      .map(fillUserPlaceholder)
+                      .find(({ assigned_to }) => assigned_to.id === currentUser.id)
+                : null;
+
+        const assigneeCount = (task_assignment_collection && task_assignment_collection.entries.length) || 0;
+        const hiddenAssigneeCount = assigneeCount - MAX_AVATARS;
+
+        return (
+            <div
+                className={classNames('bcs-task', {
+                    'bcs-is-pending': isPending || error,
+                })}
+                data-testid="task-card"
+            >
+                <Comment
+                    created_at={created_at}
+                    created_by={created_by}
+                    currentUser={currentUser}
+                    error={error}
+                    id={id}
+                    inlineDeleteMessage={messages.taskDeletePrompt}
+                    isPending={isPending}
+                    onDelete={onDelete}
+                    onEdit={onEdit}
+                    permissions={taskPermissions}
+                    tagged_message={message}
+                    translatedTaggedMessage={translatedTaggedMessage}
+                    translations={translations}
+                    getAvatarUrl={getAvatarUrl}
+                    getUserProfileUrl={getUserProfileUrl}
+                    mentionSelectorContacts={mentionSelectorContacts}
+                    getMentionWithQuery={getMentionWithQuery}
+                />
+                <div className="bcs-task-assignment-container">
+                    {due_at ? (
+                        <div className="bcs-task-due-date">
+                            <FormattedMessage {...messages.taskDueDate} />
+                            <FormattedTime value={due_at} day="numeric" month="short" year="numeric" />
+                        </div>
+                    ) : null}
+                    <div className="bcs-task-assignments">
+                        {task_assignment_collection && task_assignment_collection.entries
+                            ? task_assignment_collection.entries
+                                  .map(fillUserPlaceholder)
+                                  .slice(0, MAX_AVATARS)
+                                  .map(({ id: assignmentId, assigned_to, status }) => {
+                                      return (
+                                          <AssigneeStatus
+                                              key={assignmentId}
+                                              status={status}
+                                              user={assigned_to}
+                                              getAvatarUrl={getAvatarUrl}
+                                          />
+                                      );
+                                  })
+                            : null}
+                        {hiddenAssigneeCount > 0 ? (
+                            <span className="bcs-task-assignment-avatar bcs-task-assignment-count">
+                                +{hiddenAssigneeCount}
+                            </span>
+                        ) : null}
+                    </div>
+                    {currentUserAssignment && currentUserAssignment.status === TASK_INCOMPLETE ? (
+                        <PendingAssignment
+                            {...currentUserAssignment}
+                            onTaskApproval={
+                                isPending ? noop : () => onAssignmentUpdate(id, currentUserAssignment.id, TASK_APPROVED)
+                            }
+                            onTaskReject={
+                                isPending ? noop : () => onAssignmentUpdate(id, currentUserAssignment.id, TASK_REJECTED)
+                            }
+                            shouldShowActions={onAssignmentUpdate !== noop}
+                        />
+                    ) : null}
+                </div>
+            </div>
+        );
+    }
+}
+export default Task;
