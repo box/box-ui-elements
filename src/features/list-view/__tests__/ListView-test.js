3107dcaf45d3e9e680eaebdc0afadaa4d0f92980
Priyajeet Hora <priyajeet@box.com>
2019-02-12 13:01:26 -0800

    fix: shared link precedence in quick search
    latest pull from brui
    Shared link should take precedence in quick search item link
    query-bar and list-view updates

--- src/features/list-view/__tests__/ListView-test.js
+++ src/features/list-view/__tests__/ListView-test.js
@@ -16,7 +16,6 @@ describe('features/list-view/ListView', () => {
                 columnCount={2}
                 getGridCell={getGridCell}
                 getGridHeader={getGridHeader}
-                getColumnWidth={jest.fn()}
                 height={100}
                 rowCount={2}
                 width={100}
@@ -24,48 +23,77 @@ describe('features/list-view/ListView', () => {
             />,
         );
     };
-    describe('getGridCellData Methods', () => {
+    describe('CellRenderer()', () => {
         test.each`
-            columnIndex | rowIndex | cellData | should
-            ${0}        | ${0}     | ${'h1'}  | ${'returns h1 when columnIndex is 0 and rowIndex is 0'}
-            ${1}        | ${0}     | ${'h2'}  | ${'returns h2 when columnIndex is 1 and rowIndex is 0'}
-            ${0}        | ${1}     | ${'A'}   | ${'returns A when columnIndex is 0 and rowIndex is 1'}
-            ${0}        | ${2}     | ${'B'}   | ${'returns B when columnIndex is 0 and rowIndex is 2'}
-            ${0}        | ${3}     | ${'C'}   | ${'returns C when columnIndex is 0 and rowIndex is 3'}
-        `('$should', ({ columnIndex, rowIndex, cellData }) => {
+            columnIndex | rowIndex | cellData | should                                                  | className
+            ${0}        | ${0}     | ${'h1'}  | ${'returns h1 when columnIndex is 0 and rowIndex is 0'} | ${'list-view-column-header'}
+            ${0}        | ${1}     | ${'A'}   | ${'returns A when columnIndex is 0 and rowIndex is 1'}  | ${'list-view-name-cell'}
+            ${0}        | ${2}     | ${'B'}   | ${'returns B when columnIndex is 0 and rowIndex is 2'}  | ${'list-view-name-cell'}
+            ${0}        | ${3}     | ${'C'}   | ${'returns C when columnIndex is 0 and rowIndex is 3'}  | ${'list-view-name-cell'}
+            ${1}        | ${0}     | ${'h2'}  | ${'returns h2 when columnIndex is 1 and rowIndex is 0'} | ${'list-view-column-header'}
+            ${1}        | ${1}     | ${'D'}   | ${'returns A when columnIndex is 0 and rowIndex is 1'}  | ${'list-view-column-cell'}
+            ${1}        | ${2}     | ${'E'}   | ${'returns B when columnIndex is 0 and rowIndex is 2'}  | ${'list-view-column-cell'}
+            ${1}        | ${3}     | ${'F'}   | ${'returns C when columnIndex is 0 and rowIndex is 3'}  | ${'list-view-column-cell'}
+        `('$should', ({ columnIndex, rowIndex, cellData, className }) => {
             const wrapper = getWrapper();
+
             const cell = wrapper.instance().cellRenderer({
                 columnIndex,
                 key: 'heh',
                 rowIndex,
             });
 
-            // if rowIndex is zero that means its a Header Column
-            if (rowIndex === 0) {
-                expect(shallow(cell).html()).toEqual(`<div class="item-list-column-header">${cellData}</div>`);
-            } else if (columnIndex === 0) {
-                expect(shallow(cell).html()).toEqual(`<div class="item-list-name-cell">${cellData}</div>`);
-            } else {
-                expect(shallow(cell).html()).toEqual(`<div class="item-list-column-cell">${cellData}</div>`);
-            }
+            const testCell = shallow(cell);
+
+            expect(testCell.text()).toEqual(cellData);
+
+            expect(testCell.hasClass(className)).toBeTruthy();
         });
     });
 
-    describe('Snapshot test', () => {
-        test('Should return a snapshot of the ListView component', () => {
-            // ListView uses a React Virtualized MultiGrid, which is a wrapper around 4 grid components, shallow returns nothing useful, which is why we are using mount to see the actual data in the snapshot.
+    describe('getColumnWidth()', () => {
+        const getColumnWidth = columnIndex => [100, 500][columnIndex];
+
+        const wrapper = getWrapper({ getColumnWidth });
+
+        const wrapperProps = wrapper.instance().props;
+
+        test('should return 100 if the index is even', () => {
+            const width = wrapperProps.getColumnWidth(0);
+            expect(width).toEqual(100);
+        });
+
+        test('should return 500 if the index is odd', () => {
+            const width = wrapperProps.getColumnWidth(1);
+            expect(width).toEqual(500);
+        });
+    });
+
+    describe('render()', () => {
+        test('Should render the ListView component ', () => {
+            const columnHeaderClass = '.list-view-column-header';
+            const nameCellClass = '.list-view-name-cell';
+
+            // ListView uses a React Virtualized MultiGrid, which is a wrapper around 4 grid components, shallow returns nothing useful, which is why we are using mount instead.
             const wrapper = mount(
                 <ListView
-                    columnCount={4}
+                    columnCount={2}
                     getGridCell={getGridCell}
                     getGridHeader={getGridHeader}
-                    getColumnWidth={jest.fn()}
                     height={100}
-                    rowCount={4}
+                    rowCount={2}
                     width={100}
                 />,
             );
-            expect(wrapper).toMatchSnapshot();
+
+            const columnHeader = wrapper.find(columnHeaderClass);
+            const nameCell = wrapper.find(nameCellClass);
+
+            expect(columnHeader).toHaveLength(1);
+            expect(nameCell).toHaveLength(1);
+
+            expect(columnHeader.text()).toEqual('h1');
+            expect(nameCell.text()).toEqual('A');
         });
     });
 });
