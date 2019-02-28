5e24f5fa2a8e8ff6322b617e1d09cd0d2164f84d
Jeremy Press <jpress@box.com>
2019-02-27 16:09:25 -0800

    feat(content-sidebar): add passed in props to callback execution

--- src/elements/content-sidebar/AdditionalTab.js
+++ src/elements/content-sidebar/AdditionalTab.js
@@ -9,10 +9,14 @@ import noop from 'lodash/noop';
 import Tooltip from '../common/Tooltip';
 import PlainButton from '../../components/plain-button/PlainButton';
 
-const AdditionalTab = ({ callback: callbackFn = noop, element, id, title }: AdditionalTab) => {
+const AdditionalTab = ({ callback: callbackFn = noop, element, id, title, ...rest }: AdditionalTab) => {
     return (
         <Tooltip position="middle-left" text={title}>
-            <PlainButton className="bcs-additional-tab-btn bcs-nav-btn" type="button" onClick={() => callbackFn(id)}>
+            <PlainButton
+                className="bcs-additional-tab-btn bcs-nav-btn"
+                type="button"
+                onClick={() => callbackFn(id, ...rest)}
+            >
                 {element}
             </PlainButton>
         </Tooltip>
