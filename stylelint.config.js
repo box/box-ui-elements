533c9cbfa6d429de96946f47279e72087227cb27
Priyajeet Hora <priyajeet@box.com>
2019-03-04 11:52:54 -0800

    fix: iii

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,7 +1,19 @@
 const stylelintrc = require.resolve('@box/frontend/stylelint/stylelint.config.js');
 
+const customSuitSelector = component => {
+    const namespace = 'bdl';
+    const block = component;
+    const camelCase = '[a-z][a-zA-Z0-9]*';
+    const state = `(?:.${namespace}-(is|has)-${camelCase})?`;
+    const element = `(?:-${camelCase})?`;
+    const modifier = `(?:--${camelCase})?`;
+    const attribute = '(?:\\[.+\\])?';
+    return new RegExp(`^\\.${namespace}-${block}${element}${modifier}${attribute}${state}$`);
+};
+
 module.exports = {
     extends: [stylelintrc],
+    plugins: ['stylelint-selector-bem-pattern'],
     rules: {
         'no-descending-specificity': null, // fixme
         'declaration-no-important': null, // fixme
@@ -11,5 +23,13 @@ module.exports = {
         'selector-no-vendor-prefix': null, // fixme
         'property-no-unknown': null, // fixme
         'at-rule-no-vendor-prefix': null, // fixme
+
+        'plugin/selector-bem-pattern': {
+            preset: 'suit',
+            componentName: '[A-Z]+',
+            componentSelectors: {
+                initial: customSuitSelector,
+            },
+        },
     },
 };
