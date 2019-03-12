46d4bd0b87772d58e112133c02a11b707934a619
Tim Chan <tchan@box.com>
2019-03-12 14:31:04 -0700

    fix(list-view): Fixed off-by-one bug in ListView

--- src/features/list-view/examples/ListViewExamples.js
+++ src/features/list-view/examples/ListViewExamples.js
@@ -7,13 +7,13 @@ import ListView from '../ListView';
 
 const generateRandomString = value => String.fromCharCode((value % 65) + 65);
 
-const getGridHeader = columnIndex => generateRandomString(columnIndex);
+const getGridHeader = columnIndex => `Header ${generateRandomString(columnIndex)}`;
 
-const getGridCell = ({ columnIndex, rowIndex }) => generateRandomString(columnIndex + rowIndex);
+const getGridCell = ({ columnIndex, rowIndex }) => `Row ${rowIndex}, Column ${columnIndex}`;
 
 const ListViewExamples = () => {
     return (
-        <div style={{ height: '1000px' }}>
+        <div style={{ height: '300px' }}>
             <AutoSizer>
                 {({ height, width }) => (
                     <ListView
@@ -21,7 +21,7 @@ const ListViewExamples = () => {
                         height={height}
                         getGridCell={getGridCell}
                         getGridHeader={getGridHeader}
-                        rowCount={35}
+                        rowCount={1000}
                         width={width}
                     />
                 )}
