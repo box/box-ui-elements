67eea6d4b4198f5c6637896aabbe7db7fac62fd3
Tim Gupta <tjgupta@users.noreply.github.com>
2019-03-13 15:04:49 -0700

    fix(unified-share-modal): update ftux link (#958)

--- src/features/unified-share-modal/messages.js
+++ src/features/unified-share-modal/messages.js
@@ -25,13 +25,13 @@ const messages = defineMessages({
     },
     ftuxNewUSMUserBody: {
         defaultMessage:
-            "We’ve simplified the sharing experience when you click 'Share'. Invite people to this item here and toggle the link on or off below for easy sharing. Read more about shared link security {hereText}.",
+            "We’ve simplified the sharing experience when you click 'Share'. Invite people to this item here and toggle the link on or off below for easy sharing.",
         description: 'This text describes the purpose of the new UI, using the button label to open the modal',
         id: 'boxui.unifiedShare.ftuxNewUSMUserBody',
     },
     ftuxLinkText: {
-        defaultMessage: 'here',
-        description: 'Text on the link which allows the user to click on',
+        defaultMessage: 'Read more about shared link security here.',
+        description: 'Text on the link which allows to learn more about link security',
         id: 'boxui.unifiedShare.ftuxLinkText',
     },
     ftuxConfirmLabel: {
