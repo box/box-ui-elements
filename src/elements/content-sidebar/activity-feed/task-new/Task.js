fdb6381610b614ec0b359cfe98a1534d2f85ad6a
Alex Krolick <akrolick@box.com>
2019-03-20 17:13:55 -0700

    fix(tasks): fix flow check

--- src/elements/content-sidebar/activity-feed/task-new/Task.js
+++ src/elements/content-sidebar/activity-feed/task-new/Task.js
@@ -143,7 +143,7 @@ class Task extends React.Component<Props> {
                             </span>
                         ) : null}
                     </div>
-                    {shouldShowActions ? (
+                    {currentUserAssignment && shouldShowActions ? (
                         <TaskActions
                             taskType={task_type}
                             onTaskApproval={
