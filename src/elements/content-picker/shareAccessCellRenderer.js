55838b8778c70c84ca2de7489f0a934b671901ec
Conrad Chan <conrad@box.com>
2019-03-26 17:44:59 -0700

    perf(picker): Fetch Shared Link info on select (#994)

--- src/elements/content-picker/shareAccessCellRenderer.js
+++ src/elements/content-picker/shareAccessCellRenderer.js
@@ -5,8 +5,10 @@
  */
 
 import React from 'react';
+import getProp from 'lodash/get';
 import ShareAccessSelect from '../common/share-access-select';
 import isRowSelectable from './cellRendererHelper';
+import LoadingIndicator from '../../components/loading-indicator';
 
 export default (
     onChange: Function,
@@ -15,11 +17,23 @@ export default (
     extensionsWhitelist: string[],
     hasHitSelectionLimit: boolean,
 ) => ({ rowData }: { rowData: BoxItem }) => {
-    if (!isRowSelectable(selectableType, extensionsWhitelist, hasHitSelectionLimit, rowData)) {
+    const itemCanSetShareAccess = getProp(rowData, 'permissions.can_set_share_access', false);
+
+    if (
+        !canSetShareAccess ||
+        !itemCanSetShareAccess ||
+        !isRowSelectable(selectableType, extensionsWhitelist, hasHitSelectionLimit, rowData) ||
+        !rowData.selected
+    ) {
         return <span />;
     }
 
-    return (
+    const { allowed_shared_link_access_levels } = rowData;
+    const isLoading = !allowed_shared_link_access_levels;
+
+    return isLoading ? (
+        <LoadingIndicator className="bcp-share-access-loading" />
+    ) : (
         <ShareAccessSelect
             canSetShareAccess={canSetShareAccess}
             className="bcp-shared-access-select"
