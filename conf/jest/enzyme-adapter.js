4ba90f7676d3f440952b9f6c62b3ed41939c11c7
Alex Krolick <akrolick@box.com>
2019-04-01 21:38:00 -0700

    fix(tasks): correct task type enum

--- conf/jest/enzyme-adapter.js
+++ conf/jest/enzyme-adapter.js
@@ -11,3 +11,6 @@ Enzyme.configure({ adapter: new Adapter() });
 // make Enzyme functions available in all test files without importing
 global.shallow = shallow;
 global.mount = mount;
+
+// testing utility functions
+global.queryAllByTestId = (wrapper, testid) => wrapper.find(`[data-testid="${testid}"]`).hostNodes();
