75183aa3fda22639491aa87a2292df85bc25918c
Jared Stoffan <jstoffan@box.com>
2019-04-02 19:09:43 -0700

    chore(versions): Add keypress support for non-button NavButtons

--- src/elements/common/nav-button/NavButton.js
+++ src/elements/common/nav-button/NavButton.js
@@ -8,6 +8,7 @@ import * as React from 'react';
 import classNames from 'classnames';
 import { Route } from 'react-router-dom';
 import PlainButton from '../../../components/plain-button';
+import { KEYS } from '../../../constants';
 
 type Props = {
     activeClassName?: string,
@@ -16,46 +17,74 @@ type Props = {
     component?: React.ComponentType<any>,
     exact?: boolean,
     onClick?: (event: SyntheticEvent<>) => void,
+    onKeyPress?: (event: SyntheticEvent<>) => void,
     replace?: boolean,
     strict?: boolean,
     to: string | Location,
 };
 
-const NavButton = ({
-    activeClassName = 'bdl-is-active',
-    children,
-    className = 'bdl-NavButton',
-    component: Component = PlainButton,
-    exact,
-    onClick,
-    replace,
-    strict,
-    to,
-    ...rest
-}: Props) => {
+const isClickEvent = event => {
+    return event.button === 0 && !event.altKey && !event.ctrlKey && !event.metaKey && !event.shiftKey;
+};
+
+const isEnterEvent = event => {
+    return event.key === KEYS.enter;
+};
+
+const NavButton = React.forwardRef<Props, React.Ref<any>>((props: Props, ref: React.Ref<any>) => {
+    const {
+        activeClassName = 'bdl-is-active',
+        children,
+        className = 'bdl-NavButton',
+        component: Component = PlainButton,
+        exact,
+        onClick,
+        onKeyPress,
+        replace,
+        strict,
+        to,
+        ...rest
+    } = props;
     const path = typeof to === 'object' ? to.pathname : to;
 
     return (
         <Route exact={exact} path={path} strict={strict}>
-            {({ history, match }) => (
-                <Component
-                    className={classNames(className, { [activeClassName]: !!match })}
-                    onClick={event => {
-                        const method = replace ? history.replace : history.push;
+            {({ history, match }) => {
+                const navigate = () => {
+                    const method = replace ? history.replace : history.push;
+                    return method(path);
+                };
 
-                        if (onClick) {
-                            onClick(event);
-                        }
+                return (
+                    <Component
+                        className={classNames(className, { [activeClassName]: !!match })}
+                        onClick={event => {
+                            if (onClick) {
+                                onClick(event);
+                            }
 
-                        method(path);
-                    }}
-                    {...rest}
-                >
-                    {children}
-                </Component>
-            )}
+                            if (!event.defaultPrevented && isClickEvent(event)) {
+                                navigate();
+                            }
+                        }}
+                        onKeyPress={event => {
+                            if (onKeyPress) {
+                                onKeyPress(event);
+                            }
+
+                            if (!event.defaultPrevented && isEnterEvent(event)) {
+                                navigate();
+                            }
+                        }}
+                        ref={ref}
+                        {...rest}
+                    >
+                        {children}
+                    </Component>
+                );
+            }}
         </Route>
     );
-};
+});
 
 export default NavButton;
--- src/elements/common/nav-button/__tests__/NavButton-test.js
+++ src/elements/common/nav-button/__tests__/NavButton-test.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { render } from 'enzyme';
-import { MemoryRouter } from 'react-router-dom';
+import { mount, render } from 'enzyme';
+import { MemoryRouter, Router } from 'react-router-dom';
 import NavButton from '..';
 
 describe('elements/common/nav-button/NavButton', () => {
@@ -171,6 +171,120 @@ describe('elements/common/nav-button/NavButton', () => {
             );
 
             expect(button.hasClass('bdl-is-selected')).toBe(false);
+        });
+    });
+
+    describe('onClick', () => {
+        const mockHistory = {
+            listen: jest.fn(),
+            location: {},
+            push: jest.fn(),
+        };
+
+        it('calls onClick eventhandler and history.push', () => {
+            const clickHandler = jest.fn();
+
+            const button = mount(
+                <Router history={mockHistory}>
+                    <NavButton to="/activity/test" onClick={clickHandler}>
+                        Activity Test
+                    </NavButton>
+                </Router>,
+            );
+
+            button.simulate('click', {
+                button: 0,
+            });
+
+            expect(clickHandler).toBeCalledTimes(1);
+            expect(mockHistory.push).toBeCalledTimes(1);
+            expect(mockHistory.push).toBeCalledWith('/activity/test');
+        });
+
+        it('does not call history.push on right click', () => {
+            const button = mount(
+                <Router history={mockHistory}>
+                    <NavButton to="/activity/test">Activity Test</NavButton>
+                </Router>,
+            );
+
+            button.simulate('click', {
+                button: 1,
+            });
+
+            expect(mockHistory.push).toBeCalledTimes(0);
+        });
+
+        it('does not call history.push on prevented event', () => {
+            const button = mount(
+                <Router history={mockHistory}>
+                    <NavButton to="/activity/test">Activity Test</NavButton>
+                </Router>,
+            );
+
+            button.simulate('click', {
+                defaultPrevented: true,
+                button: 0,
+            });
+
+            expect(mockHistory.push).toBeCalledTimes(0);
+        });
+    });
+
+    describe('onKeyPress', () => {
+        const mockHistory = {
+            listen: jest.fn(),
+            location: {},
+            push: jest.fn(),
+        };
+
+        it('calls onKeyPress eventhandler and history.push', () => {
+            const keyPressHandler = jest.fn();
+
+            const button = mount(
+                <Router history={mockHistory}>
+                    <NavButton to="/activity/test" onKeyPress={keyPressHandler}>
+                        Activity Test
+                    </NavButton>
+                </Router>,
+            );
+
+            button.simulate('keyPress', {
+                key: 'Enter',
+            });
+
+            expect(keyPressHandler).toBeCalledTimes(1);
+            expect(mockHistory.push).toBeCalledTimes(1);
+            expect(mockHistory.push).toBeCalledWith('/activity/test');
+        });
+
+        it('does not call history.push on non-enter keypress', () => {
+            const button = mount(
+                <Router history={mockHistory}>
+                    <NavButton to="/activity/test">Activity Test</NavButton>
+                </Router>,
+            );
+
+            button.simulate('keyPress', {
+                key: 'Ctrl',
+            });
+
+            expect(mockHistory.push).toBeCalledTimes(0);
+        });
+
+        it('does not call history.push on prevented event', () => {
+            const button = mount(
+                <Router history={mockHistory}>
+                    <NavButton to="/activity/test">Activity Test</NavButton>
+                </Router>,
+            );
+
+            button.simulate('keyPress', {
+                defaultPrevented: true,
+                key: 'Enter',
+            });
+
+            expect(mockHistory.push).toBeCalledTimes(0);
         });
     });
 });
