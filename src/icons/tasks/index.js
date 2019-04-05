778f8b39836f35f4245f6aaa9b37b16421582e04
Shahzad <saziz@box.com>
2019-04-05 11:13:15 -0700

    feat(tasks): Add general and approval tasks icons

---
+++ src/icons/tasks/index.js
@@ -0,0 +1,4 @@
+// @flow
+export { default as IconTaskGeneral } from './IconTaskGeneral';
+export { default as IconTaskApproval } from './IconTaskApproval';
+export { default as IconTask } from '../general/IconTask';
