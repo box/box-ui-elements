f88aef514922924d91c7612f97c2eaed26fd6d1e
Jared Stoffan <jstoffan@box.com>
2019-04-09 17:13:42 -0700

    build(release): Disable auto-comments on released pull requests

--- release.config.js
+++ release.config.js
@@ -10,5 +10,9 @@ module.exports = {
             },
         ],
         '@semantic-release/github',
+        {
+            failComment: false,
+            successComment: false,
+        },
     ],
 };
