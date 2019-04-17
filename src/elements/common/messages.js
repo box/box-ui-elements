ce074ac68c78a66ca02dfb20ff4620d1768e2f69
Matt Nguyen <mattnguyen1@yahoo.com>
2019-04-17 16:40:55 -0700

    feat(tasks): Handle errors when performing actions on tasks

--- src/elements/common/messages.js
+++ src/elements/common/messages.js
@@ -778,6 +778,26 @@ const messages = defineMessages({
         description: 'Error message when a task edit fails',
         defaultMessage: 'There was an error updating this task.',
     },
+    taskActionErrorTitle: {
+        id: 'be.taskActionErrorTitle',
+        description: 'Title shown when an error occurs performing an action on a task',
+        defaultMessage: 'Error',
+    },
+    taskApproveErrorMessage: {
+        id: 'be.taskApproveErrorMessage',
+        description: 'Error message when approving a task fails',
+        defaultMessage: 'An error has occurred while approving this task. Please refresh the page and try again.',
+    },
+    taskCompleteErrorMessage: {
+        id: 'be.taskCompleteErrorMessage',
+        description: 'Error message when completing a task fails',
+        defaultMessage: 'An error has occurred while completing this task. Please refresh the page and try again.',
+    },
+    taskRejectErrorMessage: {
+        id: 'be.taskRejectErrorMessage',
+        description: 'Error message when rejecting a task fails',
+        defaultMessage: 'An error has occurred while rejecting this task. Please refresh the page and try again.',
+    },
     taskDeleteErrorMessage: {
         id: 'be.taskDeleteErrorMessage',
         description: 'Error message when a task deletion fails',
