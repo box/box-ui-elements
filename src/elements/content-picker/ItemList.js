bbd7dc7e3dec097bcc03107ade022df96ce388d9
Jeremy Press <jpress@box.com>
2019-04-24 15:28:30 -0700

    chore(pickers): Only use taller rows when picker isSmall

--- src/elements/content-picker/ItemList.js
+++ src/elements/content-picker/ItemList.js
@@ -120,7 +120,7 @@ const ItemList = ({
                             height={height}
                             disableHeader
                             headerHeight={0}
-                            rowHeight={65}
+                            rowHeight={isSmall ? 70 : 50}
                             rowCount={rowCount}
                             rowGetter={({ index }) => items[index]}
                             ref={tableRef}
