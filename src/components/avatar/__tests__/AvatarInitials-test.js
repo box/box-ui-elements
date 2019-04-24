a7af6a9889f2fd53a4f6252f07840c207ed28916
Alex Krolick <akrolick@box.com>
2019-04-23 18:33:12 -0700

    fix(avatar): swap coral out for gray, sort hues

--- src/components/avatar/__tests__/AvatarInitials-test.js
+++ src/components/avatar/__tests__/AvatarInitials-test.js
@@ -15,11 +15,11 @@ describe('components/avatar/AvatarInitials', () => {
 
     test('should set a backgroundColor based on id', () => {
         const wrapper = shallow(<AvatarInitials id="10" name="hello world" />);
-        expect(wrapper.prop('style').backgroundColor).toEqual('#003c84');
+        expect(wrapper.prop('style').backgroundColor).toEqual('#0061d5');
     });
 
     test('should set a default backgroundColor if no id is passed in', () => {
         const wrapper = shallow(<AvatarInitials name="hello world" />);
-        expect(wrapper.prop('style').backgroundColor).toEqual('#0061d5');
+        expect(wrapper.prop('style').backgroundColor).toEqual('#2486fc');
     });
 });
