4ceb93bdbdec6d7abd55841e7c2d416862867353
Jared Stoffan <jstoffan@box.com>
2019-04-29 17:29:25 -0700

    build(release): Allow chore(i18n) commits to trigger new patch versions

--- release.config.js
+++ release.config.js
@@ -1,8 +1,14 @@
 module.exports = {
     branches: ['+([1-9])?(.{+([1-9]),x}).x', 'release', { name: 'master', prerelease: 'beta' }],
     plugins: [
-        '@semantic-release/commit-analyzer',
         '@semantic-release/release-notes-generator',
+        [
+            '@semantic-release/commit-analyzer',
+            {
+                preset: 'angular',
+                releaseRules: [{ type: 'chore', scope: 'i18n', release: 'patch' }],
+            },
+        ],
         [
             '@semantic-release/npm',
             {
