f1f1074ff0b04016632f35013e2b56f4c6123a30
Jeff Tan <jtan@box.com>
2019-05-03 09:54:46 -0700

    refactor(avatar): Refactor Avatar to use hooks

--- src/components/avatar/Avatar.js
+++ src/components/avatar/Avatar.js
@@ -1,5 +1,5 @@
 // @flow
-import * as React from 'react';
+import React, { useState, useEffect } from 'react';
 import classNames from 'classnames';
 import AvatarImage from './AvatarImage';
 import AvatarInitials from './AvatarInitials';
@@ -30,50 +30,38 @@ type Props = {
     size?: $Keys<typeof SIZES>,
 };
 
-type State = {
-    /** boolean to determine if image did not load correctly */
-    hasImageErrored: boolean,
-};
+function Avatar({ avatarUrl, className, name, id, size = '' }: Props) {
+    const [hasImageErrored, setHasImageErrored] = useState(false);
+    const classes = classNames(['avatar', className, { [`avatar--${size}`]: SIZES[size] }]);
 
-class Avatar extends React.PureComponent<Props, State> {
-    state = {
-        hasImageErrored: false,
-    };
-
-    componentWillReceiveProps(nextProps: Props) {
-        if (this.state.hasImageErrored && this.props.avatarUrl !== nextProps.avatarUrl) {
-            this.setState({
-                hasImageErrored: false,
-            });
+    // Reset hasImageErrored state when avatarUrl changes
+    useEffect(() => {
+        if (hasImageErrored) {
+            setHasImageErrored(false);
         }
-    }
+    }, [avatarUrl]);
 
-    onImageError = () => {
-        this.setState({
-            hasImageErrored: true,
-        });
-    };
-
-    render() {
-        const { avatarUrl, className, name, id, size = '' }: Props = this.props;
-        const { hasImageErrored }: State = this.state;
-        const classes = classNames(['avatar', className, { [`avatar--${size}`]: SIZES[size] }]);
-
-        let avatar;
-        if (avatarUrl && !hasImageErrored) {
-            avatar = <AvatarImage onError={this.onImageError} url={avatarUrl} />;
-        } else if (name) {
-            avatar = <AvatarInitials id={id} name={name} />;
-        } else {
-            avatar = <UnknownUserAvatar className="avatar-icon" />;
-        }
-
-        return (
-            <span className={classes} role="presentation">
-                {avatar}
-            </span>
+    let avatar;
+    if (avatarUrl && !hasImageErrored) {
+        avatar = (
+            <AvatarImage
+                onError={() => {
+                    setHasImageErrored(true);
+                }}
+                url={avatarUrl}
+            />
         );
+    } else if (name) {
+        avatar = <AvatarInitials id={id} name={name} />;
+    } else {
+        avatar = <UnknownUserAvatar className="avatar-icon" />;
     }
+
+    return (
+        <span className={classes} role="presentation">
+            {avatar}
+        </span>
+    );
 }
 
 export default Avatar;
