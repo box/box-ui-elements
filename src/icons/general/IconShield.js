e0dc69b9b11b236a689c92e34b82f93c9fe1110a
Dennis Yang <dyang@box.com>
2019-06-03 15:13:09 -0700

    feat(icon-shield): Address feedback

--- src/icons/general/IconShield.js
+++ src/icons/general/IconShield.js
@@ -15,38 +15,32 @@ type Props = {
     width?: number,
 };
 
-const IconShield = ({ className, color = boxBlue, height = 62, opacity = 0.2, width = 53, title }: Props) => (
+const IconShield = ({ className, color = boxBlue, height = 32, opacity = 0.2, width = 32, title }: Props) => (
     <AccessibleSVG
         className={classNames('bdl-IconShield', className)}
         height={height}
         title={title}
-        viewBox="0 0 53 62"
+        viewBox="0 0 32 32"
         width={width}
     >
-        <g fill="none" fillRule="evenodd">
-            <path
-                fill={white}
-                stroke={color}
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M1 5.615S13.75 12.923 26.5 1C39.25 12.923 52 5.615 52 5.615v40.962L26.5 61 1 46.577V5.615z"
-            />
-            <path
-                className="fill-color"
-                fill={color}
-                fillOpacity={opacity}
-                d="M26 7c-10.5 9.538-21 3.692-21 3.692v32.77L26 55V7z"
-            />
-            <path
-                className="stroke-color"
-                stroke={color}
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M44 19v20"
-            />
-        </g>
+        <path
+            className="stroke-color"
+            fill={white}
+            stroke={color}
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M3 3.308S9.5 6.962 16 1c6.5 5.962 13 2.308 13 2.308v20.48L16 31 3 23.788V3.308z"
+        />
+        <path className="fill-color" fill={color} fillOpacity={opacity} d="M16 5C10.5 8.5 6 7 6 7v14.712L16 27V5z" />
+        <path
+            className="stroke-color"
+            stroke={color}
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M25 10v11"
+        />
     </AccessibleSVG>
 );
 
