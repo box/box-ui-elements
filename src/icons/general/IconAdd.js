0334b79b83c390fc593e9e75d2f963f3ec88d8f5
Samuel Park <sampark@box.com>
2019-06-12 10:54:00 -0700

    fix(icons): updating default icon color and removing add icon whitespace

--- src/icons/general/IconAdd.js
+++ src/icons/general/IconAdd.js
@@ -12,11 +12,11 @@ type Props = {
     width?: number,
 };
 
-const IconAdd = ({ className = '', color = '#000000', height = 24, title, width = 24 }: Props) => (
-    <AccessibleSVG className={`icon-add ${className}`} height={height} title={title} viewBox="0 0 16 16" width={width}>
+const IconAdd = ({ className = '', color = '#222', height = 24, title, width = 24 }: Props) => (
+    <AccessibleSVG className={`icon-add ${className}`} height={height} title={title} viewBox="0 0 12 12" width={width}>
         <path
             className="fill-color"
-            d="M9,7 L13.5,7 C13.7761424,7 14,7.22385763 14,7.5 L14,8.5 C14,8.77614237 13.7761424,9 13.5,9 L9,9 L9,13.5 C9,13.7761424 8.77614237,14 8.5,14 L7.5,14 C7.22385763,14 7,13.7761424 7,13.5 L7,9 L2.5,9 C2.22385763,9 2,8.77614237 2,8.5 L2,7.5 C2,7.22385763 2.22385763,7 2.5,7 L7,7 L7,2.5 C7,2.22385763 7.22385763,2 7.5,2 L8.5,2 C8.77614237,2 9,2.22385763 9,2.5 L9,7 Z"
+            d="M7,5 L11.5,5 C11.7761424,5 12,5.22385763 12,5.5 L12,6.5 C12,6.77614237 11.7761424,7 11.5,7 L7,7 L7,11.5 C7,11.7761424 6.77614237,12 6.5,12 L5.5,12 C5.22385763,12 5,11.7761424 5,11.5 L5,7 L0.5,7 C0.22385763,7 0,6.77614237 0,6.5 L0,5.5 C0,5.22385763 0.22385763,5 0.5,5 L5,5 L5,0.5 C5,0.22385763 5.22385763,0 5.5,0 L6.5,0 C6.77614237,0 7,0.22385763 7,0.5 L7,5 Z"
             fill={color}
             fillRule="evenodd"
         />
