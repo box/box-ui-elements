0a41abd87d5ccdc378be67844066564283f12dc5
Samuel Park <sampark@box.com>
2019-06-11 18:05:08 -0700

    fix(icons): updating add icon style and adding hidden eye icon

--- src/icons/general/IconAdd.js
+++ src/icons/general/IconAdd.js
@@ -13,17 +13,10 @@ type Props = {
 };
 
 const IconAdd = ({ className = '', color = '#000000', height = 24, title, width = 24 }: Props) => (
-    <AccessibleSVG
-        className={`icon-add ${className}`}
-        height={height}
-        title={title}
-        viewBox="-603 389 16 16"
-        width={width}
-    >
+    <AccessibleSVG className={`icon-add ${className}`} height={height} title={title} viewBox="0 0 16 16" width={width}>
         <path
             className="fill-color"
-            d="M-593 395.5v-4c0-.8-.7-1.5-1.5-1.5s-1.5.7-1.5 1.5v4h-4c-.8 0-1.5.7-1.5 1.5s.7
-                    1.5 1.5 1.5h4v4c0 .8.7 1.5 1.5 1.5s1.5-.7 1.5-1.5v-4h4c.8 0 1.5-.7 1.5-1.5s-.7-1.5-1.5-1.5h-4z"
+            d="M9,7 L13.5,7 C13.7761424,7 14,7.22385763 14,7.5 L14,8.5 C14,8.77614237 13.7761424,9 13.5,9 L9,9 L9,13.5 C9,13.7761424 8.77614237,14 8.5,14 L7.5,14 C7.22385763,14 7,13.7761424 7,13.5 L7,9 L2.5,9 C2.22385763,9 2,8.77614237 2,8.5 L2,7.5 C2,7.22385763 2.22385763,7 2.5,7 L7,7 L7,2.5 C7,2.22385763 7.22385763,2 7.5,2 L8.5,2 C8.77614237,2 9,2.22385763 9,2.5 L9,7 Z"
             fill={color}
             fillRule="evenodd"
         />
