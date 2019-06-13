ba1bb916e25b2910f7746df666b4d352b7e3a748
Shahzad <saziz@box.com>
2019-06-13 13:19:16 -0700

    fix: Add tests for task form

--- src/elements/content-sidebar/activity-feed/task-form/__tests__/TaskForm-test.js
+++ src/elements/content-sidebar/activity-feed/task-form/__tests__/TaskForm-test.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { mount } from 'enzyme';
 import DatePicker from '../../../../../components/date-picker/DatePicker'; // eslint-disable-line no-unused-vars
+import { TASK_EDIT_MODE_EDIT } from '../../../../../constants';
 
 import { TaskFormUnwrapped as TaskForm } from '..';
 
@@ -55,6 +56,37 @@ describe('components/ContentSidebar/ActivityFeed/task-form/TaskForm', () => {
         });
 
         expect(createTaskSpy).toHaveBeenCalled();
+    });
+
+    test('should call editTask prop on submit when form is in edit mode', () => {
+        const editTaskMock = jest.fn();
+        const id = '1';
+        const wrapper = render({
+            id,
+            editTask: editTaskMock,
+            editMode: TASK_EDIT_MODE_EDIT,
+        });
+        const instance = wrapper.instance();
+        const description = 'hey';
+
+        // Set form state to reflect updated data
+        wrapper.setState({
+            message: description,
+            isValid: true,
+        });
+
+        const submitButton = wrapper.find('[data-testid="task-form-submit-button"]').hostNodes();
+        submitButton.simulate('submit', {
+            target: {
+                checkValidity: () => true, // not implemented in JSDOM
+            },
+        });
+
+        expect(editTaskMock).toHaveBeenCalledWith(
+            { id, description, due_at: null },
+            instance.handleSubmitSuccess,
+            instance.handleSubmitError,
+        );
     });
 
     test('should call onCancel handler when cancel button is clicked', async () => {
@@ -123,13 +155,17 @@ describe('components/ContentSidebar/ActivityFeed/task-form/TaskForm', () => {
         test('should set the approval date to be one millisecond before midnight of the next day', async () => {
             // Midnight on December 3rd GMT
             const date = new Date('2018-12-03T00:00:00');
+            const validateFormMock = jest.fn();
             // 11:59:59:999 on December 3rd GMT
             const lastMillisecondOfDate = new Date('2018-12-03T23:59:59.999');
             const wrapper = render({});
 
+            wrapper.instance().validateForm = validateFormMock;
+
             wrapper.instance().handleDueDateChange(date);
 
             expect(wrapper.state('dueDate')).toEqual(lastMillisecondOfDate);
+            expect(validateFormMock).toHaveBeenCalled();
         });
 
         test('should change a previously set approval date to null if there is no approval date', () => {
