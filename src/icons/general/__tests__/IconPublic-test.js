bfa075a7f3d99d1b66807cd5caed6e756d08f9e2
Samuel Park <sam.h.park@gmail.com>
2019-06-19 14:21:07 -0700

    fix(icons): update retry and public icon (#1297)
    * fix(icons): updating retry icon and adding public icon
    
    * fix(icons): lint fix
    
    * fix(icons): add fill-color class to iconRetry

--- src/icons/general/__tests__/IconPublic-test.js
+++ src/icons/general/__tests__/IconPublic-test.js
@@ -1,25 +1,25 @@
 import React from 'react';
 
-import IconRetry from '../IconRetry';
+import IconPublic from '../IconPublic';
 
-describe('icons/general/IconRetry', () => {
+describe('icons/general/IconPublic', () => {
     test('should correctly render default icon', () => {
-        const wrapper = shallow(<IconRetry />);
+        const wrapper = shallow(<IconPublic />);
 
-        expect(wrapper.hasClass('bdl-IconRetry')).toEqual(true);
+        expect(wrapper.hasClass('bdl-IconPublic')).toEqual(true);
     });
 
     test('should correctly render icon with specified color', () => {
         const color = '#ffffff';
-        const wrapper = shallow(<IconRetry color={color} />);
+        const wrapper = shallow(<IconPublic color={color} />);
 
-        expect(wrapper.find('g').prop('fill')).toEqual(color);
+        expect(wrapper.find('path').prop('stroke')).toEqual(color);
     });
 
     test('should correctly render icon with specified width and height', () => {
         const width = 16;
         const height = 17;
-        const wrapper = shallow(<IconRetry height={height} width={width} />);
+        const wrapper = shallow(<IconPublic height={height} width={width} />);
 
         expect(wrapper.find('AccessibleSVG').prop('width')).toEqual(width);
         expect(wrapper.find('AccessibleSVG').prop('height')).toEqual(height);
@@ -27,7 +27,7 @@ describe('icons/general/IconRetry', () => {
 
     test('should correctly render icon with title', () => {
         const title = 'fool';
-        const wrapper = shallow(<IconRetry title={title} />);
+        const wrapper = shallow(<IconPublic title={title} />);
 
         expect(wrapper.find('AccessibleSVG').prop('title')).toEqual(title);
     });
