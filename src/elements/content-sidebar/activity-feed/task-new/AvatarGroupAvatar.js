0f810e4c51c5f8cc82c8c9cc594e54d474646916
Alex Krolick <alexkrolick@users.noreply.github.com>
2019-06-20 11:01:18 -0700

    refactor(task-new): componentize task assignee css (#1308)
    * refactor(task-new): componentize task assignee css
    
    * fix(task-new): revert to custom avatar size

--- src/elements/content-sidebar/activity-feed/task-new/AvatarGroupAvatar.js
+++ src/elements/content-sidebar/activity-feed/task-new/AvatarGroupAvatar.js
@@ -9,6 +9,8 @@ import Avatar from '../Avatar';
 import { TASK_NEW_APPROVED, TASK_NEW_REJECTED, TASK_NEW_COMPLETED, TASK_NEW_NOT_STARTED } from '../../../../constants';
 import messages from '../../../common/messages';
 
+import './AvatarGroupAvatar.scss';
+
 type Props = {
     className?: string,
     getAvatarUrl?: GetAvatarUrlCallback,
@@ -31,14 +33,14 @@ const StatusIcon = ({ status, ...rest }: { status: TaskCollabStatus }) => {
 
 const AvatarGroupAvatar = React.memo<Props>(({ user, status, getAvatarUrl, className, ...rest }: Props) => (
     <div
-        className={classNames('bcs-AvatarGroup-avatarContainer', className)}
+        className={classNames('bcs-AvatarGroupAvatar', className)}
         data-testid="avatar-group-avatar-container"
         {...rest}
     >
-        <Avatar className="bcs-AvatarGroup-avatar" user={user} getAvatarUrl={getAvatarUrl} />
+        <Avatar className="bcs-AvatarGroupAvatar-avatar" user={user} getAvatarUrl={getAvatarUrl} />
         <StatusIcon
             status={status}
-            className={`bcs-AvatarGroup-statusIcon ${camelCase(status)}`}
+            className={`bcs-AvatarGroupAvatar-statusIcon ${camelCase(status)}`}
             height={12}
             width={12}
             title={<FormattedMessage {...messages.completedAssignment} />}
