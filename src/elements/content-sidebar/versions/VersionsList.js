e399896ad5441cd254e8c2927536b0a65f4bda9e
Jared Stoffan <jstoffan@box.com>
2019-06-24 17:10:50 -0700

    feat(versions): Add version group headers based on relative date

--- src/elements/content-sidebar/versions/VersionsList.js
+++ src/elements/content-sidebar/versions/VersionsList.js
@@ -6,50 +6,32 @@
 
 import React from 'react';
 import { Route } from 'react-router-dom';
-import { FormattedMessage } from 'react-intl';
-import messages from './messages';
 import VersionsItem from './VersionsItem';
-import type { VersionActionCallback } from './Versions';
 import './VersionsList.scss';
 
 type Props = {
+    currentId: string,
     fileId: string,
-    isLoading: boolean,
-    onDelete: VersionActionCallback,
-    onDownload: VersionActionCallback,
-    onPreview: VersionActionCallback,
-    onPromote: VersionActionCallback,
-    onRestore: VersionActionCallback,
     versions: Array<BoxItemVersion>,
 };
 
-const VersionsList = ({ isLoading, versions, ...rest }: Props) => {
-    if (!isLoading && !versions.length) {
-        return (
-            <div className="bcs-VersionsList bcs-is-empty">
-                <FormattedMessage {...messages.versionsEmpty} />
-            </div>
-        );
-    }
-
-    return (
-        <ul className="bcs-VersionsList">
-            {versions.map((version, index) => (
-                <li className="bcs-VersionsList-item" key={version.id}>
-                    <Route
-                        render={({ match }) => (
-                            <VersionsItem
-                                isCurrent={index === 0}
-                                isSelected={match.params.versionId === version.id}
-                                version={version}
-                                {...rest}
-                            />
-                        )}
-                    />
-                </li>
-            ))}
-        </ul>
-    );
-};
+const VersionsList = ({ currentId, versions, ...rest }: Props) => (
+    <ul className="bcs-VersionsList">
+        {versions.map(version => (
+            <li className="bcs-VersionsList-item" key={version.id}>
+                <Route
+                    render={({ match }) => (
+                        <VersionsItem
+                            isCurrent={currentId === version.id}
+                            isSelected={match.params.versionId === version.id}
+                            version={version}
+                            {...rest}
+                        />
+                    )}
+                />
+            </li>
+        ))}
+    </ul>
+);
 
 export default VersionsList;
--- src/elements/content-sidebar/versions/VersionsSidebar.js
+++ src/elements/content-sidebar/versions/VersionsSidebar.js
@@ -10,42 +10,71 @@ import InlineError from '../../../components/inline-error';
 import messages from './messages';
 import messagesCommon from '../../common/messages';
 import SidebarContent from '../SidebarContent';
-import VersionsList from './VersionsList';
+import VersionsGroup, { getGroup } from './VersionsGroup';
 import { BackButton } from '../../common/nav-button';
 import { LoadingIndicatorWrapper } from '../../../components/loading-indicator';
-import type { VersionActionCallback } from './Versions';
 import './VersionsSidebar.scss';
 
 type Props = {
     error?: string,
     fileId: string,
     isLoading: boolean,
-    onDelete: VersionActionCallback,
-    onDownload: VersionActionCallback,
-    onPreview: VersionActionCallback,
-    onPromote: VersionActionCallback,
-    onRestore: VersionActionCallback,
     parentName: string,
     versions: Array<BoxItemVersion>,
 };
 
-const VersionsSidebar = ({ error, isLoading, parentName, ...rest }: Props) => (
-    <SidebarContent
-        className="bcs-Versions"
-        data-resin-component="preview"
-        data-resin-feature="versions"
-        title={
-            <React.Fragment>
-                <BackButton data-resin-target="back" to={`/${parentName}`} />
-                <FormattedMessage {...messages.versionsTitle} />
-            </React.Fragment>
-        }
-    >
-        <LoadingIndicatorWrapper className="bcs-Versions-content" crawlerPosition="top" isLoading={isLoading}>
-            {error && <InlineError title={<FormattedMessage {...messagesCommon.error} />}>{error}</InlineError>}
-            <VersionsList isLoading={isLoading} {...rest} />
-        </LoadingIndicatorWrapper>
-    </SidebarContent>
-);
+const VersionsSidebar = ({ error, isLoading, fileId, parentName, versions, ...rest }: Props) => {
+    const { id: currentId } = versions[0] || {};
+    const showVersions = !!versions.length;
+    const showEmpty = !isLoading && !showVersions;
+    const versionGroups = versions.reduce((groups, version) => {
+        const versionGroup = getGroup(version);
+
+        groups[versionGroup] = groups[versionGroup] || [];
+        groups[versionGroup].push(version);
+
+        return groups;
+    }, {});
+
+    return (
+        <SidebarContent
+            className="bcs-Versions"
+            data-resin-component="preview"
+            data-resin-feature="versions"
+            title={
+                <React.Fragment>
+                    <BackButton data-resin-target="back" to={`/${parentName}`} />
+                    <FormattedMessage {...messages.versionsTitle} />
+                </React.Fragment>
+            }
+        >
+            <LoadingIndicatorWrapper className="bcs-Versions-content" crawlerPosition="top" isLoading={isLoading}>
+                {error && <InlineError title={<FormattedMessage {...messagesCommon.error} />}>{error}</InlineError>}
+
+                {showEmpty && (
+                    <div className="bcs-Versions-empty">
+                        <FormattedMessage {...messages.versionsEmpty} />
+                    </div>
+                )}
+
+                {showVersions && (
+                    <ul className="bcs-Versions-menu">
+                        {Object.keys(versionGroups).map(versionGroupKey => (
+                            <li className="bcs-Versions-menu-item" key={versionGroupKey}>
+                                <VersionsGroup
+                                    currentId={currentId}
+                                    fileId={fileId}
+                                    versionGroup={versionGroupKey}
+                                    versions={versionGroups[versionGroupKey]}
+                                    {...rest}
+                                />
+                            </li>
+                        ))}
+                    </ul>
+                )}
+            </LoadingIndicatorWrapper>
+        </SidebarContent>
+    );
+};
 
 export default VersionsSidebar;
