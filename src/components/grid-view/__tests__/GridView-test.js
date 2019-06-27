2efb91a826504a3bf2308f1ce5355391e0b6ae53
rickstefanik <rstefanik@box.com>
2019-06-27 15:13:56 -0700

    fix: added snapshot tests for GridView

---
+++ src/components/grid-view/__tests__/GridView-test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import GridView from '../GridView';
+
+describe('components/grid-view/GridView', () => {
+    test('should render()', () => {
+        const collection = { items: [{ type: 'folder', id: '001', name: 'Example Folder' }] };
+        const wrapper = shallow(
+            <GridView
+                columnCount={5}
+                count={10}
+                currentCollection={collection}
+                height={600}
+                onItemClick={() => {}}
+                onItemSelect={() => {}}
+                slotRenderer={index => <div> {index} </div>}
+                width={400}
+            />,
+        );
+        expect(wrapper).toMatchSnapshot();
+    });
+});
