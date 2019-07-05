97a1d44ae6c6f376c8ffb4ff5895d8ea687a91b8
Richard Stefanik <43506937+rickstefanik@users.noreply.github.com>
2019-07-05 13:37:14 -0700

    feat(content-explorer): Add toggle button for grid view (#1349)
    * feat: added toggle between list and grid view
    
    * fix: added data-testid to ViewModeChangeButton

--- src/elements/common/sub-header/SubHeaderRight.js
+++ src/elements/common/sub-header/SubHeaderRight.js
@@ -7,7 +7,10 @@
 import React from 'react';
 import Sort from './Sort';
 import Add from './Add';
+import ViewModeChangeButton from './ViewModeChangeButton';
+import { FeatureFlag } from '../feature-checking';
 import { VIEW_FOLDER } from '../../../constants';
+import type { ViewMode } from '../flowTypes';
 import './SubHeaderRight.scss';
 
 type Props = {
@@ -17,25 +20,35 @@ type Props = {
     onCreate: Function,
     onSortChange: Function,
     onUpload: Function,
+    onViewModeChange?: (viewMode: ViewMode) => void,
     view: View,
+    viewMode: ViewMode,
 };
 
 const SubHeaderRight = ({
-    view,
-    onUpload,
-    onCreate,
-    canUpload,
     canCreateNewFolder,
+    canUpload,
     currentCollection,
+    onCreate,
+    onViewModeChange,
     onSortChange,
+    onUpload,
+    view,
+    viewMode,
 }: Props) => {
     const { sortBy, sortDirection, items = [] }: Collection = currentCollection;
+    const hasItems: boolean = items.length > 0;
     const isFolder: boolean = view === VIEW_FOLDER;
-    const showSort: boolean = isFolder && items.length > 0;
+    const showSort: boolean = isFolder && hasItems;
     const showAdd: boolean = (!!canUpload || !!canCreateNewFolder) && isFolder;
+    const showGridButton: boolean = isFolder && hasItems;
 
     return (
         <div className="be-sub-header-right">
+            <FeatureFlag feature="contentExplorer.gridView.enabled">
+                {showGridButton && <ViewModeChangeButton viewMode={viewMode} onViewModeChange={onViewModeChange} />}
+            </FeatureFlag>
+
             {showSort && !!sortBy && !!sortDirection && (
                 <Sort onSortChange={onSortChange} sortBy={sortBy} sortDirection={sortDirection} />
             )}
