d853240ce375ba80642ce9e1169a45a039072a32
Stephanie A. Higa <shiga@box.com>
2019-07-08 14:08:28 -0700

    feat(icons): Add sandbox and puzzle piece icons

--- src/icons/general/IconPuzzlePieceCircle.js
+++ src/icons/general/IconPuzzlePieceCircle.js
@@ -7,22 +7,25 @@ import { bdlBoxBlue } from '../../styles/variables';
 type Props = {
     className?: string,
     color?: string,
-    dimension?: number,
+    height?: number,
     title?: string | React.Element<any>,
+    width?: number,
 };
 
-const IconPuzzlePieceCircle = ({ className = '', color = bdlBoxBlue, dimension = 28, title }: Props) => (
+const IconPuzzlePieceCircle = ({ className = '', color = bdlBoxBlue, height = 32, title, width = 32 }: Props) => (
     <AccessibleSVG
         className={classNames('bdl-IconPuzzlePieceCircle', className)}
-        height={dimension}
-        viewBox="0 0 28 28"
+        height={height}
+        viewBox="0 0 32 32"
         title={title}
-        width={dimension}
+        width={width}
     >
-        <g fill="none" fillRule="evenodd" stroke={color}>
-            <rect height="27" rx="13.5" width="27" x=".5" y=".5" />
-            <path d="M8.4 10.695v3.028c.17.85.646.933 1.153.804.312-.375.78-.615 1.306-.615a1.703 1.703 0 0 1 0 3.405c-.525 0-.994-.24-1.306-.615-.507-.129-.983-.046-1.153.804v3.027h9.838v-3.027c.17-.85.646-.933 1.153-.804.312.375.78.615 1.306.615a1.703 1.703 0 0 0 0-3.405c-.526 0-.994.24-1.306.615-.507.129-.983.046-1.153-.804v-3.028H15.21c-.851-.17-.933-.645-.804-1.152a1.703 1.703 0 1 0-2.79-1.307c0 .526.24.994.614 1.307.13.507.047.982-.804 1.152H8.4z" />
-        </g>
+        <rect fill="none" height="27" rx="13.5" stroke={color} width="27" x=".5" y=".5" />
+        <path
+            fill="none"
+            stroke={color}
+            d="M8.4 10.695v3.028c.17.85.646.933 1.153.804.312-.375.78-.615 1.306-.615a1.703 1.703 0 0 1 0 3.405c-.525 0-.994-.24-1.306-.615-.507-.129-.983-.046-1.153.804v3.027h9.838v-3.027c.17-.85.646-.933 1.153-.804.312.375.78.615 1.306.615a1.703 1.703 0 0 0 0-3.405c-.526 0-.994.24-1.306.615-.507.129-.983.046-1.153-.804v-3.028H15.21c-.851-.17-.933-.645-.804-1.152a1.703 1.703 0 1 0-2.79-1.307c0 .526.24.994.614 1.307.13.507.047.982-.804 1.152H8.4z"
+        />
     </AccessibleSVG>
 );
 
