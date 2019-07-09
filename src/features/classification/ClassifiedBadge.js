fa82a8dc736d35ae22f39d1479fb46e2177f03c4
Juan Pablo Franco <juan.pablo.franco@gmail.com>
2019-07-09 16:14:20 -0700

    fix(classification): Classification badge UX tweaks (#1378)
    * feat(classification): Classification badge UX tweaks
    
    * fix: Capitalized classify message
    
    * fix: Fixed snapshot
    
    * chore: Updated properties file
    
    * chore: Removed tag icon from classification

--- src/features/classification/ClassifiedBadge.js
+++ src/features/classification/ClassifiedBadge.js
@@ -2,8 +2,6 @@
 import * as React from 'react';
 
 import Tooltip from '../../components/tooltip';
-import IconAddTags from '../../icons/general/IconAddTags';
-import { bdlYellorange } from '../../styles/variables';
 import type { Position } from '../../components/tooltip';
 import './ClassifiedBadge.scss';
 
@@ -16,7 +14,6 @@ type Props = {
 const ClassifiedBadge = ({ name, tooltipPosition = 'bottom-center', tooltipText }: Props) => (
     <Tooltip isDisabled={!tooltipText} position={tooltipPosition} text={tooltipText}>
         <h1 className="bdl-ClassifiedBadge">
-            <IconAddTags color={bdlYellorange} height={10} width={10} strokeWidth={3} />
             <span className="bdl-ClassifiedBadge-name">{name}</span>
         </h1>
     </Tooltip>
