0b52ffe9f6fe8322338236aa3c83d48cf4c91a5f
Juan Pablo Franco <jfranco@box.com>
2019-07-09 14:39:12 -0700

    chore: Removed tag icon from classification

--- src/features/classification/AddClassificationBadge.js
+++ src/features/classification/AddClassificationBadge.js
@@ -2,14 +2,11 @@
 import * as React from 'react';
 import { FormattedMessage } from 'react-intl';
 
-import IconAddTags from '../../icons/general/IconAddTags';
-import { bdlGray50 } from '../../styles/variables';
 import messages from './messages';
 import './AddClassificationBadge.scss';
 
 const AddClassificationBadge = () => (
     <h1 className="bdl-AddClassificationBadge">
-        <IconAddTags color={bdlGray50} height={10} width={10} strokeWidth={3} />
         <FormattedMessage {...messages.addClassification}>
             {value => <span className="bdl-AddClassificationBadge-name">{value}</span>}
         </FormattedMessage>
