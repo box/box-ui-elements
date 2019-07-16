c0a26d743c28150fe537cbad2cc175a2ed7e6aec
Priyajeet Hora <priyajeet@box.com>
2019-07-16 10:54:44 -0700

    fix: ii333

--- scripts/commitlint.sh
+++ scripts/commitlint.sh
@@ -7,5 +7,4 @@ BASE_SHA1=$(echo "${PULL_REQUEST_DETAILS}" | jq -r .base.sha)
 HEAD_SHA1=$(echo "${PULL_REQUEST_DETAILS}" | jq -r .head.sha)
 COMMIT_RANGE="${BASE_SHA1}...${HEAD_SHA1}"
 echo "${COMMIT_RANGE}"
-git log
 git log ${COMMIT_RANGE} --pretty=%B | ./node_modules/.bin/commitlint
