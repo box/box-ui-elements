aa588c79001c10490f8747d8efd4f2fe738e8bba
Priyajeet Hora <priyajeet@box.com>
2019-07-16 10:57:27 -0700

    fix: iii

--- scripts/commitlint.sh
+++ scripts/commitlint.sh
@@ -3,8 +3,6 @@ set -o errexit
 
 PULL_REQUEST_NUMBER=$(echo "${CIRCLE_PULL_REQUEST}" | cut -d/ -f7)
 PULL_REQUEST_DETAILS=$(curl https://api.github.com/repos/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}/pulls/${PULL_REQUEST_NUMBER})
-BASE_SHA1=$(echo "${PULL_REQUEST_DETAILS}" | jq -r .base.sha)
 HEAD_SHA1=$(echo "${PULL_REQUEST_DETAILS}" | jq -r .head.sha)
-COMMIT_RANGE="${BASE_SHA1}...${HEAD_SHA1}"
-echo "${COMMIT_RANGE}"
+COMMIT_RANGE=origin/master...${HEAD_SHA1}
 git log ${COMMIT_RANGE} --pretty=%B | ./node_modules/.bin/commitlint
