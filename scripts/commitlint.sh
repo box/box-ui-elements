590e5e7dee78fdd1f5ad5ea0410ead42bb1278a0
Priyajeet Hora <priyajeet@box.com>
2019-07-18 22:28:54 -0700

    chore: use node instead of jq

--- scripts/commitlint.sh
+++ scripts/commitlint.sh
@@ -7,9 +7,12 @@ set -o errexit
 if [ -n "${CIRCLE_PULL_REQUEST}" ]
 then
   PULL_REQUEST_DETAILS=$(curl https://api.github.com/repos/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}/pulls/${CIRCLE_PR_NUMBER})
-  BASE_SHA1=$(echo "${PULL_REQUEST_DETAILS}" | jq -r .base.sha)
-  HEAD_SHA1=$(echo "${PULL_REQUEST_DETAILS}" | jq -r .head.sha)
+  echo $PULL_REQUEST_DETAILS
+  BASE_SHA1=$(node -pe 'JSON.parse(process.argv[1]).base.sha' ${PULL_REQUEST_DETAILS})
+  echo $BASE_SHA1
+  HEAD_SHA1=$(node -pe 'JSON.parse(process.argv[1]).head.sha' ${PULL_REQUEST_DETAILS})
+  echo $HEAD_SHA1
   COMMIT_RANGE="${BASE_SHA1}...${HEAD_SHA1}"
-  echo "${COMMIT_RANGE}"
+  echo ${COMMIT_RANGE}
   git log ${COMMIT_RANGE} --pretty=%B | ./node_modules/.bin/commitlint
 fi
