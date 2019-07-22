e5e1e020725382504ac50481a6ad8b33156eb1d7
Alex Krolick <alexkrolick@users.noreply.github.com>
2019-07-22 13:43:21 -0700

    chore(tasks): delete old task ui from activity feed (#1437)
    * chore(tasks): remove old tasks ui
    
    * chore(tasks): remove refs to legacy task feed api

--- src/elements/content-sidebar/activity-feed/activity-feed/ActiveState.js
+++ src/elements/content-sidebar/activity-feed/activity-feed/ActiveState.js
@@ -4,10 +4,8 @@
  */
 import * as React from 'react';
 import getProp from 'lodash/get';
-import { FeatureFlag } from '../../../common/feature-checking';
 import AppActivity from '../app-activity';
 import Comment from '../comment';
-import Task from '../task';
 import TaskNew from '../task-new';
 import Version, { CollapsedVersion } from '../version';
 import Keywords from '../keywords';
@@ -74,56 +72,23 @@ const ActiveState = ({
                     );
                 case 'task':
                     return (
-                        <FeatureFlag
-                            key={type + id}
-                            feature="activityFeed.tasks.newCards"
-                            disabled={() => {
-                                const hasAssignments =
-                                    item.task_assignment_collection && item.task_assignment_collection.total_count;
-                                return (
-                                    hasAssignments && (
-                                        <li className="bcs-activity-feed-task" data-testid="task">
-                                            <Task
-                                                {...item}
-                                                currentUser={currentUser}
-                                                getAvatarUrl={getAvatarUrl}
-                                                getMentionWithQuery={getMentionWithQuery}
-                                                getUserProfileUrl={getUserProfileUrl}
-                                                mentionSelectorContacts={mentionSelectorContacts}
-                                                onAssignmentUpdate={onTaskAssignmentUpdate}
-                                                onDelete={onTaskDelete}
-                                                onEdit={onTaskEdit}
-                                                // permissions are not part of task API so hard code to true
-                                                permissions={{
-                                                    can_delete: true,
-                                                    can_edit: true,
-                                                }}
-                                                translations={translations}
-                                            />
-                                        </li>
-                                    )
-                                );
-                            }}
-                            enabled={() => (
-                                <li className="bcs-activity-feed-task-new" data-testid="task">
-                                    <TaskNew
-                                        {...item}
-                                        approverSelectorContacts={approverSelectorContacts}
-                                        currentUser={currentUser}
-                                        getApproverWithQuery={getApproverWithQuery}
-                                        getAvatarUrl={getAvatarUrl}
-                                        getMentionWithQuery={getMentionWithQuery}
-                                        getUserProfileUrl={getUserProfileUrl}
-                                        mentionSelectorContacts={mentionSelectorContacts}
-                                        onAssignmentUpdate={onTaskAssignmentUpdate}
-                                        onDelete={onTaskDelete}
-                                        onEdit={onTaskEdit}
-                                        onModalClose={onTaskModalClose}
-                                        translations={translations}
-                                    />
-                                </li>
-                            )}
-                        />
+                        <li key={type + id} className="bcs-activity-feed-task-new" data-testid="task">
+                            <TaskNew
+                                {...item}
+                                approverSelectorContacts={approverSelectorContacts}
+                                currentUser={currentUser}
+                                getApproverWithQuery={getApproverWithQuery}
+                                getAvatarUrl={getAvatarUrl}
+                                getMentionWithQuery={getMentionWithQuery}
+                                getUserProfileUrl={getUserProfileUrl}
+                                mentionSelectorContacts={mentionSelectorContacts}
+                                onAssignmentUpdate={onTaskAssignmentUpdate}
+                                onDelete={onTaskDelete}
+                                onEdit={onTaskEdit}
+                                onModalClose={onTaskModalClose}
+                                translations={translations}
+                            />
+                        </li>
                     );
                 case 'file_version':
                     return (
