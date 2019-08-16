66f7ef8601e6e27c9c6e999b1bf1f19efbd04f01
Shahzad <saziz@box.com>
2019-08-15 17:45:29 -0700

    fix: default icon color

--- src/icons/collections/IconCollectionsStarFilled.js
+++ src/icons/collections/IconCollectionsStarFilled.js
@@ -3,7 +3,7 @@ import * as React from 'react';
 import classNames from 'classnames';
 
 import AccessibleSVG from '../accessible-svg';
-import { bdlNeutral03 } from '../../styles/variables';
+import { bdlGray } from '../../styles/variables';
 
 type Props = {
     className?: string,
@@ -14,7 +14,7 @@ type Props = {
     width?: number,
 };
 
-const IconCollectionsStarFilled = ({ className = '', color = bdlNeutral03, height = 32, title, width = 32 }: Props) => (
+const IconCollectionsStarFilled = ({ className = '', color = bdlGray, height = 32, title, width = 32 }: Props) => (
     <AccessibleSVG
         className={classNames('bdl-IconCollectionsStarFilled', className)}
         height={height}
