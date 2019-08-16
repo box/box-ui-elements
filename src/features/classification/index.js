e9869591cb23531d93ced8e5de1c72ca29c0738d
Feixuan Li <fli@box.com>
2019-08-15 18:53:36 -0700

    feat(classification): remove classify from breadcrumbs

--- src/features/classification/index.js
+++ src/features/classification/index.js
@@ -1,7 +1,6 @@
 // @flow
 
 export { default } from './Classification';
-export { default as AddClassificationBadge } from './AddClassificationBadge';
 export { default as ClassifiedBadge } from './ClassifiedBadge';
 export { default as classificationMessages } from './messages';
 export { default as EditClassificationButton } from './EditClassificationButton';
