a814657ed7e4a2b3390cda83fd324cede79a5ab0
Alex Krolick <alexkrolick@users.noreply.github.com>
2019-08-28 14:03:56 -0700

    fix(inlinenotice): update to box design standard (#1576)
    * fix(inlinenotice): accessible contrast ratio
    
    Updates the InlineNotice component to comply with the the Box Design Language, including changing the text color to an accessible one
    
    * feat(inlinenotice): add title prop to all inline notice types
    
    * refactor(inlinenotice): prevent issue where falsy values could render
    
    * refactor(inlinenotice): strong instead of b tag
    
    * refactor(inlinenotice): renders/strongs
    
    * refactor(inlinenotice): externalize snapshots
    
    * refactor(inlinenotice): pull up color definition
    
    * feat(inlinenotice): add generic type

--- src/components/inline-notice/__tests__/InlineNotice-test.js
+++ src/components/inline-notice/__tests__/InlineNotice-test.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { render } from 'enzyme';
 
 import InlineNotice from '..';
 
@@ -11,6 +12,21 @@ describe('components/inline-notice/InlineNotice', () => {
         expect(wrapper.hasClass('inline-alert')).toBe(true);
         expect(wrapper.hasClass('inline-alert-visible')).toBe(true);
         expect(wrapper.text()).toEqual(children);
+    });
+
+    test('should render with title prop if provided', () => {
+        const children = 'this is a message to put in the notice';
+        const title = 'this is the title';
+
+        const wrapper = shallow(<InlineNotice title={title}>{children}</InlineNotice>);
+
+        expect(wrapper.find('strong').text()).toEqual(title);
+        expect(
+            wrapper
+                .children()
+                .find('div')
+                .text(),
+        ).toEqual(children);
     });
 
     test('should have a default type', () => {
@@ -35,5 +51,19 @@ describe('components/inline-notice/InlineNotice', () => {
         const wrapper = shallow(<InlineNotice className="testClass">{children}</InlineNotice>);
 
         expect(wrapper.hasClass('testClass')).toBe(true);
+    });
+
+    test('should pass other props to div', () => {
+        const label = 'an error occured';
+        const role = 'alert';
+
+        const wrapper = render(
+            <InlineNotice role={role} aria-label={label}>
+                a message
+            </InlineNotice>,
+        );
+
+        expect(wrapper.prop('role')).toBe(role);
+        expect(wrapper.prop('aria-label')).toBe(label);
     });
 });
