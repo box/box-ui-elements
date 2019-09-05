781a24696e1b8acad4861fe35cfdfaf4bd7d287e
Chris Wei <chrisz.wei@gmail.com>
2019-09-05 14:13:12 -0700

    feat(avatarinitials): move backgroundColor into scss (#1585)
    * feat(avatar): allow style override for avatarinitials
    
    * feat(avatarintials): move backgroundcolor of initials into scss
    
    * feat(avatarintials): use data attribute to determine backgroundColor
    
    * feat(avatarinitials): moved avatar colors into _theme.scss
    
    * feat(avatarinitials): parse sass lists when compiling constants files
    
    * feat(avatarinitials): remove unused sass variable
    
    * build(buildstylevars): readded line
    
    * style(avatar): readded removed line

--- src/components/avatar/__tests__/AvatarInitials-test.js
+++ src/components/avatar/__tests__/AvatarInitials-test.js
@@ -13,13 +13,13 @@ describe('components/avatar/AvatarInitials', () => {
         expect(wrapper.text()).toEqual('HW');
     });
 
-    test('should set a backgroundColor based on id', () => {
+    test('should set data-bg-idx attribute based on id', () => {
         const wrapper = shallow(<AvatarInitials id="10" name="hello world" />);
-        expect(wrapper.prop('style').backgroundColor).toEqual('#0061d5');
+        expect(wrapper.prop('data-bg-idx')).toEqual(1);
     });
 
-    test('should set a default backgroundColor if no id is passed in', () => {
+    test('should set a default data-bg-idx attribute if no id is passed in', () => {
         const wrapper = shallow(<AvatarInitials name="hello world" />);
-        expect(wrapper.prop('style').backgroundColor).toEqual('#2486fc');
+        expect(wrapper.prop('data-bg-idx')).toEqual(0);
     });
 });
