e1b75420ada3d3ad3a1532d2a7cd4c478b67fdfb
Stephanie A. Higa <shiga@box.com>
2019-09-06 18:12:16 -0700

    feat(metadata): Enable metadata template filtering

--- src/features/metadata-instance-editor/TemplatedInstance.js
+++ src/features/metadata-instance-editor/TemplatedInstance.js
@@ -26,32 +26,30 @@ const TemplatedInstance = ({ canEdit, data = {}, errors, onFieldChange, onFieldR
     return (
         <>
             {hasVisibleFields &&
-                fields.map(field => {
-                    return (
-                        <Field
-                            key={field.id}
-                            canEdit={canEdit}
-                            dataKey={field.key}
-                            dataValue={data[field.key]}
-                            description={field.description}
-                            displayName={field.displayName}
-                            error={errors[field.key]}
-                            isHidden={isHidden(field)} // Checking both isHidden and hidden attributes due to differences in V2 and V3 APIs
-                            onChange={(key: string, value: MetadataFieldValue) => {
-                                if (canEdit && onFieldChange) {
-                                    onFieldChange(key, value, field.type);
-                                }
-                            }}
-                            onRemove={(key: string) => {
-                                if (canEdit && onFieldRemove) {
-                                    onFieldRemove(key);
-                                }
-                            }}
-                            options={field.options}
-                            type={field.type}
-                        />
-                    );
-                })}
+                fields.map(field => (
+                    <Field
+                        key={field.id}
+                        canEdit={canEdit}
+                        dataKey={field.key}
+                        dataValue={data[field.key]}
+                        description={field.description}
+                        displayName={field.displayName}
+                        error={errors[field.key]}
+                        isHidden={isHidden(field)} // Checking both isHidden and hidden attributes due to differences in V2 and V3 APIs
+                        onChange={(key: string, value: MetadataFieldValue) => {
+                            if (canEdit && onFieldChange) {
+                                onFieldChange(key, value, field.type);
+                            }
+                        }}
+                        onRemove={(key: string) => {
+                            if (canEdit && onFieldRemove) {
+                                onFieldRemove(key);
+                            }
+                        }}
+                        options={field.options}
+                        type={field.type}
+                    />
+                ))}
             {showHiddenFieldsMessage && (
                 <div className="attributes-hidden-message">
                     <FormattedMessage {...messages.allAttributesAreHidden} />
