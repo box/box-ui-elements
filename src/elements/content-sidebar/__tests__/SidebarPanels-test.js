b716cd736cccbdbc066bd55f0939c950994179d6
Jared Stoffan <jstoffan@users.noreply.github.com>
2019-09-27 19:28:38 -0700

    chore(sidebar): Add unit tests for sidebar panel routing logic (#1650)

--- src/elements/content-sidebar/__tests__/SidebarPanels-test.js
+++ src/elements/content-sidebar/__tests__/SidebarPanels-test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { mount, shallow } from 'enzyme/build';
+import { mount } from 'enzyme/build';
 import { MemoryRouter } from 'react-router-dom';
 import SidebarPanels from '../SidebarPanels';
 
@@ -27,43 +27,73 @@ jest.mock('../SidebarUtils', () => ({
 }));
 
 describe('elements/content-sidebar/SidebarPanels', () => {
-    const getWrapper = props => shallow(<SidebarPanels file={{ id: '' }} isOpen {...props} />);
+    const getWrapper = ({ path = '/', ...rest } = {}) =>
+        mount(
+            <MemoryRouter initialEntries={[path]} keyLength={0}>
+                <SidebarPanels
+                    file={{ id: '1234' }}
+                    hasActivity
+                    hasDetails
+                    hasMetadata
+                    hasSkills
+                    hasVersions
+                    isOpen
+                    {...rest}
+                />
+            </MemoryRouter>,
+        );
 
     describe('render', () => {
-        test('should render no sidebar', () => {
-            const wrapper = getWrapper();
-            expect(wrapper).toMatchSnapshot();
+        test.each`
+            path                         | sidebar
+            ${'/activity'}               | ${'ActivitySidebar'}
+            ${'/activity/comments'}      | ${'ActivitySidebar'}
+            ${'/activity/comments/1234'} | ${'ActivitySidebar'}
+            ${'/activity/tasks'}         | ${'ActivitySidebar'}
+            ${'/activity/tasks/1234'}    | ${'ActivitySidebar'}
+            ${'/activity/versions'}      | ${'VersionsSidebar'}
+            ${'/activity/versions/1234'} | ${'VersionsSidebar'}
+            ${'/details'}                | ${'DetailsSidebar'}
+            ${'/details/versions'}       | ${'VersionsSidebar'}
+            ${'/details/versions/1234'}  | ${'VersionsSidebar'}
+            ${'/metadata'}               | ${'MetadataSidebar'}
+            ${'/skills'}                 | ${'SkillsSidebar'}
+            ${'/nonsense'}               | ${'SkillsSidebar'}
+            ${'/'}                       | ${'SkillsSidebar'}
+        `('should render $sidebar given the path $path', ({ path, sidebar }) => {
+            const wrapper = getWrapper({ path });
+            expect(wrapper.exists(sidebar)).toBe(true);
         });
 
-        describe('skills sidebar', () => {
-            test('should render default component', () => {
-                const wrapper = getWrapper({ hasSkills: true });
-                expect(wrapper).toMatchSnapshot();
+        test('should render nothing if the sidebar is closed', () => {
+            const wrapper = getWrapper({
+                isOpen: false,
             });
+            expect(wrapper.isEmptyRender()).toBe(true);
+        });
+
+        test('should render nothing if all sidebars are disabled', () => {
+            const wrapper = getWrapper({
+                hasActivity: false,
+                hasDetails: false,
+                hasMetadata: false,
+                hasSkills: false,
+                hasVersions: false,
+            });
+            expect(wrapper.isEmptyRender()).toBe(true);
         });
 
         describe('activity sidebar', () => {
-            test('should render default component', () => {
-                const wrapper = getWrapper({ hasActivity: true });
-                expect(wrapper).toMatchSnapshot();
-            });
             test('should render with tasks deeplink', () => {
-                const wrapper = mount(
-                    <MemoryRouter initialEntries={['/activity/tasks/12345']} initialIndex={0}>
-                        <SidebarPanels hasActivity file={{ id: '1234' }} isOpen />
-                    </MemoryRouter>,
-                );
+                const wrapper = getWrapper({ path: '/activity/tasks/12345' });
                 expect(wrapper.find('ActivitySidebar').props()).toMatchObject({
                     activeFeedEntryType: 'task',
                     activeFeedEntryId: '12345',
                 });
             });
+
             test('should render with comments deeplink', () => {
-                const wrapper = mount(
-                    <MemoryRouter initialEntries={['/activity/comments/12345']} initialIndex={0}>
-                        <SidebarPanels hasActivity file={{ id: '1234' }} isOpen />
-                    </MemoryRouter>,
-                );
+                const wrapper = getWrapper({ path: '/activity/comments/12345' });
                 expect(wrapper.find('ActivitySidebar').props()).toMatchObject({
                     activeFeedEntryType: 'comment',
                     activeFeedEntryId: '12345',
@@ -71,11 +101,7 @@ describe('elements/content-sidebar/SidebarPanels', () => {
             });
 
             test('should render with versions deeplink', () => {
-                const wrapper = mount(
-                    <MemoryRouter initialEntries={['/activity/versions/12345']} initialIndex={0}>
-                        <SidebarPanels hasActivity file={{ id: '1234' }} isOpen hasVersions />
-                    </MemoryRouter>,
-                );
+                const wrapper = getWrapper({ path: '/activity/versions/12345' });
                 expect(wrapper.find('VersionsSidebar').props()).toMatchObject({
                     versionId: '12345',
                 });
@@ -83,31 +109,11 @@ describe('elements/content-sidebar/SidebarPanels', () => {
         });
 
         describe('details sidebar', () => {
-            test('should render default component', () => {
-                const wrapper = getWrapper({ hasDetails: true });
-                expect(wrapper).toMatchSnapshot();
-            });
-
-            test('should render versions', () => {
-                const wrapper = getWrapper({ hasVersions: true });
-                expect(wrapper).toMatchSnapshot();
-            });
             test('should render with versions deeplink', () => {
-                const wrapper = mount(
-                    <MemoryRouter initialEntries={['/details/versions/12345']} initialIndex={0}>
-                        <SidebarPanels hasDetails file={{ id: '1234' }} isOpen hasVersions />
-                    </MemoryRouter>,
-                );
+                const wrapper = getWrapper({ path: '/details/versions/12345' });
                 expect(wrapper.find('VersionsSidebar').props()).toMatchObject({
                     versionId: '12345',
                 });
-            });
-        });
-
-        describe('metadata sidebar', () => {
-            test('should render default component', () => {
-                const wrapper = getWrapper({ hasMetadata: true });
-                expect(wrapper).toMatchSnapshot();
             });
         });
     });
