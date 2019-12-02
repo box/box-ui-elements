c454e3f522e319b1e944d2661e54e969685ec2ad
Tim Gupta <tjgupta@gmail.com>
2019-12-02 13:28:31 -0800

    refactor(types): port metadata types to explicit imports

--- src/features/metadata-instance-editor/constants.js
+++ src/features/metadata-instance-editor/constants.js
@@ -7,3 +7,7 @@ export const FIELD_TYPE_FLOAT: 'float' = 'float';
 export const FIELD_TYPE_MULTISELECT: 'multiSelect' = 'multiSelect';
 export const FIELD_TYPE_STRING: 'string' = 'string';
 export const TEMPLATE_CUSTOM_PROPERTIES: 'properties' = 'properties';
+export const JSON_PATCH_OP_ADD: 'add' = 'add';
+export const JSON_PATCH_OP_REMOVE: 'remove' = 'remove';
+export const JSON_PATCH_OP_REPLACE: 'replace' = 'replace';
+export const JSON_PATCH_OP_TEST: 'test' = 'test';
