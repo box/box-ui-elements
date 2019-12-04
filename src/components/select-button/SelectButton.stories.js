2b69693942cd3b131cb100dc1a5a1eacc8de7cc0
Stephanie A. Higa <shiga@box.com>
2019-12-04 15:18:03 -0800

    feat(storybook): Create Storybook stories for several components

--- src/components/select-button/SelectButton.stories.js
+++ src/components/select-button/SelectButton.stories.js
@@ -5,11 +5,23 @@ import { boolean } from '@storybook/addon-knobs';
 import SelectButton from './SelectButton';
 import notes from './SelectButton.stories.md';
 
-export const regular = () => <SelectButton isDisabled={boolean('isDisabled', false)}>Click Here</SelectButton>;
+export const regular = () => (
+    <SelectButton className="" isDisabled={boolean('isDisabled', false)}>
+        Click Here
+    </SelectButton>
+);
 
-export const disabled = () => <SelectButton isDisabled>Click Here</SelectButton>;
+export const disabled = () => (
+    <SelectButton className="" isDisabled>
+        Click Here
+    </SelectButton>
+);
 
-export const withError = () => <SelectButton error="Error text">Click Here</SelectButton>;
+export const withError = () => (
+    <SelectButton className="" error="Error text" isDisabled={false}>
+        Click Here
+    </SelectButton>
+);
 
 export default {
     title: 'Components|SelectButton',
