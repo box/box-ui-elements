4ba8e707d487a20f4a89ba5e50b4bf0e72e75079
Stephanie A. Higa <shiga@box.com>
2019-12-03 16:18:04 -0800

    feat(storybook): Create Storybook stories for several components

---
+++ jest-puppeteer.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    launch: {
+        args: ['--disable-lcd-text'],
+    },
+};
--- src/components/select-button/__tests__/SelectButton.stories.test.js
+++ src/components/select-button/__tests__/SelectButton.stories.test.js
@@ -1,9 +1,10 @@
 describe('components/select-button/SelectButton', () => {
-    test.each([['components-selectbutton--regular'], ['components-selectbutton--disabled']])(
-        'looks visually correct when using story %s',
-        async id => {
-            const image = await takeScreenshot(id);
-            expect(image).toMatchImageSnapshot();
-        },
-    );
+    test.each([
+        ['components-selectbutton--regular'],
+        ['components-selectbutton--disabled'],
+        ['components-selectbutton--with-error'],
+    ])('looks visually correct when using story %s', async id => {
+        const image = await takeScreenshot(id);
+        expect(image).toMatchImageSnapshot();
+    });
 });
