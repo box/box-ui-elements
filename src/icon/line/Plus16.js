a51aafc17965d5788a336448d9ceed91e8596a52
Andrew Holloway <booc0mtaco@gmail.com>
2019-12-30 10:18:25 -0800

    docs(assets): add auto-generated documentation to icons/illustrations
    This includes link to issue ticket creation to aid in future improvements

--- src/icon/line/Plus16.js
+++ src/icon/line/Plus16.js
@@ -4,6 +4,17 @@ import * as React from 'react';
 import AccessibleSVG from '../../icons/accessible-svg';
 import type { Icon } from '../../icons/flowTypes';
 
+/**
+ * This is an auto-generated component and should not be edited
+ * manually in contributor pull requests.
+ *
+ * If you have problems with this component:
+ * - https://github.com/box/box-ui-elements/issues/new?template=Bug_report.md
+ *
+ * If there are missing features in this component:
+ * - https://github.com/box/box-ui-elements/issues/new?template=Feature_request.md
+ */
+
 const Plus16 = (props: Icon) => (
     <AccessibleSVG width={16} height={16} viewBox="0 0 16 16" {...props}>
         <path fill="#222" fillRule="evenodd" d="M8.5 2v5.5H14v1H8.5V14h-1V8.5H2v-1h5.5V2z" />
