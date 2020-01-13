8547da2f4421066daae4bb6072369219e40825d7
Alex Laughnan <alaughnan@box.com>
2020-01-13 11:13:59 -0800

    feat(storybook): Include all states in test

--- src/components/avatar/__tests__/Avatar.stories.test.js
+++ src/components/avatar/__tests__/Avatar.stories.test.js
@@ -3,6 +3,7 @@ describe('components/avatar/Avatar', () => {
         ['components-avatar--regular'],
         ['components-avatar--with-avatar-url'],
         ['components-avatar--with-multiple-avatars'],
+        ['components-avatar--without-name-or-initials'],
     ])('looks visually correct when using story %s', async id => {
         const image = await takeScreenshot(id);
         return expect(image).toMatchImageSnapshot();
