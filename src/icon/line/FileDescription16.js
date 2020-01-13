6f885bf14fdfe379101ac7b3450c7a60c4e359c8
Andrew Holloway <booc0mtaco@gmail.com>
2020-01-13 11:28:17 -0800

    feat(assets): add new and update existing assets

--- src/icon/line/FileDescription16.js
+++ src/icon/line/FileDescription16.js
@@ -16,14 +16,14 @@ import type { Icon } from '../../icons/flowTypes';
  * - https://github.com/box/box-ui-elements/issues/new?template=Feature_request.md
  */
 
-const MetadataBadge16 = (props: Icon) => (
+const FileDescription16 = (props: Icon) => (
     <AccessibleSVG width={16} height={16} viewBox="0 0 16 16" {...props}>
         <path
             fill={vars.bdlGray}
             fillRule="evenodd"
-            d="M8 1a7 7 0 110 14A7 7 0 018 1zm0 1a6 6 0 100 12A6 6 0 008 2zm2.456 3.794l.03.085 1 4a.5.5 0 01-.941.328l-.03-.086-.727-2.903-1.372 2.06a.5.5 0 01-.772.073l-.06-.074-1.373-2.059-.726 2.903a.5.5 0 01-.517.378l-.09-.014a.5.5 0 01-.377-.517l.014-.09 1-4a.5.5 0 01.845-.226l.056.07L8 8.098l1.584-2.374a.5.5 0 01.872.07z"
+            d="M8.062 1a1.5 1.5 0 01.99.374l.112.109L11.488 4H13.5A1.5 1.5 0 0115 5.5v5a1.5 1.5 0 01-1.5 1.5H12v1.5a1.5 1.5 0 01-1.356 1.493L10.5 15h-8a1.5 1.5 0 01-1.493-1.356L1 13.5v-11a1.5 1.5 0 011.356-1.493L2.5 1h5.562zm0 1H2.5a.5.5 0 00-.492.41L2 2.5v11a.5.5 0 00.41.492L2.5 14h8a.5.5 0 00.492-.41L11 13.5V12H5.5A1.5 1.5 0 014 10.5v-5A1.5 1.5 0 015.5 4h4.628L8.43 2.16a.5.5 0 00-.285-.153L8.062 2zm2.918 3H5.5a.5.5 0 00-.5.5v5a.5.5 0 00.5.5h8a.5.5 0 00.5-.5v-5a.5.5 0 00-.5-.5H11v.141a.5.5 0 00-.02-.14zm.52 3.5a.5.5 0 110 1h-4a.5.5 0 010-1h4zm0-2a.5.5 0 110 1h-4a.5.5 0 010-1h4z"
         />
     </AccessibleSVG>
 );
 
-export default MetadataBadge16;
+export default FileDescription16;
