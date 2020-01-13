887a9efdfcb927f3d85056505ab1f94328e12501
Andrew Holloway <booc0mtaco@users.noreply.github.com>
2020-01-13 14:12:23 -0800

    feat(assets): add new and update existing assets (#1843)
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- src/icon/line/EmailAddress16.js
+++ src/icon/line/EmailAddress16.js
@@ -16,14 +16,14 @@ import type { Icon } from '../../icons/flowTypes';
  * - https://github.com/box/box-ui-elements/issues/new?template=Feature_request.md
  */
 
-const MetadataBadge16 = (props: Icon) => (
+const EmailAddress16 = (props: Icon) => (
     <AccessibleSVG width={16} height={16} viewBox="0 0 16 16" {...props}>
         <path
             fill={vars.bdlGray}
             fillRule="evenodd"
-            d="M8 1a7 7 0 110 14A7 7 0 018 1zm0 1a6 6 0 100 12A6 6 0 008 2zm2.456 3.794l.03.085 1 4a.5.5 0 01-.941.328l-.03-.086-.727-2.903-1.372 2.06a.5.5 0 01-.772.073l-.06-.074-1.373-2.059-.726 2.903a.5.5 0 01-.517.378l-.09-.014a.5.5 0 01-.377-.517l.014-.09 1-4a.5.5 0 01.845-.226l.056.07L8 8.098l1.584-2.374a.5.5 0 01.872.07z"
+            d="M8 1a7 7 0 016.996 6.76L15 8v.5a2.5 2.5 0 01-4.62 1.325 3 3 0 11-.38-4.06V5.5a.5.5 0 01.992-.09L11 5.5v3a1.5 1.5 0 002.993.144L14 8.5V8a6 6 0 10-2.787 5.068.5.5 0 01.536.844A7 7 0 118 1zm0 5a2 2 0 100 4 2 2 0 000-4z"
         />
     </AccessibleSVG>
 );
 
-export default MetadataBadge16;
+export default EmailAddress16;
