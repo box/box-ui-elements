148f72a5c198676cc22600d9e7f9122609971916
Alex Laughnan <alaughnan@box.com>
2020-01-12 16:25:23 -0800

    feat(storybook): Create stories for Avatar

---
+++ src/components/avatar/__tests__/Avatar.stories.test.js
@@ -0,0 +1,10 @@
+describe('components/avatar/Avatar', () => {
+    test.each([
+        ['components-avatar--regular'],
+        ['components-avatar--with-avatar-url'],
+        ['components-avatar--with-multiple-avatars'],
+    ])('looks visually correct when using story %s', async id => {
+        const image = await takeScreenshot(id);
+        return expect(image).toMatchImageSnapshot();
+    });
+});
