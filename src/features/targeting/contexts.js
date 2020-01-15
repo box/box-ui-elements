6ed67e3e08ffaca0a3741a6f87b2bfaba5d916a8
Albert Zhang <azhang@box.com>
2020-01-14 19:01:33 -0800

    feat(targeting): add types used for targeting

--- src/features/targeting/contexts.js
+++ src/features/targeting/contexts.js
@@ -5,6 +5,15 @@ import { type FtuxContextValue, type TargetingContextValue } from './types';
 
 const FtuxContext = createContext<FtuxContextValue>({});
 
-const TargetingContext = createContext<TargetingContextValue>({});
+const defaultTargetingContextValue = {
+    getTargetingApi: () => ({
+        shouldShow: () => false,
+        onSeen: () => {},
+        onClosed: () => {},
+    }),
+    setEligibleMessageHeaders: () => {},
+};
+
+const TargetingContext = createContext<TargetingContextValue>(defaultTargetingContextValue);
 
 export { FtuxContext, TargetingContext };
--- src/features/targeting/types.js
+++ src/features/targeting/types.js
@@ -1,4 +1,35 @@
 // @flow
+import type { SetEligibleMessageHeaders, TargetingApi } from './targeting-api';
+import { MESSAGE_TYPES } from './constants';
+
+export type MessageType = $Values<typeof MESSAGE_TYPES>;
+
+export type MessageID = number;
+
+export type MessageName = string;
+
+export type MessageHeader = {|
+    // an integer used to load message in getMessageRepresentation
+    canBeShownMultipleTimes: boolean,
+    // used to form messageIdentifier where namespace is 'contextual'
+    createdDate: number,
+    // whether it can be shown again
+    id: MessageID,
+    isCustomMessage: boolean,
+    // if it is custom message, custom message maker must exist to show
+    isExemptFromSuppression: boolean,
+    // is this message exempt from suppression
+    isMessageTypeSuppressed: boolean,
+    // is this message type suppressed for the user
+    name: MessageName,
+    priority: number,
+    shouldAutoShow: boolean,
+    type: MessageType,
+|};
+
 export type FtuxContextValue = {};
 
-export type TargetingContextValue = {};
+export type TargetingContextValue = {|
+    getTargetingApi: MessageName => TargetingApi,
+    setEligibleMessageHeaders: SetEligibleMessageHeaders,
+|};
