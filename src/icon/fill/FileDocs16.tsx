9fa91571789e756ae5ad7947c69ce7bf2901f38c
Priyajeet Hora <priyajeet@box.com>
2020-01-21 12:26:25 -0800

    chore(typescript): migrate generated icons

---
+++ src/icon/fill/FileDocs16.tsx
@@ -0,0 +1,26 @@
+/* eslint-disable react/jsx-sort-props */
+import * as React from 'react';
+import * as vars from '../../styles/variables';
+import AccessibleSVG, { SVGProps } from '../../components/accessible-svg/AccessibleSVG';
+
+/**
+ * This is an auto-generated component and should not be edited
+ * manually in contributor pull requests.
+ *
+ * If you have problems with this component:
+ * - https://github.com/box/box-ui-elements/issues/new?template=Bug_report.md
+ *
+ * If there are missing features in this component:
+ * - https://github.com/box/box-ui-elements/issues/new?template=Feature_request.md
+ */
+
+const FileDocs16 = (props: SVGProps) => (
+    <AccessibleSVG width={16} height={16} viewBox="0 0 16 16" {...props}>
+        <path
+            fill={vars.bdlGray50}
+            d="M9.422 1c.146 0 .286.057.389.158l3.528 3.454a.533.533 0 01.161.38v8.393c0 .892-.74 1.615-1.65 1.615h-7.7c-.911 0-1.65-.723-1.65-1.615V2.615C2.5 1.723 3.239 1 4.15 1h5.272zM8 11.5H5v.5h3v-.5zm2.5-1.5H5v.5h5.5V10zm0-1.5H5V9h5.5v-.5zm0-1.5H5v.5h5.5V7z"
+        />
+    </AccessibleSVG>
+);
+
+export default FileDocs16;
---
+++ src/icon/line/CollectionPlus16.stories.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+import CollectionPlus16 from './CollectionPlus16';
+
+export const collectionPlus16 = () => <CollectionPlus16 />;
+
+export default {
+    title: 'Icon|Line|CollectionPlus16',
+    component: CollectionPlus16,
+    parameters: {
+        notes: "`import CollectionPlus16 from 'box-ui-elements/es/icon/line/CollectionPlus16';`",
+    },
+};
