70d60b70c01c88f46725b21ba7e70efa6e4e7300
Stephanie A. Higa <shiga@box.com>
2020-02-05 10:00:36 -0800

    chore(css): Add backwards-compatible names to common components

--- src/utils/dom.js
+++ src/utils/dom.js
@@ -51,9 +51,8 @@ export function isFocusableElement(element: HTMLElement | EventTarget | null): b
 
     const isButton =
         element.classList.contains('btn-content') ||
-        (element.parentElement instanceof HTMLElement &&
-            (element.parentElement.classList.contains('btn') ||
-                element.parentElement.classList.contains('bdl-Button'))) ||
+        (element.parentElement instanceof HTMLElement && element.parentElement.classList.contains('btn')) ||
+        (element.parentElement instanceof HTMLElement && element.parentElement.classList.contains('bdl-Button')) ||
         false;
 
     return isInputElement(element) || tag === 'button' || tag === 'a' || tag === 'option' || isCheckbox || isButton;
