7dc87890a030d00a4804dd0595e8be479facd55f
Juan Pablo Franco <jfranco@box.com>
2020-02-21 19:15:24 -0800

    chore(radio-group): Add custom radio button story

--- src/components/radio/stories/RadioGroup.stories.tsx
+++ src/components/radio/stories/RadioGroup.stories.tsx
@@ -1,12 +1,13 @@
 import * as React from 'react';
 import { boolean } from '@storybook/addon-knobs';
 
-import RadioButton from '../RadioButton';
+import { bdlGray20, bdlPurpleRain } from '../../../styles/variables';
+import RadioButton, { RadioButtonProps } from '../RadioButton';
 
 import RadioGroup from '../RadioGroup';
 import notes from './RadioGroup.stories.md';
 
-export const example = () => (
+export const basic = () => (
     <RadioGroup name="radiogroup" value="radio3">
         <RadioButton label="Radio Button 1" value="radio1" description="I have a description" />
         <RadioButton label="Radio Button 2" value="radio2" description="I also have a description" />
@@ -15,6 +16,42 @@ export const example = () => (
         <RadioButton label="Disabled Radio Button" value="radio5" isDisabled={boolean('isDisabled', true)} />
     </RadioGroup>
 );
+
+export const withCustomRadioButtonComponent = () => {
+    const CustomRadioButton = ({ isSelected, name, value }: RadioButtonProps) => (
+        <span style={{ marginRight: '8px', position: 'relative' }}>
+            <input
+                checked={isSelected}
+                name={name}
+                type="radio"
+                value={value}
+                style={{ height: '20px', opacity: 0, width: '20px' }}
+            />
+            <span
+                style={{
+                    backgroundColor: isSelected ? bdlPurpleRain : bdlGray20,
+                    borderRadius: '50%',
+                    display: 'inline-block',
+                    height: '20px',
+                    left: '0px',
+                    position: 'absolute',
+                    width: '20px',
+                    zIndex: -1,
+                }}
+            />
+        </span>
+    );
+
+    return (
+        <RadioGroup name="customradiogroup" value="customRadio3">
+            <CustomRadioButton label="Radio Button 1" value="customRadio1" />
+            <CustomRadioButton label="Radio Button 2" value="customRadio2" />
+            <CustomRadioButton label="Radio Button 3" value="customRadio3" />
+            <CustomRadioButton label="Radio Button 4" value="customRadio4" />
+            <CustomRadioButton label="Radio Button 5" value="customRadio5" />
+        </RadioGroup>
+    );
+};
 
 export default {
     title: 'Components|Radio/RadioGroup',
