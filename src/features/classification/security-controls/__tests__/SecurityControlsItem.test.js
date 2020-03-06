3cc0ad12a0f70bc9160948c129a54a35280a284b
feixuanli <fli@box.com>
2020-03-05 16:37:43 -0800

    feat(classification): Remove shield icon from security controls (#2010)

--- src/features/classification/security-controls/__tests__/SecurityControlsItem.test.js
+++ src/features/classification/security-controls/__tests__/SecurityControlsItem.test.js
@@ -1,32 +1,21 @@
 import React from 'react';
-
 import SecurityControlsItem from '../SecurityControlsItem';
-import IconSecurityClassificationSolid from '../../../../icons/general/IconSecurityClassificationSolid';
 
 describe('features/classification/security-controls/SecurityControlsItem', () => {
     let wrapper;
     let message;
 
-    const getWrapper = (props = {}) => shallow(<SecurityControlsItem message={message} {...props} />);
+    const getWrapper = (props = {}) => shallow(<SecurityControlsItem {...props} />);
 
     beforeEach(() => {
         message = {
             id: 'msg1',
             defaultMessage: 'message',
         };
-        wrapper = getWrapper();
+        wrapper = getWrapper({ message });
     });
 
     test('should render a SecurityControlsItem with a message', () => {
-        wrapper = getWrapper({ controlsFormat: 'shortWithBtn' });
-
         expect(wrapper).toMatchSnapshot();
-        expect(wrapper.find(IconSecurityClassificationSolid)).toHaveLength(1);
-    });
-
-    test('should not render IconSecurityClassificationSolid if controlsFormat is FULL', () => {
-        wrapper = getWrapper({ controlsFormat: 'full' });
-
-        expect(wrapper.find(IconSecurityClassificationSolid)).toHaveLength(0);
     });
 });
