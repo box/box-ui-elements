84802d373dfd09cecf89c379eae6cb5dca8272f8
Stephanie A. Higa <shiga@box.com>
2020-03-09 14:39:55 -0700

    refactor(typescript): Migrate Checkbox and ErrorMask (#2003)
    * refactor(typescript): Migrate ErrorMask to TypeScript
    
    * feat(typescript): Convert Checkbox to TypeScript
    
    * refactor(typescript): Migrate Checkbox to TypeScript
    
    * refactor(typescript): Migrate Checkbox and ErrorMask
    
    * refactor(typescript): Migrate Checkbox and ErrorMask
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- src/components/index.ts
+++ src/components/index.ts
@@ -10,6 +10,12 @@ export * from './button';
 export { default as ButtonGroup } from './button-group';
 export * from './button-group';
 
+export { default as Checkbox } from './checkbox';
+export * from './checkbox';
+
+export { default as ErrorMask } from './error-mask';
+export * from './error-mask';
+
 export { default as Fieldset } from './fieldset';
 export * from './fieldset';
 
