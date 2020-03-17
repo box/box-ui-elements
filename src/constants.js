b1b8f8462d690bbb4c4220e69023eb48c8d88c4f
Mingze <mxiao@box.com>
2020-03-17 14:57:12 -0700

    chore(preview): Bump default preview version to 2.37.0 (#2030)

--- src/constants.js
+++ src/constants.js
@@ -156,7 +156,7 @@ export const DELIMITER_SLASH: 'slash' = 'slash';
 export const DELIMITER_CARET: 'caret' = 'caret';
 
 /* ---------------------- Defaults -------------------------- */
-export const DEFAULT_PREVIEW_VERSION = '2.33.1';
+export const DEFAULT_PREVIEW_VERSION = '2.37.0';
 export const DEFAULT_LOCALE = 'en-US';
 export const DEFAULT_PATH_STATIC = 'platform/elements';
 export const DEFAULT_PATH_STATIC_PREVIEW = 'platform/preview';
