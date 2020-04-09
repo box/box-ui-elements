3da6bdb661d75a2f49e6fcb7431aa5f81ccf984f
Conrad Chan <conradchan@box.com>
2020-04-09 13:04:46 -0700

    chore(annotations): fix imports

--- src/elements/common/annotator-context/index.ts
+++ src/elements/common/annotator-context/index.ts
@@ -1,9 +1,4 @@
 export { default as AnnotatorContext } from './AnnotatorContext';
-export {
-    default as withAnnotations,
-    WithAnnotationsProps,
-    WithAnnotationsComponent,
-    ComponentWithAnnotations,
-} from './withAnnotations';
-export { default as withAnnotatorContext, WithAnnotatorContextProps } from './withAnnotatorContext';
+export { default as withAnnotations } from './withAnnotations';
+export { default as withAnnotatorContext } from './withAnnotatorContext';
 export * from './types';
