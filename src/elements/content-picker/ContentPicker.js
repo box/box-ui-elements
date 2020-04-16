e75cd266bee48fa89edba45473c64a515fc5bb42
Stephanie A. Higa <sahiga@gmail.com>
2020-04-16 09:48:15 -0700

    fix(content-picker): Fix pagination and add tests

--- src/elements/content-picker/ContentPicker.js
+++ src/elements/content-picker/ContentPicker.js
@@ -17,7 +17,7 @@ import UploadDialog from '../common/upload-dialog';
 import CreateFolderDialog from '../common/create-folder-dialog';
 import Internationalize from '../common/Internationalize';
 import makeResponsive from '../common/makeResponsive';
-import Pagination from '../../features/pagination';
+import OffsetBasedPagination from '../../features/pagination/OffsetBasedPagination';
 import { isFocusableElement, isInputElement, focus } from '../../utils/dom';
 import API from '../../api';
 import Content from './Content';
@@ -1233,7 +1233,7 @@ class ContentPicker extends Component<Props, State> {
                             chooseButtonLabel={chooseButtonLabel}
                             cancelButtonLabel={cancelButtonLabel}
                         >
-                            <Pagination
+                            <OffsetBasedPagination
                                 offset={offset}
                                 onOffsetChange={this.paginate}
                                 pageSize={currentPageSize}
