4559fd48ee682faa345d6302ec2a7c4bb8776998
Samuel Park <sampark@box.com>
2020-05-06 15:54:35 -0700

    feat(collapsible-sidebar): add color knob to storybook

--- src/features/collapsible-sidebar/CollapsibleSidebar.stories.js
+++ src/features/collapsible-sidebar/CollapsibleSidebar.stories.js
@@ -2,7 +2,7 @@
 import * as React from 'react';
 
 import { ThemeProvider } from 'styled-components';
-import { boolean } from '@storybook/addon-knobs';
+import { boolean, color } from '@storybook/addon-knobs';
 import noop from 'lodash/noop';
 
 // eslint-disable-next-line import/named
@@ -21,9 +21,6 @@ import ClockBadge16 from '../../icon/fill/ClockBadge16';
 import Code16 from '../../icon/fill/Code16';
 import Trash16 from '../../icon/fill/Trash16';
 import FileDefault16 from '../../icon/fill/FileDefault16';
-
-const hexColor = '#0061d5';
-const theme = createTheme(hexColor);
 
 const renderFiles = () => {
     const items = [];
@@ -51,91 +48,98 @@ const renderFiles = () => {
     return items;
 };
 
-export const basic = () => (
-    <ThemeProvider theme={theme}>
-        <CollapsibleSidebar expanded={boolean('isExpanded', true)}>
-            <CollapsibleSidebarLogo
-                canEndTrial={false}
-                onToggle={noop}
-                expanded={boolean('isExpanded', true)}
-                resinTarget="test-resin"
-            />
-            <CollapsibleSidebarNav>
-                <ul>
-                    <li key="djb-leftnav-menu-item-all-files">
-                        <a href="/" className="is-currentPage">
-                            <CollapsibleSidebarItem
-                                collapsedElement={
-                                    <CollapsibleSidebarMenuItem icon={<Folder16 height={20} width={20} />} />
-                                }
-                                expanded={boolean('isExpanded', true)}
-                                expandedElement={
-                                    <CollapsibleSidebarMenuItem
-                                        icon={<Folder16 height={20} width={20} />}
-                                        text="All Files"
-                                    />
-                                }
-                                tooltipMessage="All Files Link"
-                            />
-                        </a>
-                    </li>
-                    <li key="djb-leftnav-menu-item-all-recents">
-                        <a href="/">
-                            <CollapsibleSidebarItem
-                                collapsedElement={
-                                    <CollapsibleSidebarMenuItem icon={<Folder16 height={20} width={20} />} />
-                                }
-                                expanded={boolean('isExpanded', true)}
-                                expandedElement={
-                                    <CollapsibleSidebarMenuItem
-                                        icon={<ClockBadge16 height={20} width={20} />}
-                                        text="Recents"
-                                    />
-                                }
-                                tooltipMessage="Recents Link"
-                            />
-                        </a>
-                    </li>
-                    <li key="djb-leftnav-menu-item-all-trash">
-                        <a href="/">
-                            <CollapsibleSidebarItem
-                                collapsedElement={
-                                    <CollapsibleSidebarMenuItem icon={<Trash16 height={20} width={20} />} />
-                                }
-                                expanded={boolean('isExpanded', true)}
-                                expandedElement={
-                                    <CollapsibleSidebarMenuItem
-                                        icon={<Trash16 height={20} width={20} />}
-                                        text="Really really long trash link name Trash Link"
-                                    />
-                                }
-                                tooltipMessage="Trash Link"
-                            />
-                        </a>
-                    </li>
-                    {renderFiles()}
-                </ul>
-            </CollapsibleSidebarNav>
-            <CollapsibleSidebarFooter>
-                <ul>
-                    <li key="djb-leftnav-menu-item-all-files">
-                        <CollapsibleSidebarItem
-                            collapsedElement={<CollapsibleSidebarMenuItem icon={<Code16 height={20} width={20} />} />}
-                            expanded={boolean('isExpanded', true)}
-                            expandedElement={
-                                <CollapsibleSidebarMenuItem
-                                    icon={<Code16 height={20} width={20} />}
-                                    text="Developer Console"
+export const basic = () => {
+    const hexColor = color('Theme Color', '#0061d5');
+    const theme = createTheme(hexColor);
+
+    return (
+        <ThemeProvider theme={theme}>
+            <CollapsibleSidebar expanded={boolean('isExpanded', true)}>
+                <CollapsibleSidebarLogo
+                    canEndTrial={false}
+                    onToggle={noop}
+                    expanded={boolean('isExpanded', true)}
+                    resinTarget="test-resin"
+                />
+                <CollapsibleSidebarNav>
+                    <ul>
+                        <li key="djb-leftnav-menu-item-all-files">
+                            <a href="/" className="is-currentPage">
+                                <CollapsibleSidebarItem
+                                    collapsedElement={
+                                        <CollapsibleSidebarMenuItem icon={<Folder16 height={20} width={20} />} />
+                                    }
+                                    expanded={boolean('isExpanded', true)}
+                                    expandedElement={
+                                        <CollapsibleSidebarMenuItem
+                                            icon={<Folder16 height={20} width={20} />}
+                                            text="All Files"
+                                        />
+                                    }
+                                    tooltipMessage="All Files Link"
                                 />
-                            }
-                            tooltipMessage="Developer Console Link"
-                        />
-                    </li>
-                </ul>
-            </CollapsibleSidebarFooter>
-        </CollapsibleSidebar>
-    </ThemeProvider>
-);
+                            </a>
+                        </li>
+                        <li key="djb-leftnav-menu-item-all-recents">
+                            <a href="/">
+                                <CollapsibleSidebarItem
+                                    collapsedElement={
+                                        <CollapsibleSidebarMenuItem icon={<Folder16 height={20} width={20} />} />
+                                    }
+                                    expanded={boolean('isExpanded', true)}
+                                    expandedElement={
+                                        <CollapsibleSidebarMenuItem
+                                            icon={<ClockBadge16 height={20} width={20} />}
+                                            text="Recents"
+                                        />
+                                    }
+                                    tooltipMessage="Recents Link"
+                                />
+                            </a>
+                        </li>
+                        <li key="djb-leftnav-menu-item-all-trash">
+                            <a href="/">
+                                <CollapsibleSidebarItem
+                                    collapsedElement={
+                                        <CollapsibleSidebarMenuItem icon={<Trash16 height={20} width={20} />} />
+                                    }
+                                    expanded={boolean('isExpanded', true)}
+                                    expandedElement={
+                                        <CollapsibleSidebarMenuItem
+                                            icon={<Trash16 height={20} width={20} />}
+                                            text="Really really long trash link name Trash Link"
+                                        />
+                                    }
+                                    tooltipMessage="Trash Link"
+                                />
+                            </a>
+                        </li>
+                        {renderFiles()}
+                    </ul>
+                </CollapsibleSidebarNav>
+                <CollapsibleSidebarFooter>
+                    <ul>
+                        <li key="djb-leftnav-menu-item-all-files">
+                            <CollapsibleSidebarItem
+                                collapsedElement={
+                                    <CollapsibleSidebarMenuItem icon={<Code16 height={20} width={20} />} />
+                                }
+                                expanded={boolean('isExpanded', true)}
+                                expandedElement={
+                                    <CollapsibleSidebarMenuItem
+                                        icon={<Code16 height={20} width={20} />}
+                                        text="Developer Console"
+                                    />
+                                }
+                                tooltipMessage="Developer Console Link"
+                            />
+                        </li>
+                    </ul>
+                </CollapsibleSidebarFooter>
+            </CollapsibleSidebar>
+        </ThemeProvider>
+    );
+};
 
 basic.story = {
     name: 'Top-left, basic',
