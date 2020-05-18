4c143e378effb7bbcdc29e2c42f12adc7d824654
Alex Krolick <akrolick@box.com>
2020-05-18 13:04:12 -0700

    fix(collapsiblesidebar): focus state for menu and logo

--- src/features/collapsible-sidebar/CollapsibleSidebarLogo.js
+++ src/features/collapsible-sidebar/CollapsibleSidebarLogo.js
@@ -5,26 +5,57 @@ import styled from 'styled-components';
 
 import { injectIntl } from 'react-intl';
 import type { InjectIntlProvidedProps } from 'react-intl';
+import { bdlTransitionDurationBase, bdlGridUnit } from '../../styles/variables';
 import Logo from '../../icon/logo/BoxLogo';
 import PlainButton from '../../components/plain-button/PlainButton';
 import LinkBase from '../../components/link/LinkBase';
 import IconHamburger from '../../icons/general/IconHamburger';
 
 import CollapsibleSidebarItem from './CollapsibleSidebarItem';
-import './CollapsibleSidebarLogo.scss';
 
 import messages from './messages';
 
 const StyledLogo = styled(Logo)`
+    padding: ${bdlGridUnit};
+    border: 1px solid transparent;
+    border-radius: 8px;
+    /* transition: ${bdlTransitionDurationBase}; */
+
     & path,
+    & .fill-color {
+        fill: ${props => props.theme?.primary?.foreground};
+    }
+
+    a:focus & {
+        /* since root navlink is focusable, give logo some kind of focus state */
+        border-color: ${props => props.theme?.primary?.foreground};
+    }
+`;
+
+const StyledIconHamburger = styled(IconHamburger)`
+    position: relative;
+    top: 1px; /* svg alignment */
     & .fill-color {
         fill: ${props => props.theme.primary.foreground};
     }
 `;
 
-const StyledIconHamburger = styled(IconHamburger)`
-    & .fill-color {
-        fill: ${props => props.theme.primary.foreground};
+const StyledToggleButton = styled(PlainButton)`
+    /* override .btn-plain's overzealous pseudoelement styling */
+    &,
+    &:focus,
+    &:active,
+    &:hover {
+        padding: 8px 12px; /* we don't have unitless variables to multiply in JS yet */
+        line-height: 1;
+        border-color: transparent;
+        border-style: solid;
+        border-width: 1px;
+        border-radius: 8px; /* we don't have unitless variables to multiply in JS yet */
+    }
+
+    &:focus {
+        border-color: ${props => props?.theme?.primary?.foreground};
     }
 `;
 
@@ -49,13 +80,14 @@ function CollapsibleSidebarLogo(props: Props) {
     const classes = classNames('bdl-CollapsibleSidebar-logo', className);
 
     const toggleButton = (
-        <PlainButton
+        <StyledToggleButton
             className="bdl-CollapsibleSidebar-toggleButton"
             onClick={onToggle}
             aria-label={intl.formatMessage(expanded ? messages.collapseButtonLabel : messages.expandButtonLabel)}
+            type="button"
         >
             <StyledIconHamburger height={20} width={20} />
-        </PlainButton>
+        </StyledToggleButton>
     );
 
     return (
@@ -68,7 +100,11 @@ function CollapsibleSidebarLogo(props: Props) {
                         {toggleButton}
                         <LinkBase data-resin-target={resinTarget} href="/">
                             <>
-                                <StyledLogo className="bdl-CollapsibleSidebar-logoIcon" height={32} width={61} />
+                                <StyledLogo
+                                    className="bdl-CollapsibleSidebar-logoIcon"
+                                    height={32 + 2 * 1 /* border */ + 2 * 4 /* padding */}
+                                    width={61 + 2 * 1 /* border */ + 2 * 4 /* padding */}
+                                />
                                 {badge}
                             </>
                         </LinkBase>
