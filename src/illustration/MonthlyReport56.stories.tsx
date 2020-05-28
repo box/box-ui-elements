72c5e594253572033a78407cff560186d516662d
Andrew Holloway <booc0mtaco@users.noreply.github.com>
2020-05-27 17:45:28 -0700

    feat(assets): update to latest assets from design (#2154)
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

---
+++ src/illustration/MonthlyReport56.stories.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+import MonthlyReport56 from './MonthlyReport56';
+
+export const monthlyReport56 = () => <MonthlyReport56 />;
+
+export default {
+    title: 'Illustration|MonthlyReport56',
+    component: MonthlyReport56,
+    parameters: {
+        notes: "`import MonthlyReport56 from 'box-ui-elements/es/illustration/MonthlyReport56';`",
+    },
+};
