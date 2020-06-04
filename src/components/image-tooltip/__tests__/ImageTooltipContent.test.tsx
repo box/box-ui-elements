a0196c73b3a8a0a304f5783de3b7263080385ea0
Alex Laughnan <alaughnan@box.com>
2020-06-04 15:00:01 -0700

    feat(visual-tooltip): Add in VisualTooltip component (#2165)
    * feat(visual-tooltip): Add in VisualTooltip component
    
    * feat(visual-tooltip): Allow incoming classes from parent
    
    * feat(visual-tooltip): Fix types based on feedback
    
    * feat(visual-tooltip): Fix type import and re-export
    
    * feat(visual-tooltip): Update based on PR feedback
    
    * feat(visual-tooltip): Update based on PR feedback
    
    * feat(visual-tooltip): Fix component prop comment
    
    * feat(visual-tooltip): Rename to ImageTooltip
    
    * feat(visual-tooltip): Update to use units and CSS specificity
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

---
+++ src/components/image-tooltip/__tests__/ImageTooltipContent.test.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { shallow, ShallowWrapper } from 'enzyme';
+
+import ImageTooltipContent from '../ImageTooltipContent';
+import testImageSrc from '../getTestImageSrc';
+
+describe('components/image-tooltip/ImageTooltipContent', () => {
+    test('should correctly render an ImageTooltipContent', () => {
+        const tooltipContent = 'Hey I am content';
+        const tooltipTitle = 'I am a title';
+        const image = <img src={testImageSrc} alt="foo" />;
+
+        const wrapper: ShallowWrapper = shallow(
+            <ImageTooltipContent content={tooltipContent} title={tooltipTitle}>
+                {image}
+            </ImageTooltipContent>,
+        );
+
+        expect(wrapper).toMatchSnapshot();
+    });
+});
