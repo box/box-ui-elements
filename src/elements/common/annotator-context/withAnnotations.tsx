4858ac4bd28c0f7a49b547f300f8145a9b5c01f3
Mingze <mxiao@box.com>
2020-07-09 16:57:29 -0700

    feat(annotations): Handle API fetch error (#2172)
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- src/elements/common/annotator-context/withAnnotations.tsx
+++ src/elements/common/annotator-context/withAnnotations.tsx
@@ -21,8 +21,8 @@ export type ComponentWithAnnotations = {
     handleActiveChange: ActiveChangeEventHandler;
     handleAnnotationChangeEvent: (id: string | null) => void;
     handleAnnotationCreate: (eventData: AnnotationActionEvent) => void;
+    handleAnnotationFetchError: ({ error }: { error: Error }) => void;
     handleAnnotator: (annotator: Annotator) => void;
-    handleAnnotatorEvent: ({ event, data }: { event: string; data?: unknown }) => void;
     handlePreviewDestroy: (shouldReset?: boolean) => void;
 };
 
@@ -130,10 +130,19 @@ export default function withAnnotations<P extends object>(
             this.setState({ activeAnnotationFileVersionId: fileVersionId, activeAnnotationId: annotationId });
         };
 
+        handleAnnotationFetchError = ({ error }: { error: Error }): void => {
+            const { onError } = this.props;
+
+            if (onError && error) {
+                onError(error, 'fetch_annotations_error');
+            }
+        };
+
         handleAnnotator = (annotator: Annotator): void => {
             this.annotator = annotator;
             this.annotator.addListener('annotations_active_change', this.handleActiveChange);
             this.annotator.addListener('annotations_create', this.handleAnnotationCreate);
+            this.annotator.addListener('annotations_fetch_error', this.handleAnnotationFetchError);
         };
 
         handlePreviewDestroy = (shouldReset = true): void => {
@@ -144,6 +153,7 @@ export default function withAnnotations<P extends object>(
             if (this.annotator) {
                 this.annotator.removeListener('annotations_active_change', this.handleActiveChange);
                 this.annotator.removeListener('annotations_create', this.handleAnnotationCreate);
+                this.annotator.removeListener('annotations_fetch_error', this.handleAnnotationFetchError);
             }
 
             this.annotator = null;
