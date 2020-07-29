ecdea5b8650013ca3ff85ceba7556a045ffa32ca
Stephen Choi <viodragon2@gmail.com>
2020-07-29 15:20:06 -0700

    feat(sandbox-banner): fix storybook test

--- src/features/sandbox-banner/__tests__/SandboxBanner.stories.test.tsx
+++ src/features/sandbox-banner/__tests__/SandboxBanner.stories.test.tsx
@@ -1,5 +1,5 @@
 describe('features/sandbox-banner/SandboxBanner', () => {
-    test.each([['features-sandbox-banner--basic']])('looks visually correct when using story %s', async id => {
+    test.each([['features-sandboxbanner--basic']])('looks visually correct when using story %s', async id => {
         const image = await BoxVisualTestUtils.takeScreenshot(id);
         return expect(image).toMatchImageSnapshot();
     });
