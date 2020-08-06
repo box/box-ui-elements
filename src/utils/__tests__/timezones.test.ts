b9c41534cce3af1291266eb91c5f07a48938b957
Edwin Hoogerbeets <ehoogerbeets@box.com>
2020-08-05 22:55:18 -0700

    feat(i18n): support dynamic time zones if the platform supports it
    If the platform supports it, return a dynamic list of time zones
    where the offsets are given for the current day based on whether
    or not the zone is in DST. If the zone does not use DST, or if
    the JS engine does not support time zone calculations, then just
    return the static string from @box/cldr-data instead.

--- src/utils/__tests__/timezones.test.ts
+++ src/utils/__tests__/timezones.test.ts
@@ -12,97 +12,254 @@ const germanZoneData = germanLocaleData.timezones;
 const russianZoneData = russianLocaleData.timezones;
 const japaneseZoneData = japaneseLocaleData.timezones;
 
+const RealDate = Date;
+
+function mockDate(isoDate: string) {
+    const unixtime = new Date(isoDate).getTime();
+    global.Date.now = jest.fn(() => {
+        return unixtime;
+    });
+}
+
 describe('util/timezones', () => {
+    afterEach(() => {
+        global.Date.now = RealDate.now;
+    });
+
     test('should work in English', () => {
+        mockDate('2020-06-01T00:00:00z');
+
         const zones: TimeZoneEntries = timezones({
             includeZones: 'all',
         }); // default is English
+
+        // no DST
         expect(zones['1']).toStrictEqual({
             name: 'Africa/Abidjan',
+            nameLocalized: 'Africa/Abidjan',
             displayName: 'GMT+00:00 Africa/Abidjan GMT',
+            abbreviationStandard: 'GMT',
             id: 1,
         });
+        // has DST
         expect(zones['139']).toStrictEqual({
             name: 'America/Los_Angeles',
-            displayName: 'GMT-08:00 America/Los_Angeles PST',
+            nameLocalized: 'America/Los_Angeles',
+            displayName: 'GMT-07:00 America/Los_Angeles PDT',
+            abbreviationStandard: 'PST',
+            abbreviationDaylight: 'PDT',
             enabled: true,
             id: 139,
         });
     });
 
+    test('should be sensitive to the current date in English', () => {
+        mockDate('2020-01-01T00:00:00z');
+
+        const zones: TimeZoneEntries = timezones({
+            includeZones: 'all',
+        }); // default is English
+        // no DST
+        expect(zones['1']).toStrictEqual({
+            name: 'Africa/Abidjan',
+            nameLocalized: 'Africa/Abidjan',
+            displayName: 'GMT+00:00 Africa/Abidjan GMT',
+            abbreviationStandard: 'GMT',
+            id: 1,
+        });
+        // has DST
+        expect(zones['139']).toStrictEqual({
+            name: 'America/Los_Angeles',
+            nameLocalized: 'America/Los_Angeles',
+            displayName: 'GMT-08:00 America/Los_Angeles PST',
+            abbreviationStandard: 'PST',
+            abbreviationDaylight: 'PDT',
+            enabled: true,
+            id: 139,
+        });
+    });
+
+    test('should work in the southern hemisphere standard time', () => {
+        mockDate('2020-06-01T00:00:00z');
+        const zones: TimeZoneEntries = timezones({
+            includeZones: 'all',
+        });
+        expect(zones['321']).toStrictEqual({
+            name: 'Australia/Melbourne',
+            nameLocalized: 'Australia/Melbourne',
+            displayName: 'GMT+10:00 Australia/Melbourne AEST',
+            abbreviationStandard: 'AEST',
+            abbreviationDaylight: 'AEDT',
+            id: 321,
+        });
+    });
+
+    test('should work in the southern hemisphere daylight time', () => {
+        mockDate('2020-01-01T00:00:00z');
+        const zones: TimeZoneEntries = timezones({
+            includeZones: 'all',
+        });
+        expect(zones['321']).toStrictEqual({
+            name: 'Australia/Melbourne',
+            nameLocalized: 'Australia/Melbourne',
+            displayName: 'GMT+11:00 Australia/Melbourne AEDT',
+            abbreviationStandard: 'AEST',
+            abbreviationDaylight: 'AEDT',
+            id: 321,
+        });
+    });
+
+    test('should be able to format partial hour offsets standard', () => {
+        mockDate('2020-01-01T00:00:00z');
+
+        const zones: TimeZoneEntries = timezones({
+            includeZones: 'all',
+        }); // default is English
+        // northern hemisphere
+        expect(zones['184']).toStrictEqual({
+            name: 'America/St_Johns',
+            nameLocalized: 'America/St_Johns',
+            displayName: 'GMT-03:30 America/St_Johns NST',
+            abbreviationStandard: 'NST',
+            abbreviationDaylight: 'NDT',
+            enabled: true,
+            id: 184,
+        });
+        // southern hemisphere
+        expect(zones['487']).toStrictEqual({
+            name: 'Pacific/Chatham',
+            nameLocalized: 'Pacific/Chatham',
+            displayName: 'GMT+13:45 Pacific/Chatham +1245/+1345',
+            abbreviationStandard: '+1245/+1345',
+            abbreviationDaylight: '+1245/+1345',
+            id: 487,
+        });
+    });
+
+    test('should be able to format partial hour offsets daylight', () => {
+        mockDate('2020-06-01T00:00:00z');
+
+        const zones: TimeZoneEntries = timezones({
+            includeZones: 'all',
+        }); // default is English
+        // northern hemisphere
+        expect(zones['184']).toStrictEqual({
+            name: 'America/St_Johns',
+            nameLocalized: 'America/St_Johns',
+            displayName: 'GMT-02:30 America/St_Johns NDT',
+            abbreviationStandard: 'NST',
+            abbreviationDaylight: 'NDT',
+            enabled: true,
+            id: 184,
+        });
+        // southern hemisphere
+        expect(zones['487']).toStrictEqual({
+            name: 'Pacific/Chatham',
+            nameLocalized: 'Pacific/Chatham',
+            displayName: 'GMT+12:45 Pacific/Chatham +1245/+1345',
+            abbreviationStandard: '+1245/+1345',
+            abbreviationDaylight: '+1245/+1345',
+            id: 487,
+        });
+    });
+
     test('should work in German', () => {
+        mockDate('2020-06-01T00:00:00z');
         const zones: TimeZoneEntries = timezones({
             includeZones: 'all',
             tzData: germanZoneData,
         });
         expect(zones['295']).toStrictEqual({
             name: 'Asia/Yekaterinburg',
+            nameLocalized: 'Jekaterinburg',
             displayName: 'GMT+05:00 Jekaterinburg +05',
+            abbreviationStandard: '+05',
             enabled: true,
             id: 295,
         });
         expect(zones['301']).toStrictEqual({
             name: 'Atlantic/Faeroe',
+            nameLocalized: 'Färöer',
             displayName: 'GMT+00:00 Färöer UTC',
+            abbreviationStandard: 'UTC',
             id: 301,
         });
     });
 
     test('should work in Japanese', () => {
+        mockDate('2020-06-01T00:00:00z');
         const zones: TimeZoneEntries = timezones({
             includeZones: 'all',
             tzData: japaneseZoneData,
         });
         expect(zones['321']).toStrictEqual({
             name: 'Australia/Melbourne',
+            nameLocalized: 'メルボルン',
             displayName: 'GMT+10:00 メルボルン AEST',
+            abbreviationStandard: 'AEST',
+            abbreviationDaylight: 'AEDT',
             id: 321,
         });
         expect(zones['390']).toStrictEqual({
             name: 'Europe/Amsterdam',
-            displayName: 'GMT+01:00 アムステルダム CET',
+            nameLocalized: 'アムステルダム',
+            displayName: 'GMT+02:00 アムステルダム CEST',
+            abbreviationStandard: 'CET',
+            abbreviationDaylight: 'CEST',
             enabled: true,
             id: 390,
         });
     });
 
     test('should work in Russian', () => {
+        mockDate('2020-06-01T00:00:00z');
         const zones: TimeZoneEntries = timezones({
             includeZones: 'all',
             tzData: russianZoneData,
         });
         expect(zones['295']).toStrictEqual({
             name: 'Asia/Yekaterinburg',
+            nameLocalized: 'Екатеринбург',
             displayName: 'GMT+05:00 Екатеринбург +05',
+            abbreviationStandard: '+05',
             enabled: true,
             id: 295,
         });
         expect(zones['402']).toStrictEqual({
             name: 'Europe/Dublin',
+            nameLocalized: 'Дублин',
             displayName: 'GMT+01:00 Дублин IST/GMT',
+            abbreviationStandard: 'IST/GMT',
             id: 402,
         });
     });
 
     test('should get all zones', () => {
+        mockDate('2020-06-01T00:00:00z');
         const zones: TimeZoneEntries = timezones({
             includeZones: 'all',
         });
         expect(Object.keys(zones).length).toBe(551);
         expect(zones['1']).toStrictEqual({
             name: 'Africa/Abidjan',
+            nameLocalized: 'Africa/Abidjan',
             displayName: 'GMT+00:00 Africa/Abidjan GMT',
+            abbreviationStandard: 'GMT',
             id: 1,
         });
         expect(zones['139']).toStrictEqual({
             name: 'America/Los_Angeles',
-            displayName: 'GMT-08:00 America/Los_Angeles PST',
+            nameLocalized: 'America/Los_Angeles',
+            displayName: 'GMT-07:00 America/Los_Angeles PDT',
+            abbreviationStandard: 'PST',
+            abbreviationDaylight: 'PDT',
             enabled: true,
             id: 139,
         });
     });
 
     test('should get only enabled zones', () => {
+        mockDate('2020-01-01T00:00:00z');
         const zones: TimeZoneEntries = timezones({
             includeZones: 'enabled',
         });
@@ -110,48 +267,82 @@ describe('util/timezones', () => {
         expect(zones['1']).toBeUndefined();
         expect(zones['139']).toStrictEqual({
             name: 'America/Los_Angeles',
+            nameLocalized: 'America/Los_Angeles',
             displayName: 'GMT-08:00 America/Los_Angeles PST',
+            abbreviationStandard: 'PST',
+            abbreviationDaylight: 'PDT',
             enabled: true,
             id: 139,
         });
     });
 
     test('findzone should work in English', () => {
+        mockDate('2020-06-01T00:00:00z');
         const zone: TimeZone = findTimeZone(139);
         expect(zone).toStrictEqual({
             name: 'America/Los_Angeles',
-            displayName: 'GMT-08:00 America/Los_Angeles PST',
+            nameLocalized: 'America/Los_Angeles',
+            displayName: 'GMT-07:00 America/Los_Angeles PDT',
+            abbreviationStandard: 'PST',
+            abbreviationDaylight: 'PDT',
             enabled: true,
             id: 139,
         });
     });
 
     test('findzone should work in English with a string param', () => {
+        mockDate('2020-06-01T00:00:00z');
         const zone: TimeZone = findTimeZone('139');
         expect(zone).toStrictEqual({
             name: 'America/Los_Angeles',
-            displayName: 'GMT-08:00 America/Los_Angeles PST',
+            nameLocalized: 'America/Los_Angeles',
+            displayName: 'GMT-07:00 America/Los_Angeles PDT',
+            abbreviationStandard: 'PST',
+            abbreviationDaylight: 'PDT',
             enabled: true,
             id: 139,
         });
     });
 
     test('findzone should work with disabled zones in English', () => {
+        mockDate('2020-06-01T00:00:00z');
         const zone: TimeZone = findTimeZone(1);
         expect(zone).toStrictEqual({
             name: 'Africa/Abidjan',
+            nameLocalized: 'Africa/Abidjan',
             displayName: 'GMT+00:00 Africa/Abidjan GMT',
+            abbreviationStandard: 'GMT',
             id: 1,
         });
     });
 
     test('findzone should work in Japanese', () => {
+        mockDate('2020-06-01T00:00:00z');
         const zone: TimeZone = findTimeZone(139, {
             tzData: japaneseZoneData,
         });
         expect(zone).toStrictEqual({
             name: 'America/Los_Angeles',
+            nameLocalized: 'ロサンゼルス',
+            displayName: 'GMT-07:00 ロサンゼルス PDT',
+            abbreviationStandard: 'PST',
+            abbreviationDaylight: 'PDT',
+            enabled: true,
+            id: 139,
+        });
+    });
+
+    test('findzone should be sensitive to the date in Japanese', () => {
+        mockDate('2020-01-01T00:00:00z');
+        const zone: TimeZone = findTimeZone(139, {
+            tzData: japaneseZoneData,
+        });
+        expect(zone).toStrictEqual({
+            name: 'America/Los_Angeles',
+            nameLocalized: 'ロサンゼルス',
             displayName: 'GMT-08:00 ロサンゼルス PST',
+            abbreviationStandard: 'PST',
+            abbreviationDaylight: 'PDT',
             enabled: true,
             id: 139,
         });
--- src/utils/timezones.ts
+++ src/utils/timezones.ts
@@ -18,6 +18,93 @@ export interface TimeZoneEntries {
     [id: string]: TimeZone;
 }
 
+/**
+ * Test whether or not the current platform and JS engine supports the Intl object
+ * and resolving time zones. If it doesn't, we just return a static list of time
+ * zones. If it does, we make a dynamic list where each time zone is given with the
+ * offset for the current day, taking into account whether that is in DST or not.
+ */
+const supportsIntl: boolean = (function support() {
+    if (typeof Intl === 'undefined' || typeof Intl.DateTimeFormat === 'undefined') {
+        return false;
+    }
+
+    const tz: string = new Intl.DateTimeFormat('en-US', {
+        timeZone: 'America/Los_Angeles',
+    }).resolvedOptions().timeZone;
+
+    return typeof tz !== 'undefined';
+})();
+
+/**
+ * Convert the offset in minutes to an hour:minute string.
+ */
+function getOffsetStr(offsetInMinutes: number): string {
+    const offsetHours = Math.floor(Math.abs(offsetInMinutes) / 60);
+    const offsetMinutes = Math.floor(Math.abs(offsetInMinutes) % (offsetHours * 60));
+
+    return `${offsetInMinutes < 1 ? '+' : '-'}${offsetHours < 10 ? `0${offsetHours}` : offsetHours}:${
+        offsetMinutes < 10 ? `0${offsetMinutes}` : offsetMinutes
+    }`;
+}
+
+/**
+ * Return the offset from UTC in minutes.
+ */
+function offset(date: Date, timeZone: string): number {
+    // use Japanese with toLocaleString because they have a nice arrangement of date parts from largest to smallest.
+    // If toLocaleString on this engine does not support locale or options parameters, then this call will throw
+    // and we will catch that
+    const arr: number[] = date
+        .toLocaleString('ja', { timeZone })
+        .split(/[/\s:]/)
+        .map(part => {
+            return Number(part);
+        });
+    arr[1] -= 1; // months are zero-based in JS for some reason, so subtract one to get the right month number
+    const t1 = Date.UTC.apply(null, arr as [number, number, number, number, number, number]);
+    const t2 = new Date(date).setMilliseconds(0);
+    return (t2 - t1) / 60000;
+}
+
+/**
+ * Calculate the display name of the zone for the given date. If the zone is in DST on that date or not,
+ * then return the string appropriately. If the zone does not use DST or if the platform cannot calculate it,
+ * then just return the static displayName string from @box/cldr-data
+ */
+function getCurrentDisplayName(date: Date, zone: TimeZone): string {
+    const { name, displayName } = zone;
+    if (supportsIntl && zone.abbreviationDaylight) {
+        try {
+            const jan1 = new Date(date.getFullYear(), 0, 1, 0, 0, 0);
+            const jun1 = new Date(date.getFullYear(), 5, 1, 0, 0, 0);
+            const jan1offset = offset(jan1, name);
+            const jun1offset = offset(jun1, name);
+            if (jan1offset === jun1offset) {
+                // no DST in this zone in this year, so just return the already calculated display name
+                return displayName;
+            }
+
+            const current = offset(new Date(Date.now()), name);
+            let zonename = `GMT${getOffsetStr(current)} ${zone.nameLocalized} `;
+            if (jan1offset < jun1offset) {
+                // southern hemisphere
+                zonename += current === jan1offset ? zone.abbreviationDaylight : zone.abbreviationStandard;
+            } else {
+                // northern hemisphere
+                zonename += current === jun1offset ? zone.abbreviationDaylight : zone.abbreviationStandard;
+            }
+            return zonename;
+        } catch (e) {
+            // sometimes the current platform does not support all of the zones or toLocaleString does not support
+            // locale and option parameters, so we have to catch for those cases
+            return displayName;
+        }
+    } else {
+        return displayName;
+    }
+}
+
 function timezones(options: TimeZoneOptions = {}): TimeZoneEntries {
     const zones: TimeZoneEntries = {};
 
@@ -30,9 +117,13 @@ function timezones(options: TimeZoneOptions = {}): TimeZoneEntries {
         tzData = boxzones;
     }
 
+    const today = new Date(Date.now());
     tzData.forEach(zone => {
         if (includeZones === 'all' || zone.enabled) {
-            zones[String(zone.id)] = zone;
+            zones[String(zone.id)] = {
+                ...zone,
+                displayName: getCurrentDisplayName(today, zone),
+            };
         }
     });
 
