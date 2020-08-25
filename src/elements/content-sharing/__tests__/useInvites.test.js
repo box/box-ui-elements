c16ca300af16b2765069fe22e8035dfce6259374
Stephanie A. Higa <sahiga@gmail.com>
2020-08-25 11:19:21 -0700

    feat(content-sharing): Display avatars

--- src/elements/content-sharing/__tests__/useInvites.test.js
+++ src/elements/content-sharing/__tests__/useInvites.test.js
@@ -1,6 +1,6 @@
 // @flow
 
-import React from 'react';
+import * as React from 'react';
 import { act } from 'react-dom/test-utils';
 import { mount } from 'enzyme';
 import API from '../../../api';
--- src/elements/content-sharing/hooks/useAvatars.js
+++ src/elements/content-sharing/hooks/useAvatars.js
@@ -1,48 +1,44 @@
 // @flow
 
 import * as React from 'react';
-import noop from 'lodash/noop';
 import API from '../../../api';
-import type { ContentSharingHooksOptions, GetAvatarsFnType } from '../types';
+import type { Collaboration, Collaborations } from '../../../common/types/core';
+import type { AvatarURLMap } from '../types';
 
 /**
- * Generate the getAvatars() function, which is used for retrieving potential collaborators in the USM.
+ * Generate a map of avatar URLs, which are used to display collaborators in the USM.
  *
  * @param {API} api
- * @param {collaboratorsList}
- * @returns {GetAvatarsFnType | null}
+ * @param {string} itemID
+ * @param {Collaborations | null} collaboratorsList
+ * @returns {AvatarURLMap | null}
  */
-function useAvatars(
-    api: API,
-    itemID,
-    collaboratorsList,
-    options: ContentSharingHooksOptions = {},
-): GetAvatarsFnType | null {
-    const [avatars, setAvatars] = React.useState<null | GetAvatarsFnType>(null);
-    const { handleSuccess = noop, handleError = noop } = options;
+function useAvatars(api: API, itemID: string, collaboratorsList: Collaborations | null): AvatarURLMap | null {
+    const [avatarURLMap, setAvatarURLMap] = React.useState<AvatarURLMap | null>(null);
 
     React.useEffect(() => {
-        if (avatars || !collaboratorsList || !collaboratorsList.entries) return;
+        if (avatarURLMap || !collaboratorsList || !collaboratorsList.entries) return;
+
         const usersAPI = api.getUsersAPI(false);
 
         (async () => {
-            const idToAvatarMap = {};
+            const retrievedAvatarURLMap: AvatarURLMap = {};
+            const entries = collaboratorsList ? collaboratorsList.entries : []; // needed for Flow
             await Promise.all(
-                collaboratorsList.entries.map(async collab => {
-                    if (!collab || !collab.accessible_by) return null;
+                entries.map(async (collab: Collaboration) => {
+                    if (!collab || !collab.accessible_by) return;
                     const {
                         accessible_by: { id: userID },
                     } = collab;
-                    const url = await usersAPI.getAvatarUrlWithAccessToken(userID, itemID);
-                    idToAvatarMap[userID] = url;
-                    return null;
+                    const url = await usersAPI.getAvatarUrlWithAccessToken(userID.toString(), itemID);
+                    retrievedAvatarURLMap[userID] = url;
                 }),
             );
-            setAvatars(idToAvatarMap);
+            setAvatarURLMap(retrievedAvatarURLMap);
         })();
-    }, [api, avatars, collaboratorsList, handleError, handleSuccess, itemID]);
+    }, [api, avatarURLMap, collaboratorsList, itemID]);
 
-    return avatars;
+    return avatarURLMap;
 }
 
 export default useAvatars;
