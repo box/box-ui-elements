6f7e4cf2efe035a9c4b92b53c2ee81856086002b
Jack Chen <yuxi.chen@berkeley.edu>
2020-10-20 13:50:37 -0700

    feat(annotations): update error callback for annotation modify (#2376)
    * feat(annotations): update error callback for annotation modify

--- src/api/messages.js
+++ src/api/messages.js
@@ -27,6 +27,11 @@ const messages = defineMessages({
         description: 'Error message when a comment deletion fails',
         defaultMessage: 'There was an error deleting this comment.',
     },
+    commentUpdateErrorMessage: {
+        id: 'be.api.commentUpdateErrorMessage',
+        description: 'Error message when an annotation comment update fails',
+        defaultMessage: 'This comment could not be modified.',
+    },
     taskActionErrorTitle: {
         id: 'be.api.taskActionErrorTitle',
         description: 'Title shown when an error occurs performing an action on a task',
