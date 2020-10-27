a883d6cf480ac0cabce65b44eea7dfc1e71c1866
Jack Chen <yuxi.chen@berkeley.edu>
2020-10-27 12:50:00 -0700

    fix(annotation): enable modify annotations
    Changes in this PR:
    - [x] enables modify annotations

--- src/api/Feed.js
+++ src/api/Feed.js
@@ -1281,7 +1281,7 @@ class Feed extends Base {
                 }
             },
             errorCallback: (e: ErrorResponseData, code: string) => {
-                this.feedErrorCallback(true, e, code);
+                this.updateCommentErrorCallback(e, code, commentId);
             },
         });
     };
