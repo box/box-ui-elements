f01d890114ede1c7262747cfb1db64b4fb49cb1a
Mingze Xiao <mxiao@box.com>
2020-10-28 15:29:49 -0700

    test(cypress): Add E2E tests for annotatoins in content-preview

--- scripts/styleguide.setup.js
+++ scripts/styleguide.setup.js
@@ -4,8 +4,8 @@ import features from '../examples/src/features';
 global.FEATURES = global.FEATURES || features;
 global.FILE_ID = global.FILE_ID || __FILEID__ || '415542803939'; // eslint-disable-line
 global.FOLDER_ID = global.FOLDER_ID || __FOLDERID__ || '69083462919'; // eslint-disable-line
-// NOTE: The token used is a readonly token accessing public data in a demo enterprise.
-global.TOKEN = global.TOKEN || __TOKEN__ || 'S8wjvjOL9GEK5VtXsQNVMOwSrx1g55oC'; // eslint-disable-line
+// NOTE: The token used is a readonly token accessing public data in a demo enterprise. DO NOT PUT A WRITE TOKEN
+global.TOKEN = global.TOKEN || __TOKEN__ || 'QvMQdM3Lqk9zyYjxS2a9bCNkM1R4Icr1'; // eslint-disable-line
 global.PROPS = global.PROPS || {}; // eslint-disable-line
 
 // Components
--- test/support/commands.js
+++ test/support/commands.js
@@ -2,3 +2,11 @@ Cypress.Commands.add('getByTestId', testId => cy.get(`[data-testid="${testId}"]`
 Cypress.Commands.add('getByTitle', title => cy.get(`[title="${title}"]`));
 Cypress.Commands.add('getTooltip', () => cy.get('[role="tooltip"]'));
 Cypress.Commands.add('getByAriaLabel', ariaLabel => cy.get(`[aria-label="${ariaLabel}"]`));
+
+// Annotations-specific commands
+Cypress.Commands.add('clickAnnotationLinkByComment', comment => {
+    cy.contains(comment)
+        .siblings()
+        .filter('[data-testid="bcs-AnnotationActivity-link"]')
+        .click();
+});
