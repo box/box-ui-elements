a20c8625019e63b49979b58a502aea24dc7490a3
Conrad Chan <conradchan@box.com>
2020-11-16 11:29:56 -0800

    feat(assets): update assets to latest from design (#2412)

---
+++ src/icon/line/Undo16.stories.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+import Undo16 from './Undo16';
+
+export const undo16 = () => <Undo16 />;
+
+export default {
+    title: 'Icon|Line|Undo16',
+    component: Undo16,
+    parameters: {
+        notes: "`import Undo16 from 'box-ui-elements/es/icon/line/Undo16';`",
+    },
+};
