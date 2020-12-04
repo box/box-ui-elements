f6f1836193ce24f62d459f28f2d1f7910ead8eb3
Mingze <mxiao@box.com>
2020-12-03 16:29:04 -0800

    test(cypress): Change to new primary token (#2424)

--- scripts/styleguide.setup.js
+++ scripts/styleguide.setup.js
@@ -5,7 +5,7 @@ global.FEATURES = global.FEATURES || features;
 global.FILE_ID = global.FILE_ID || __FILEID__ || '415542803939'; // eslint-disable-line
 global.FOLDER_ID = global.FOLDER_ID || __FOLDERID__ || '69083462919'; // eslint-disable-line
 // NOTE: The token used is a readonly token accessing public data in a demo enterprise. DO NOT PUT A WRITE TOKEN
-global.TOKEN = global.TOKEN || __TOKEN__ || 'QvMQdM3Lqk9zyYjxS2a9bCNkM1R4Icr1'; // eslint-disable-line
+global.TOKEN = global.TOKEN || __TOKEN__ || 'P1n3ID8nYMxHRWvenDatQ9k6JKzWzYrz'; // eslint-disable-line
 global.PROPS = global.PROPS || {}; // eslint-disable-line
 
 // Components
