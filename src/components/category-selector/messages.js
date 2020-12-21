4efb4b90f43245179d60ee19a50f5a91ee562e03
Megan Smith <megansmith@box.com>
2020-12-21 15:42:09 -0800

    feat(preview): add category selector component

--- src/components/category-selector/messages.js
+++ src/components/category-selector/messages.js
@@ -2,7 +2,7 @@ import { defineMessages } from 'react-intl';
 
 const messages = defineMessages({
     more: {
-        id: 'boxrelay.templates.category.label.more',
+        id: 'categoryselector.templates.category.label.more',
         description: 'Button label for the "more" dropdown menu',
         defaultMessage: 'More',
     },
