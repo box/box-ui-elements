7bdf2fb62ef43e5247e2c8c343989a03fb01c55d
fsuarez <fsuarez@box.com>
2021-03-25 09:57:56 -0500

    fix: Add arial-label attribute in SideBarButton and NavButton Components

--- src/elements/content-sidebar/SidebarNavButton.js
+++ src/elements/content-sidebar/SidebarNavButton.js
@@ -11,6 +11,7 @@ import Tooltip from '../../components/tooltip/Tooltip';
 import './SidebarNavButton.scss';
 
 type Props = {
+    'aria-label'?: string,
     'data-resin-target'?: string,
     'data-testid'?: string,
     children: React.Node,
@@ -29,6 +30,7 @@ const SidebarNavButton = React.forwardRef<Props, React.Ref<any>>((props: Props, 
         isOpen,
         sidebarView,
         tooltip,
+        'aria-label': dataAriaLabel,
     } = props;
     const sidebarPath = `/${sidebarView}`;
 
