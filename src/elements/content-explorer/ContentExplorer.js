af366840cda9dde059c3e6051903a50a3354266b
Stephanie A. Higa <sahiga@gmail.com>
2021-04-21 11:12:08 -0700

    fix(content-explorer): Request fields for Preview SDK file cache (#2540)
    * fix(content-explorer): Request fields for Preview SDK file cache
    
    * fix(content-explorer): Request fields for Preview SDK file cache
    
    * fix(content-explorer): Request fields for Preview SDK file cache

--- src/elements/content-explorer/ContentExplorer.js
+++ src/elements/content-explorer/ContentExplorer.js
@@ -31,7 +31,8 @@ import DeleteConfirmationDialog from './DeleteConfirmationDialog';
 import Content from './Content';
 import isThumbnailReady from './utils';
 import { isFocusableElement, isInputElement, focus } from '../../utils/dom';
-import { FILE_SHARED_LINK_FIELDS_TO_FETCH, FOLDER_FIELDS_TO_FETCH } from '../../utils/fields';
+import { FILE_SHARED_LINK_FIELDS_TO_FETCH } from '../../utils/fields';
+import CONTENT_EXPLORER_FOLDER_FIELDS_TO_FETCH from './constants';
 import LocalStore from '../../utils/LocalStore';
 import { withFeatureConsumer, withFeatureProvider, type FeatureConfig } from '../common/feature-checking';
 import {
@@ -580,7 +581,7 @@ class ContentExplorer extends Component<Props, State> {
                 this.fetchFolderSuccessCallback(collection, triggerNavigationEvent);
             },
             this.errorCallback,
-            { fields: FOLDER_FIELDS_TO_FETCH, forceFetch: true },
+            { fields: CONTENT_EXPLORER_FOLDER_FIELDS_TO_FETCH, forceFetch: true },
         );
     };
 
@@ -643,7 +644,7 @@ class ContentExplorer extends Component<Props, State> {
         this.api
             .getSearchAPI()
             .search(id, query, currentPageSize, currentOffset, this.searchSuccessCallback, this.errorCallback, {
-                fields: FOLDER_FIELDS_TO_FETCH,
+                fields: CONTENT_EXPLORER_FOLDER_FIELDS_TO_FETCH,
                 forceFetch: true,
             });
     }, DEFAULT_SEARCH_DEBOUNCE);
@@ -739,7 +740,7 @@ class ContentExplorer extends Component<Props, State> {
                 this.recentsSuccessCallback(collection, triggerNavigationEvent);
             },
             this.errorCallback,
-            { fields: FOLDER_FIELDS_TO_FETCH, forceFetch: true },
+            { fields: CONTENT_EXPLORER_FOLDER_FIELDS_TO_FETCH, forceFetch: true },
         );
     }
 
