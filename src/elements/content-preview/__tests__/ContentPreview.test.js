0af42cdfcd1af6a71d28dd3a25fdd51e295e3b29
megansmith-box <75504287+megansmith-box@users.noreply.github.com>
2021-04-28 12:54:16 -0700

    feat(content-preview): update props in content preview (#2549)
    * feat(content-preview): update props in content preview
    
    * feat(content-preview): update props in content preview
    
    * feat(content-preview): update props in content preview

--- src/elements/content-preview/__tests__/ContentPreview.test.js
+++ src/elements/content-preview/__tests__/ContentPreview.test.js
@@ -24,6 +24,7 @@ describe('elements/content-preview/ContentPreview', () => {
             this.show = jest.fn();
             this.updateToken = jest.fn();
             this.addListener = jest.fn();
+            this.updateExperiences = jest.fn();
         };
         global.performance = {
             now: jest.fn().mockReturnValue(PERFORMANCE_TIME),
@@ -954,6 +955,15 @@ describe('elements/content-preview/ContentPreview', () => {
                 token: 'bar',
             });
             expect(instance.updatePreviewToken).toBeCalledTimes(1);
+        });
+
+        test('should update experiences in preview when previewExperiences changes', async () => {
+            instance.preview = new global.Box.Preview();
+            wrapper.setProps({
+                previewExperiences: {},
+            });
+
+            expect(instance.preview.updateExperiences).toBeCalledTimes(1);
         });
     });
 
