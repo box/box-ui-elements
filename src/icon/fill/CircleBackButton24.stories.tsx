b4dfca4ad0b45abc5df356232c11b15bbb568266
Jared Stoffan <jstoffan@users.noreply.github.com>
2021-06-01 12:53:43 -0700

    feat(icons): Import latest assets from design (#2583)

---
+++ src/icon/fill/CircleBackButton24.stories.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+import CircleBackButton24 from './CircleBackButton24';
+
+export const circleBackButton24 = () => <CircleBackButton24 />;
+
+export default {
+    title: 'Icon|Fill|CircleBackButton24',
+    component: CircleBackButton24,
+    parameters: {
+        notes: "`import CircleBackButton24 from 'box-ui-elements/es/icon/fill/CircleBackButton24';`",
+    },
+};
--- src/icon/fill/InProgress16.tsx
+++ src/icon/fill/InProgress16.tsx
@@ -1,9 +1,7 @@
-// @flow
 /* eslint-disable react/jsx-sort-props */
 import * as React from 'react';
 import * as vars from '../../styles/variables';
-import AccessibleSVG from '../../icons/accessible-svg';
-import type { Icon } from '../../icons/flowTypes';
+import AccessibleSVG, { SVGProps } from '../../components/accessible-svg/AccessibleSVG';
 
 /**
  * This is an auto-generated component and should not be edited
@@ -16,13 +14,14 @@ import type { Icon } from '../../icons/flowTypes';
  * - https://github.com/box/box-ui-elements/issues/new?template=Feature_request.md
  */
 
-const Checkmark16 = (props: Icon) => (
+const InProgress16 = (props: SVGProps) => (
     <AccessibleSVG width={16} height={16} viewBox="0 0 16 16" {...props}>
         <path
             fill={vars.bdlGray50}
-            d="M6.617 14c.55 0 .968-.2 1.253-.603l6.34-9.273c.102-.15.175-.296.221-.438.046-.143.069-.28.069-.41a1.2 1.2 0 00-.387-.912C13.855 2.121 13.53 2 13.139 2c-.26 0-.482.05-.666.15-.183.101-.357.275-.52.523l-5.357 8.152-2.693-3.12a1.327 1.327 0 00-1.05-.476c-.39 0-.714.121-.97.364-.255.243-.383.55-.383.919 0 .163.029.319.087.466.058.147.162.302.311.466l3.518 4.023c.318.355.719.533 1.201.533z"
+            fillRule="evenodd"
+            d="M8 1a7 7 0 110 14A7 7 0 018 1zm0 6a1 1 0 110 2 1 1 0 010-2zm3 0a1 1 0 110 2 1 1 0 010-2zM5 7a1 1 0 110 2 1 1 0 010-2z"
         />
     </AccessibleSVG>
 );
 
-export default Checkmark16;
+export default InProgress16;
