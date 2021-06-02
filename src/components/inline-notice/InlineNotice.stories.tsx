6052450759b7c5c2fad6f66b99f6445a0b062447
Alex Plyushchay <aplyushchay@box.com>
2021-06-02 12:47:37 -0400

    refactor(typescript): Fix stories

--- src/components/inline-notice/InlineNotice.stories.tsx
+++ src/components/inline-notice/InlineNotice.stories.tsx
@@ -4,53 +4,53 @@ import notes from './InlineNotice.stories.md';
 
 export const defaultInlineNotice = () => (
     <>
-        <InlineNotice>This is a default inline notice</InlineNotice>
         <InlineNotice title="Default">This is a default inline notice with title</InlineNotice>
+        <InlineNotice>This is a default inline notice</InlineNotice>
     </>
 );
 
 export const successInlineNotice = () => (
     <>
-        <InlineNotice type={InlineNoticeType.SUCCESS}>This is a success inline notice</InlineNotice>
         <InlineNotice title="Success" type={InlineNoticeType.SUCCESS}>
             This is a success inline notice with title
         </InlineNotice>
+        <InlineNotice type={InlineNoticeType.SUCCESS}>This is a success inline notice</InlineNotice>
     </>
 );
 
 export const infoInlineNotice = () => (
     <>
-        <InlineNotice type={InlineNoticeType.INFO}>This is an info inline notice</InlineNotice>
         <InlineNotice title="Info" type={InlineNoticeType.INFO}>
             This is an info inline notice with title
         </InlineNotice>
+        <InlineNotice type={InlineNoticeType.INFO}>This is an info inline notice</InlineNotice>
     </>
 );
 
 export const warningInlineNotice = () => (
     <>
-        <InlineNotice type={InlineNoticeType.WARNING}>This is a warning inline notice</InlineNotice>
         <InlineNotice title="Warning" type={InlineNoticeType.WARNING}>
             This is a warning inline notice with title
         </InlineNotice>
+        <InlineNotice type={InlineNoticeType.WARNING}>This is a warning inline notice</InlineNotice>
     </>
 );
 
 export const errorInlineNotice = () => (
     <>
-        <InlineNotice type={InlineNoticeType.ERROR}>This is an error inline notice</InlineNotice>
         <InlineNotice title="Error" type={InlineNoticeType.ERROR}>
             This is an error inline notice with title
         </InlineNotice>
+        <InlineNotice type={InlineNoticeType.ERROR}>This is an error inline notice</InlineNotice>
     </>
 );
 
 export const genericInlineNotice = () => (
     <>
-        <InlineNotice type={InlineNoticeType.GENERIC}>This is a generic inline notice</InlineNotice>
         <InlineNotice title="Generic" type={InlineNoticeType.GENERIC}>
             This is a generic inline notice with title
         </InlineNotice>
+        <InlineNotice type={InlineNoticeType.GENERIC}>This is a generic inline notice</InlineNotice>
     </>
 );
 
