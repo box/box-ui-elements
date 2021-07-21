91136d796085ec4b1d46e3a65a04d086c8a65a30
Hormaza <matiasbecerra@box.com>
2021-07-21 16:53:02 -0300

    fix(metadata): autofocus when delete metadata

--- src/features/metadata-instance-editor/MetadataInstanceConfirmDialog.js
+++ src/features/metadata-instance-editor/MetadataInstanceConfirmDialog.js
@@ -15,20 +15,33 @@ type Props = {
     onConfirm: Function,
 };
 
-const MetadataInstanceConfirmDialog = ({ onCancel, onConfirm, confirmationMessage }: Props) => (
-    <div className="metadata-instance-confirm-cover">
-        <div className="metadata-instance-confim-container">
-            <p className="metadata-instance-confirm-text">{confirmationMessage}</p>
-            <div className="metadata-instance-confirm-buttons">
-                <Button data-resin-target="metadata-confirmcancel" onClick={onCancel} type="button">
-                    <FormattedMessage {...messages.metadataCancel} />
-                </Button>
-                <PrimaryButton data-resin-target="metadata-confirmremove" onClick={onConfirm} type="button">
-                    <FormattedMessage {...messages.customRemove} />
-                </PrimaryButton>
+const MetadataInstanceConfirmDialog = ({ onCancel, onConfirm, confirmationMessage }: Props) => {
+    const confirmationMessageElement = React.useRef(null);
+
+    React.useEffect(() => {
+        if (confirmationMessageElement.current) {
+            confirmationMessageElement.current.focus();
+        }
+    });
+
+    return (
+        <div className="metadata-instance-confirm-cover">
+            <div className="metadata-instance-confim-container">
+                {/* eslint-disable-next-line jsx-a11y/no-noninteractive-tabindex */}
+                <div role="alert" tabIndex="0" ref={confirmationMessageElement}>
+                    <p className="metadata-instance-confirm-text">{confirmationMessage}</p>
+                </div>
+                <div className="metadata-instance-confirm-buttons">
+                    <Button data-resin-target="metadata-confirmcancel" onClick={onCancel} type="button">
+                        <FormattedMessage {...messages.metadataCancel} />
+                    </Button>
+                    <PrimaryButton data-resin-target="metadata-confirmremove" onClick={onConfirm} type="button">
+                        <FormattedMessage {...messages.customRemove} />
+                    </PrimaryButton>
+                </div>
             </div>
         </div>
-    </div>
-);
+    );
+};
 
 export default MetadataInstanceConfirmDialog;
