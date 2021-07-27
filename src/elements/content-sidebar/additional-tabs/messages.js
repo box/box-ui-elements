252eeaacff9cc06f3d03687cd268a0cde2918b45
Daniel Sosebee <35543368+dnsosebee@users.noreply.github.com>
2021-07-27 11:09:57 -0700

    fix: update shield tooltip wording (#2643)

--- src/elements/content-sidebar/additional-tabs/messages.js
+++ src/elements/content-sidebar/additional-tabs/messages.js
@@ -9,7 +9,7 @@ import { defineMessages } from 'react-intl';
 const messages = defineMessages({
     blockedByShieldAccessPolicy: {
         id: 'be.additionalTab.blockedByShieldAccessPolicy',
-        defaultMessage: 'Use of this app has been disabled by the applied access policy',
+        defaultMessage: 'Use of this app is blocked due to a security policy.',
         description: 'Text to display when app is disabled by applied access policy',
     },
 });
