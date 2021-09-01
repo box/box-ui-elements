2f7e7a54a3ffedfc385b34bd531f5b62367b4bf2
Jack <yuxi.chen@berkeley.edu>
2021-09-01 13:52:24 -0700

    feat(shared-link-permission-menu): update message

--- src/features/unified-share-modal/messages.js
+++ src/features/unified-share-modal/messages.js
@@ -208,11 +208,11 @@ const messages = defineMessages({
         description: 'Label for a shared link permission level',
         id: 'boxui.unifiedShare.sharedLinkPermissionsViewOnly',
     },
-    ftuxSharedLinkPermissionsEdit: {
+    ftuxSharedLinkPermissionsEditTag: {
         defaultMessage: 'NEW',
         description:
             'Label for the LabelPill that is shown when the user first opens the SharedLinkPermissions dropdown and sees the Can Edit option',
-        id: 'boxui.unifiedShare.ftuxSharedLinkPermissionsEdit',
+        id: 'boxui.unifiedShare.ftuxSharedLinkPermissionsEditTag',
     },
     sharedLinkPermissionsEdit: {
         defaultMessage: 'Can edit',
