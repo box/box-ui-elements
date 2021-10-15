9364dc6ee625b6b6a93a603f7c97784ae62c1acc
msoriano <msoriano@box.com>
2021-10-15 16:31:31 -0500

    fix(alerts): Improved Color Contrast for accessibility

--- src/components/notification/Notification.js
+++ src/components/notification/Notification.js
@@ -2,6 +2,7 @@
 import * as React from 'react';
 import { defineMessages, injectIntl } from 'react-intl';
 import classNames from 'classnames';
+import { bdlGray } from '../../styles/variables';
 
 import IconAlertCircle from '../../icons/general/IconAlertCircle';
 import IconBell from '../../icons/general/IconBell';
@@ -107,7 +108,7 @@ class Notification extends React.Component<Props> {
         return (
             <div className={classes}>
                 {React.cloneElement(ICON_RENDERER[type](), {
-                    color: '#fff',
+                    color: bdlGray,
                     height: 20,
                     width: 20,
                 })}
@@ -118,7 +119,7 @@ class Notification extends React.Component<Props> {
                     onClick={this.onClose}
                     type="button"
                 >
-                    <IconClose color="#FFF" height={18} width={18} />
+                    <IconClose color={bdlGray} height={18} width={18} />
                 </button>
             </div>
         );
