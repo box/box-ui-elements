ee1bcd1ab544ee533880e4b4bcf0ee2a8741000b
Darya Mazaleuskaya <dashayarohovich@mail.ru>
2022-01-14 14:24:06 +0100

    feat(assets): added icons for retentions

---
+++ src/icon/content/RetentionPolicyNonModifiable32.stories.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+import RetentionPolicyNonModifiable32 from './RetentionPolicyNonModifiable32';
+
+export const retentionPolicyNonModifiable32 = () => <RetentionPolicyNonModifiable32 />;
+
+export default {
+    title: 'Icon|Content|RetentionPolicyNonModifiable32',
+    component: RetentionPolicyNonModifiable32,
+    parameters: {
+        notes:
+            "`import RetentionPolicyNonModifiable32 from 'box-ui-elements/es/icon/content/RetentionPolicyNonModifiable32';`",
+    },
+};
