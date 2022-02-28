0eeaa5612df2ba8bde43b524ef502a93173584d8
karelee7 <95440409+karelee7@users.noreply.github.com>
2022-02-28 09:20:52 -0800

    fix(presence-collaborators-list): simplify presence collaborators list (#2834)
    Fix for simplifying the presence collaborators list into a reusable sub-component to be used in content analytics.

--- scripts/findReactIntlViolators.js
+++ scripts/findReactIntlViolators.js
@@ -9,7 +9,7 @@ const features = 'formatRelativeTime|FormattedRelativeTime|createIntl|useIntl';
 const allowList = [
     'src/components/time/ReadableTime.js',
     'src/features/presence/PresenceAvatarTooltipContent.tsx',
-    'src/features/presence/PresenceCollaboratorsList.js',
+    'src/features/presence/PresenceCollaborator.js',
     'src/features/virtualized-table-renderers/lastModifiedByCellRenderer.js',
 ];
 function getFilesInDirectory(dir) {
---
+++ src/features/presence/PresenceCollaborator.js
@@ -0,0 +1,81 @@
+import * as React from 'react';
+import isEmpty from 'lodash/isEmpty';
+import PropTypes from 'prop-types';
+import { FormattedMessage, injectIntl } from 'react-intl';
+
+import Link from '../../components/link/LinkBase';
+import messages from './messages';
+import PresenceAvatar from './PresenceAvatar';
+import { determineInteractionMessage } from './utils/presenceUtils';
+
+import './PresenceCollaborator.scss';
+
+export const renderTimestampMessage = (interactedAt, interactionType, intl) => {
+    const lastActionMessage = determineInteractionMessage(interactionType, interactedAt);
+    const timeAgo = intl.formatRelativeTime
+        ? intl.formatRelativeTime(interactedAt - Date.now())
+        : intl.formatRelative(interactedAt);
+
+    if (lastActionMessage) {
+        return (
+            <FormattedMessage
+                {...lastActionMessage}
+                values={{
+                    timeAgo,
+                }}
+            />
+        );
+    }
+    return null;
+};
+
+const PresenceCollaborator = ({ collaborator, intl, ...props }) => {
+    const { avatarUrl, id, interactedAt, interactionType, isActive, name, profileUrl } = collaborator;
+
+    return (
+        <div className="bdl-PresenceCollaborator" {...props}>
+            <PresenceAvatar avatarUrl={avatarUrl} id={id} isActive={isActive} isDropDownAvatar name={name} />
+            <div className="bdl-PresenceCollaborator-info-container">
+                <div className="bdl-PresenceCollaborator-info-name">
+                    {isEmpty(profileUrl) ? (
+                        <span>{name}</span>
+                    ) : (
+                        <Link href={profileUrl} target="_blank">
+                            {name}
+                        </Link>
+                    )}
+                </div>
+                <div className="bdl-PresenceCollaborator-info-time">
+                    {isActive ? (
+                        <FormattedMessage {...messages.activeNowText} />
+                    ) : (
+                        renderTimestampMessage(interactedAt, interactionType, intl)
+                    )}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+PresenceCollaborator.propTypes = {
+    collaborator: PropTypes.shape({
+        /** Url to avatar image. If passed in, component will render the avatar image instead of the initials */
+        avatarUrl: PropTypes.string,
+        /** Users id */
+        id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+        isActive: PropTypes.bool,
+        /** Unix timestamp of when the user last interacted with the document */
+        interactedAt: PropTypes.number,
+        /** The type of interaction by the user */
+        interactionType: PropTypes.string,
+        /** User's full name */
+        name: PropTypes.string.isRequired,
+        /** Custom Profile URL */
+        profileUrl: PropTypes.string,
+    }).isRequired,
+    /* Intl object */
+    intl: PropTypes.any,
+};
+
+export { PresenceCollaborator as PresenceCollaboratorComponent };
+export default injectIntl(PresenceCollaborator);
