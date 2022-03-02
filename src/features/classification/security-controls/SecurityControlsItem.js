81f09c8ece2133bbb5397d1a09283e5ade3e0f5e
Bao Xie <bxie@box.com>
2022-03-01 17:22:55 -0800

    feat(classification): update security controls watermarking copies

--- src/features/classification/security-controls/SecurityControlsItem.js
+++ src/features/classification/security-controls/SecurityControlsItem.js
@@ -13,22 +13,25 @@ type Props = MessageItem;
 
 const ICON_SIZE = 13;
 
-const SecurityControlsItem = ({ message, tooltipMessage }: Props) => (
-    <li className="bdl-SecurityControlsItem">
-        <FormattedMessage {...message} />
-        {tooltipMessage && (
-            <Tooltip
-                className="bdl-SecurityControlsItem-tooltip"
-                text={<FormattedMessage {...tooltipMessage} />}
-                position="middle-right"
-                isTabbable={false}
-            >
-                <span className="bdl-SecurityControlsItem-tooltipIcon">
-                    <IconInfo color={bdlBoxBlue} width={ICON_SIZE} height={ICON_SIZE} />
-                </span>
-            </Tooltip>
-        )}
-    </li>
-);
+const SecurityControlsItem = ({ message, tooltipMessage }: Props) => {
+    return (
+        <li className="bdl-SecurityControlsItem">
+            {/* $FlowFixMe */}
+            {React.isValidElement(message) ? message : <FormattedMessage {...message} />}
+            {tooltipMessage && (
+                <Tooltip
+                    className="bdl-SecurityControlsItem-tooltip"
+                    text={<FormattedMessage {...tooltipMessage} />}
+                    position="middle-right"
+                    isTabbable={false}
+                >
+                    <span className="bdl-SecurityControlsItem-tooltipIcon">
+                        <IconInfo color={bdlBoxBlue} width={ICON_SIZE} height={ICON_SIZE} />
+                    </span>
+                </Tooltip>
+            )}
+        </li>
+    );
+};
 
 export default SecurityControlsItem;
