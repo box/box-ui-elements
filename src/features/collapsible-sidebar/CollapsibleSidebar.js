e638169da6fdda4be617ad9852a02a8c4ba1f794
John Camden <jcamden@box.com>
2022-03-04 12:45:10 -0500

    fix(collapsible-sidebar): remove incorrect scss import

--- src/features/collapsible-sidebar/CollapsibleSidebar.js
+++ src/features/collapsible-sidebar/CollapsibleSidebar.js
@@ -13,7 +13,6 @@ import tabbable from 'tabbable';
 import { KEYS } from '../../constants';
 
 import './CollapsibleSidebar.scss';
-import 'styles/modifiers/_visibility.scss';
 
 const StyledNav = styled.nav`
     background-color: ${props => props.theme.primary.background};
@@ -93,7 +92,7 @@ class CollapsibleSidebar extends React.Component<Props> {
             'bdl-CollapsibleSidebar',
             className,
         );
-        const wrapperClasses = classNames('bdl-CollapsibleSidebar-wrapper', { hidden: isHidden });
+        const wrapperClasses = classNames('bdl-CollapsibleSidebar-wrapper', { 'is-hidden': isHidden });
         const ariaAttributes = { 'aria-hidden': isHidden ? 'true' : undefined };
         return (
             <div
