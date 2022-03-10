e9d5a229b69e510a3d07323af4d1a33185714d07
Mick Ryan <mickryan@box.com>
2022-03-10 13:38:39 -0500

    fix(usm): Email addresses should not be marked as external due to case
    * Add lowercase normalization to email when merging new contacts with existing contacts
    * Add test for different case email
    * Test when an entry does not match and does not contain an id

--- src/features/unified-share-modal/utils/mergeContacts.js
+++ src/features/unified-share-modal/utils/mergeContacts.js
@@ -5,7 +5,8 @@ import type { contactType as Contact } from '../flowTypes';
 const mergeContacts = (existingContacts: Array<Contact>, fetchedContacts: Object): Array<Contact> => {
     const contactsMap = Object.keys(fetchedContacts).reduce((map, email) => {
         const contact = fetchedContacts[email];
-        map[email] = { ...contact, text: contact.name, value: contact.email || contact.id };
+        // Since objects are case-sensitive, normalize the key to lowercase.
+        map[email.toLowerCase()] = { ...contact, text: contact.name, value: contact.email || contact.id };
         return map;
     }, {});
 
@@ -14,7 +15,8 @@ const mergeContacts = (existingContacts: Array<Contact>, fetchedContacts: Object
             return contact;
         }
         return (
-            (contact.value && contactsMap[contact.value]) || {
+            // Normalize the getter in contactsMap so that matching existing contacts will be case-insensitive
+            (contact.value && contactsMap[contact.value.toLowerCase()]) || {
                 email: String(contact.value),
                 id: String(contact.value),
                 isExternalUser: true,
