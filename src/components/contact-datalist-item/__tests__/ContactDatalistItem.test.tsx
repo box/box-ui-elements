28900e102d7d6d440d14d85d7dc1227e10a36834
Greg Wong <gregorywong@box.com>
2022-05-26 21:27:59 -0400

    refactor(typescript): Migrated ContactDataListItem to typescript

--- src/components/contact-datalist-item/__tests__/ContactDatalistItem.test.tsx
+++ src/components/contact-datalist-item/__tests__/ContactDatalistItem.test.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
+import { shallow } from 'enzyme';
 
 import ContactDatalistItem from '../ContactDatalistItem';
+import Avatar from '../../avatar';
+
+import { ContactType } from '../../../features/unified-share-modal/types';
 
 describe('components/contact-datalist-item/ContactDatalistItem', () => {
     test('should render a DatalistItem with name and subtitle', () => {
@@ -9,21 +13,21 @@ describe('components/contact-datalist-item/ContactDatalistItem', () => {
         expect(wrapper.find('DatalistItem').length).toBe(1);
         expect(wrapper.find('.contact-name').text()).toEqual('name');
         expect(wrapper.find('.contact-sub-name').text()).toEqual('subtitle');
-        expect(wrapper.find('Avatar').length).toBe(0);
+        expect(wrapper.find(Avatar).length).toBe(0);
     });
 
     test('should not render a subtitle when not provided', () => {
         const wrapper = shallow(<ContactDatalistItem name="name" />);
 
         expect(wrapper.find('.contact-sub-name').length).toBe(0);
-        expect(wrapper.find('Avatar').length).toBe(0);
+        expect(wrapper.find(Avatar).length).toBe(0);
     });
 
     describe('avatars with or without image URLs', () => {
         test('should show avatar component when specified', () => {
             const wrapper = shallow(<ContactDatalistItem name="name" showAvatar />);
 
-            expect(wrapper.find('Avatar').length).toBe(1);
+            expect(wrapper.find(Avatar).length).toBe(1);
         });
 
         test('should do nothing when getPillImageUrl returns a rejected Promise', () => {
@@ -35,38 +39,32 @@ describe('components/contact-datalist-item/ContactDatalistItem', () => {
                     getPillImageUrl={() => Promise.reject(new Error())}
                 />,
             );
-            expect(wrapper.state('avatarUrl')).toBe(undefined);
+
             const instance = wrapper.instance();
+            instance.componentDidMount?.();
 
-            instance.componentDidMount();
-
-            setImmediate(() => {
-                wrapper.update();
-                expect(wrapper.state('avatarUrl')).toBeUndefined();
-                expect(wrapper.find('Avatar').length).toBe(1);
-                expect(wrapper.find('Avatar').props().avatarUrl).toBeUndefined();
-            });
+            wrapper.update();
+            expect(wrapper.find(Avatar).length).toBe(1);
+            expect(wrapper.find(Avatar).props().avatarUrl).toBeUndefined();
         });
 
-        test.each([[contact => `/test?id=${contact.id}`], [contact => Promise.resolve(`/test?id=${contact.id}`)]])(
-            'should use the avatar URL when the prop (and show avatar) are provided',
-            getContactAvatarUrl => {
-                const wrapper = shallow(
-                    <ContactDatalistItem name="name" id="123" showAvatar getContactAvatarUrl={getContactAvatarUrl} />,
-                );
-                expect(wrapper.state('avatarUrl')).toBeUndefined();
-                const instance = wrapper.instance();
+        test.each([
+            [(contact: ContactType) => `/test?id=${contact.id}`],
+            [(contact: ContactType) => Promise.resolve(`/test?id=${contact.id}`)],
+        ])('should use the avatar URL when the prop (and show avatar) are provided', async getContactAvatarUrl => {
+            const wrapper = shallow(
+                <ContactDatalistItem name="name" id="123" showAvatar getContactAvatarUrl={getContactAvatarUrl} />,
+            );
+            expect(wrapper.find(Avatar).length).toBe(1);
+            expect(wrapper.find(Avatar).props().avatarUrl).toBeUndefined();
 
-                instance.componentDidMount();
+            const instance = await wrapper.instance();
+            instance.componentDidMount?.();
 
-                setImmediate(() => {
-                    wrapper.update();
-                    expect(wrapper.state('avatarUrl')).toBe('/test?id=123');
-                    expect(wrapper.find('Avatar').length).toBe(1);
-                    expect(wrapper.find('Avatar').props().avatarUrl).toEqual('/test?id=123');
-                });
-            },
-        );
+            wrapper.update();
+            expect(wrapper.find(Avatar).length).toBe(1);
+            expect(wrapper.find(Avatar).props().avatarUrl).toEqual('/test?id=123');
+        });
 
         test('should not have the avatar URL when the id prop is missing', () => {
             const wrapper = shallow(
@@ -77,8 +75,8 @@ describe('components/contact-datalist-item/ContactDatalistItem', () => {
                 />,
             );
 
-            expect(wrapper.find('Avatar').length).toBe(1);
-            expect(wrapper.find('Avatar').props().avatarUrl).toBeUndefined();
+            expect(wrapper.find(Avatar).length).toBe(1);
+            expect(wrapper.find(Avatar).props().avatarUrl).toBeUndefined();
         });
     });
 });
