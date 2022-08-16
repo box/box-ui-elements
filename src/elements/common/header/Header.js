06071790c8c65038805949460337c8b999d201bd
Ygor Oliveira Do Carmo <ygoro@look.com.au>
2022-08-16 12:57:31 +1000

    feat(content explorer): Content Explorer Search By Created At Date

--- src/elements/common/header/Header.js
+++ src/elements/common/header/Header.js
@@ -10,24 +10,36 @@ import Logo from './Logo';
 import messages from '../messages';
 import { VIEW_FOLDER, VIEW_SEARCH } from '../../../constants';
 import type { View } from '../../../common/types/core';
+import { Flyout, Overlay } from '../../../components/flyout';
+import { DatePicker } from '../../../components';
+import Button from '../../../components/button';
 
 import './Header.scss';
 
 type Props = {
+    fromDateQuery?: string,
     intl: any,
     isHeaderLogoVisible?: boolean,
     isSmall: boolean,
     logoUrl?: string,
     onSearch: Function,
     searchQuery: string,
+    toDateQuery?: string,
     view: View,
 };
 
 // eslint-disable-next-line react/prop-types
-const Header = ({ isHeaderLogoVisible = true, view, isSmall, searchQuery, onSearch, logoUrl, intl }: Props) => {
-    const search = ({ currentTarget }: { currentTarget: HTMLInputElement }) => onSearch(currentTarget.value);
+const Header = ({ isHeaderLogoVisible = true, view, isSmall, onSearch, logoUrl, intl }: Props) => {
     const isFolder = view === VIEW_FOLDER;
     const isSearch = view === VIEW_SEARCH;
+
+    const [values, setValues] = React.useState({ query: '', fromDate: null, toDate: null });
+    const search = (query, fromDate, toDate) => onSearch(query, fromDate, toDate);
+
+    const MAX_TIME = new Date('3000-01-01T00:00:00.000Z');
+    const MIN_TIME = new Date(0);
+    const TODAY = new Date();
+
     return (
         <div className="be-header">
             {isHeaderLogoVisible && <Logo isSmall={isSmall} url={logoUrl} />}
@@ -35,12 +47,61 @@ const Header = ({ isHeaderLogoVisible = true, view, isSmall, searchQuery, onSear
                 <input
                     aria-label="search"
                     disabled={!isFolder && !isSearch}
-                    onChange={search}
+                    onChange={e => {
+                        setValues({ ...values, query: e.currentTarget.value });
+                        search(e.currentTarget.value, values.fromDate, values.toDate);
+                    }}
                     placeholder={intl.formatMessage(messages.searchPlaceholder)}
                     type="search"
-                    value={searchQuery}
+                    value={values.query}
                 />
             </div>
+            <Flyout position="middle-left">
+                <Button>Search Options</Button>
+                <Overlay>
+                    <div className="accessible-overlay-content">
+                        <div>
+                            <DatePicker
+                                className="date-picker-example"
+                                displayFormat={{
+                                    day: 'numeric',
+                                    month: 'long',
+                                    year: 'numeric',
+                                }}
+                                hideOptionalLabel
+                                label="From Date"
+                                maxDate={values.toDate || MAX_TIME}
+                                name="datepicker-from"
+                                onChange={data => {
+                                    setValues({ ...values, fromDate: data });
+                                    search(values.query, data, values.toDate);
+                                }}
+                                placeholder="Choose a Date"
+                                value={values.fromDate}
+                            />
+                            <DatePicker
+                                className="date-picker-example"
+                                displayFormat={{
+                                    day: 'numeric',
+                                    month: 'long',
+                                    year: 'numeric',
+                                }}
+                                hideOptionalLabel
+                                label="To Date"
+                                minDate={values.fromDate || MIN_TIME}
+                                maxDate={TODAY}
+                                name="datepicker-to"
+                                onChange={data => {
+                                    setValues({ ...values, toDate: data });
+                                    search(values.query, values.fromDate, data);
+                                }}
+                                placeholder="Choose a Date"
+                                value={values.toDate}
+                            />
+                        </div>
+                    </div>
+                </Overlay>
+            </Flyout>
         </div>
     );
 };
