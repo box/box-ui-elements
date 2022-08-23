903c24a89b8cb01e4f8a114a307a89fa07ecb42b
Andre <aassadi@box.com>
2022-08-23 09:41:34 -0700

    feat: added styleguide

--- scripts/styleguide.config.js
+++ scripts/styleguide.config.js
@@ -89,6 +89,7 @@ const allSections = [
             '../src/components/button-group/ButtonGroup.tsx',
             '../src/components/category-selector/CategorySelector.tsx',
             '../src/components/collapsible/Collapsible.tsx',
+            '../src/components/sidebar-collapsible/SidebarCollapsible.tsx',
             '../src/components/context-menu/ContextMenu.tsx',
             '../src/components/datalist-item/DatalistItem.tsx',
             '../src/components/label/Label.tsx',
