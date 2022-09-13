4363c5618935b9be7990e92c2973ec6329bcf1ba
Wioletta Piesiak <wioletta.piesiak@gmail.com>
2022-09-13 10:22:42 +0200

    feat(annotattion-thread): create new annotation

--- src/elements/content-sidebar/activity-feed/annotation-thread/AnnotationThread.js
+++ src/elements/content-sidebar/activity-feed/annotation-thread/AnnotationThread.js
@@ -5,15 +5,18 @@
  */
 
 import React from 'react';
+import debounce from 'lodash/debounce';
 import classNames from 'classnames';
 import { IntlProvider } from 'react-intl';
 import AnnotationThreadContent from './AnnotationThreadContent';
+import AnnotationThreadCreate from './AnnotationThreadCreate';
 import API from '../../../../api/APIFactory';
-import { DEFAULT_HOSTNAME_API } from '../../../../constants';
+import { DEFAULT_COLLAB_DEBOUNCE, DEFAULT_HOSTNAME_API } from '../../../../constants';
 
 import type APICache from '../../../../utils/Cache';
-import type { BoxItemPermission, StringMap, Token } from '../../../../common/types/core';
+import type { BoxItem, SelectorItems, StringMap, Token, User } from '../../../../common/types/core';
 import type { ErrorContextProps } from '../../../../common/types/api';
+import type { Target } from '../../../../common/types/annotations';
 
 import './AnnotationThread.scss';
 
@@ -23,10 +26,13 @@ type Props = {
     cache?: APICache,
     className: string,
     clientName: string,
-    fileId: string,
-    filePermissions: BoxItemPermission,
+    currentUser: User,
+    file: BoxItem,
+    handleCancel: Function,
     language?: string,
     messages?: StringMap,
+    onAnnotationCreate: Function,
+    target: Target,
     token: Token,
 } & ErrorContextProps;
 
@@ -36,17 +42,16 @@ const AnnotationThread = ({
     cache,
     className = '',
     clientName,
-    fileId,
-    filePermissions,
+    currentUser,
+    file,
+    handleCancel,
     language,
     messages,
+    onAnnotationCreate,
     onError,
+    target,
     token,
 }: Props) => {
-    if (!annotationId) {
-        return null;
-    }
-
     const api = new API({
         apiHost,
         cache,
@@ -55,16 +60,52 @@ const AnnotationThread = ({
         token,
     });
 
+    const [mentionSelectorContacts, setMentionSelectorContacts] = React.useState([]);
+
+    const getAvatarUrl = async (userId: string): Promise<?string> =>
+        api.getUsersAPI(false).getAvatarUrlWithAccessToken(userId, file.id);
+
+    const getMentionContactsSuccessCallback = (collaborators: { entries: SelectorItems<> }): void => {
+        setMentionSelectorContacts(collaborators.entries);
+    };
+
+    const getMentionWithQuery = debounce(searchStr => {
+        api.getFileCollaboratorsAPI(false).getCollaboratorsWithQuery(
+            file.id,
+            getMentionContactsSuccessCallback,
+            onError,
+            searchStr,
+        );
+    }, DEFAULT_COLLAB_DEBOUNCE);
+
     return (
         <div className={classNames('AnnotationThread', className)} data-testid="annotation-thread">
             <IntlProvider locale={language} messages={messages}>
-                <AnnotationThreadContent
-                    api={api}
-                    annotationId={annotationId}
-                    fileId={fileId}
-                    filePermissions={filePermissions}
-                    onError={onError}
-                />
+                {!annotationId ? (
+                    <AnnotationThreadCreate
+                        api={api}
+                        currentUser={currentUser}
+                        file={file}
+                        getAvatarUrl={getAvatarUrl}
+                        getMentionWithQuery={getMentionWithQuery}
+                        handleCancel={handleCancel}
+                        mentionSelectorContacts={mentionSelectorContacts}
+                        onAnnotationCreate={onAnnotationCreate}
+                        onError={onError}
+                        target={target}
+                    />
+                ) : (
+                    <AnnotationThreadContent
+                        api={api}
+                        annotationId={annotationId}
+                        currentUser={currentUser}
+                        file={file}
+                        getAvatarUrl={getAvatarUrl}
+                        getMentionWithQuery={getMentionWithQuery}
+                        mentionSelectorContacts={mentionSelectorContacts}
+                        onError={onError}
+                    />
+                )}
             </IntlProvider>
         </div>
     );
--- src/elements/content-sidebar/activity-feed/annotation-thread/__tests__/AnnotationThread.test.js
+++ src/elements/content-sidebar/activity-feed/annotation-thread/__tests__/AnnotationThread.test.js
@@ -1,18 +1,28 @@
 // @flow
 import { render } from '@testing-library/react';
 import React from 'react';
+import { IntlProvider } from 'react-intl';
 import AnnotationThread from '../AnnotationThread';
 
-describe('elements/content-sidebar/activity-feed/annotation-thread/AnnotationThread', () => {
-    const getWrapper = (props = {}) => render(<AnnotationThread {...props} />);
+jest.mock('react-intl', () => jest.requireActual('react-intl'));
+jest.mock('../AnnotationThreadContent', () => () => <div data-testid="annotation-content" />);
+jest.mock('../AnnotationThreadCreate', () => () => <div data-testid="annotation-create" />);
 
-    test('should not render if annotationId is undefined', async () => {
-        const { container } = getWrapper();
-        expect(container).toBeEmptyDOMElement();
+describe('elements/content-sidebar/activity-feed/annotation-thread/AnnotationThread', () => {
+    const IntlWrapper = ({ children }: { children?: React.ReactNode }) => {
+        return <IntlProvider locale="en">{children}</IntlProvider>;
+    };
+    const getWrapper = props => render(<AnnotationThread {...props} />, { wrapper: IntlWrapper });
+
+    test('should render AnnotationThreadCreate if annotationId is undefined', async () => {
+        const { getByTestId } = getWrapper();
+        expect(getByTestId('annotation-thread')).toBeInTheDocument();
+        expect(getByTestId('annotation-create')).toBeInTheDocument();
     });
 
-    test('should render properly', () => {
+    test('should render AnnotationThreadContent if annotationId is defined', () => {
         const { getByTestId } = getWrapper({ annotationId: '1' });
         expect(getByTestId('annotation-thread')).toBeInTheDocument();
+        expect(getByTestId('annotation-content')).toBeInTheDocument();
     });
 });
