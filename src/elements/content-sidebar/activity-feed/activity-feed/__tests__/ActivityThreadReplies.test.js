3ffa0027217bf97993c1a0d9df371960f5508100
Wioletta Piesiak <wioletta.piesiak@gmail.com>
2022-09-16 14:21:01 +0200

    feat(threaded-replies): fix typos

--- src/elements/content-sidebar/activity-feed/activity-feed/__tests__/ActivityThreadReplies.test.js
+++ src/elements/content-sidebar/activity-feed/activity-feed/__tests__/ActivityThreadReplies.test.js
@@ -8,17 +8,17 @@ import { replies } from '../fixtures';
 describe('src/elements/content-sidebar/activity-feed/activity-feed/ActivityThreadReplies', () => {
     const getWrapper = props => render(<ActivityThreadReplies replies={replies} isExpanded={false} {...props} />);
 
-    test('should render last reply by dafault', () => {
+    test('should render last reply by default', () => {
         const { queryByText } = getWrapper();
 
-        expect(queryByText('Last reply')).toBeVisible();
-        expect(queryByText('Firsy reply')).not.toBeInTheDocument();
+        expect(queryByText(replies[1].message)).toBeVisible();
+        expect(queryByText(replies[0].message)).not.toBeInTheDocument();
     });
 
     test('should render all replies if isExpanded is true', () => {
         const { queryByText } = getWrapper({ isExpanded: true });
 
-        expect(queryByText('Last reply')).toBeVisible();
-        expect(queryByText('First reply')).toBeVisible();
+        expect(queryByText(replies[1].message)).toBeVisible();
+        expect(queryByText(replies[0].message)).toBeVisible();
     });
 });
