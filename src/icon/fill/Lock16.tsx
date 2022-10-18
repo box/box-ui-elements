b7910b2f7bdc8fecd62a9f0322e6f38d746e5403
Ivan Thai <ithai@box.com>
2022-10-18 16:22:04 -0700

    feat(icons): add addl props to Lock16 icon for customization

--- src/icon/fill/Lock16.tsx
+++ src/icon/fill/Lock16.tsx
@@ -1,7 +1,8 @@
 /* eslint-disable react/jsx-sort-props */
 import * as React from 'react';
 import * as vars from '../../styles/variables';
-import AccessibleSVG, { SVGProps } from '../../components/accessible-svg/AccessibleSVG';
+import AccessibleSVG from '../../components/accessible-svg/AccessibleSVG';
+import { Icon } from '../../icons/iconTypes';
 
 /**
  * This is an auto-generated component and should not be edited
@@ -14,10 +15,10 @@ import AccessibleSVG, { SVGProps } from '../../components/accessible-svg/Accessi
  * - https://github.com/box/box-ui-elements/issues/new?template=Feature_request.md
  */
 
-const Lock16 = (props: SVGProps) => (
-    <AccessibleSVG width={16} height={16} viewBox="0 0 16 16" {...props}>
+const Lock16 = ({ color = vars.bdlGray50, ...rest }: Icon) => (
+    <AccessibleSVG width={16} height={16} viewBox="0 0 16 16" {...rest}>
         <path
-            fill={vars.bdlGray50}
+            fill={color}
             fillRule="evenodd"
             d="M8 1a4 4 0 013.995 3.8L12 5v1a2 2 0 012 2v5a2 2 0 01-2 2H4a2 2 0 01-2-2V8a2 2 0 012-2V5c0-2.21 1.79-4 4-4zm1.5 9h-3a.5.5 0 00-.09.992L6.5 11h3a.5.5 0 000-1zM8 3a2.001 2.001 0 00-1.995 1.85L6 5v1h4V5a2 2 0 00-2-2z"
         />
