8c13592a0eda284fe26dd2f30402b2672607bd6f
Greg Wong <gregorywong@box.com>
2022-12-06 13:59:39 -0500

    chore(time): feedback

--- src/components/time/ReadableTime.stories.js
+++ src/components/time/ReadableTime.stories.js
@@ -3,48 +3,38 @@ import * as React from 'react';
 import ReadableTime from './ReadableTime';
 import notes from './ReadableTime.stories.md';
 
-export const relativeTimestamps = () => {
-    const msInADay = 24 * 60 * 60 * 1000;
-    const oneHourInMs = 60 * 60 * 1000;
+const msInADay = 24 * 60 * 60 * 1000;
+const oneHourInMs = 60 * 60 * 1000;
 
-    return (
+export const relativeTimestamps = () => (
+    <div>
         <div>
-            <div>
-                <ReadableTime timestamp={Date.now() - oneHourInMs + 30 * 60 * 1000} relativeThreshold={oneHourInMs} />
-            </div>
-            <div>
-                <ReadableTime timestamp={Date.now() - 2 * oneHourInMs} relativeThreshold={oneHourInMs} />
-            </div>
-            <div>
-                <ReadableTime timestamp={Date.now() - msInADay} relativeThreshold={oneHourInMs} />
-            </div>
+            <ReadableTime timestamp={Date.now() - oneHourInMs + 30 * 60 * 1000} relativeThreshold={oneHourInMs} />
         </div>
-    );
-};
+        <div>
+            <ReadableTime timestamp={Date.now() - 2 * oneHourInMs} relativeThreshold={oneHourInMs} />
+        </div>
+        <div>
+            <ReadableTime timestamp={Date.now() - msInADay} relativeThreshold={oneHourInMs} />
+        </div>
+    </div>
+);
 
-export const dateWithoutTime = () => {
-    const msInADay = 24 * 60 * 60 * 1000;
-    const oneHourInMs = 60 * 60 * 1000;
-    return <ReadableTime timestamp={Date.now() - 7 * msInADay} relativeThreshold={oneHourInMs} />;
-};
+export const dateWithoutTime = () => (
+    <ReadableTime timestamp={Date.now() - 7 * msInADay} relativeThreshold={oneHourInMs} />
+);
 
-export const dateWithTime = () => {
-    const msInADay = 24 * 60 * 60 * 1000;
-    const oneHourInMs = 60 * 60 * 1000;
-    return <ReadableTime timestamp={Date.now() - 7 * msInADay} relativeThreshold={oneHourInMs} alwaysShowTime />;
-};
+export const dateWithTime = () => (
+    <ReadableTime timestamp={Date.now() - 7 * msInADay} relativeThreshold={oneHourInMs} alwaysShowTime />
+);
 
-export const dateInTheFutureWhenNotAllowed = () => {
-    const msInADay = 24 * 60 * 60 * 1000;
-    const oneHourInMs = 60 * 60 * 1000;
-    return (
-        <ReadableTime
-            timestamp={Date.now() + 70 * msInADay}
-            relativeThreshold={oneHourInMs}
-            allowFutureTimestamps={false}
-        />
-    );
-};
+export const dateInTheFutureWhenNotAllowed = () => (
+    <ReadableTime
+        timestamp={Date.now() + 70 * msInADay}
+        relativeThreshold={oneHourInMs}
+        allowFutureTimestamps={false}
+    />
+);
 
 export default {
     title: 'Components|ReadableTime',
