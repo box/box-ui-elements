4937f0c457b04fceadd54e2b6a996172eaa20226
Marie Rogers <marie.rogers9784@gmail.com>
2022-12-13 11:32:25 -0800

    fix(content-picker): tooltip bug fix

--- src/elements/content-picker/Footer.js
+++ src/elements/content-picker/Footer.js
@@ -56,60 +56,61 @@ const Footer = ({
     children,
     renderCustomActionButtons,
     showSelectedButton,
-}: Props) => (
-    <footer className="bcp-footer">
-        <div className="bcp-footer-left">
-            {showSelectedButton && !isSingleSelect && (
-                <Button className="bcp-selected" onClick={onSelectedClick} type="button">
-                    <FormattedMessage
-                        className="bcp-selected-count"
-                        {...messages.selected}
-                        values={{ count: selectedCount }}
-                    />
-                    {hasHitSelectionLimit && (
-                        <span className="bcp-selected-max">
-                            (<FormattedMessage {...messages.max} />)
-                        </span>
-                    )}
-                </Button>
-            )}
-        </div>
-        <div className="bcp-footer-right">
-            {children}
+}: Props) => {
+    const cancelMessage = intl.formatMessage(messages.cancel);
+    const chooseMessage = intl.formatMessage(messages.choose);
+    return (
+        <footer className="bcp-footer">
+            <div className="bcp-footer-left">
+                {showSelectedButton && !isSingleSelect && (
+                    <Button className="bcp-selected" onClick={onSelectedClick} type="button">
+                        <FormattedMessage
+                            className="bcp-selected-count"
+                            {...messages.selected}
+                            values={{ count: selectedCount }}
+                        />
+                        {hasHitSelectionLimit && (
+                            <span className="bcp-selected-max">
+                                (<FormattedMessage {...messages.max} />)
+                            </span>
+                        )}
+                    </Button>
+                )}
+            </div>
+            <div className="bcp-footer-right">
+                {children}
 
-            {renderCustomActionButtons ? (
-                renderCustomActionButtons({
-                    currentFolderId: currentCollection.id,
-                    currentFolderName: currentCollection.name,
-                    onCancel,
-                    onChoose,
-                    selectedCount,
-                    selectedItems,
-                })
-            ) : (
-                <ButtonGroup className="bcp-footer-actions">
-                    <Tooltip text={cancelButtonLabel || <FormattedMessage {...messages.cancel} />}>
-                        <Button aria-label={intl.formatMessage(messages.cancel)} onClick={onCancel} type="button">
-                            <IconClose height={16} width={16} />
-                        </Button>
-                    </Tooltip>
-                    <Tooltip
-                        isDisabled={!selectedCount}
-                        text={chooseButtonLabel || <FormattedMessage {...messages.choose} />}
-                    >
-                        <PrimaryButton
-                            aria-label={intl.formatMessage(messages.choose)}
-                            isDisabled={!selectedCount}
-                            onClick={onChoose}
-                            type="button"
-                        >
-                            <IconCheck color="#fff" height={16} width={16} />
-                        </PrimaryButton>
-                    </Tooltip>
-                </ButtonGroup>
-            )}
-        </div>
-    </footer>
-);
+                {renderCustomActionButtons ? (
+                    renderCustomActionButtons({
+                        currentFolderId: currentCollection.id,
+                        currentFolderName: currentCollection.name,
+                        onCancel,
+                        onChoose,
+                        selectedCount,
+                        selectedItems,
+                    })
+                ) : (
+                    <ButtonGroup className="bcp-footer-actions">
+                        <Tooltip text={cancelButtonLabel || cancelMessage}>
+                            <Button aria-label={cancelMessage} onClick={onCancel} type="button">
+                                <IconClose height={16} width={16} />
+                            </Button>
+                        </Tooltip>
+                        <Tooltip isDisabled={!selectedCount} text={chooseButtonLabel || chooseMessage}>
+                            <PrimaryButton
+                                aria-label={chooseMessage}
+                                isDisabled={!selectedCount}
+                                onClick={onChoose}
+                                type="button"
+                            >
+                                <IconCheck color="#fff" height={16} width={16} />
+                            </PrimaryButton>
+                        </Tooltip>
+                    </ButtonGroup>
+                )}
+            </div>
+        </footer>
+    );
+};
 
 export default injectIntl(Footer);
