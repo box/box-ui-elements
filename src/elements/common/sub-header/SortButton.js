8cbe42b84dbb749e391351541dc0c0914dfcb284
dchai <chai.qingyu00@gmail.com>
2022-12-13 11:05:40 -0800

    fix(sub-header): add accessibility label to SortButton

--- src/elements/common/sub-header/SortButton.js
+++ src/elements/common/sub-header/SortButton.js
@@ -5,19 +5,22 @@
  */
 
 import React from 'react';
-import { FormattedMessage } from 'react-intl';
+import { injectIntl } from 'react-intl';
 import Button from '../../../components/button';
 import IconSort from '../../../icons/general/IconSort';
 import messages from '../messages';
 import Tooltip from '../Tooltip';
 import './SortButton.scss';
 
-const SortButton = (props: ?Object) => (
-    <Tooltip text={<FormattedMessage {...messages.sort} />}>
-        <Button className="be-btn-sort" type="button" {...props}>
-            <IconSort />
-        </Button>
-    </Tooltip>
-);
+const SortButton = ({ intl, ...rest }: ?Object) => {
+    const sortMessage = intl.formatMessage(messages.sort);
+    return (
+        <Tooltip text={sortMessage}>
+            <Button aria-label={sortMessage} className="be-btn-sort" type="button" {...rest}>
+                <IconSort />
+            </Button>
+        </Tooltip>
+    );
+};
 
-export default SortButton;
+export default injectIntl(SortButton);
