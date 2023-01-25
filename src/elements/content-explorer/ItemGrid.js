526d51d41329c9b56446a5de3a74daef3da9894e
dchai <chai.qingyu00@gmail.com>
2023-01-24 16:10:34 -0800

    fix(content-explorer): fix lint

--- src/elements/content-explorer/ItemGrid.js
+++ src/elements/content-explorer/ItemGrid.js
@@ -14,6 +14,7 @@ import type { BoxItem, Collection } from '../../common/types/core';
 type Props = {
     currentCollection: Collection,
     gridColumnCount: number,
+    rootElement: HTMLElement,
     ...$Exact<ItemGridProps>,
 };
 
