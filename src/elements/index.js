ed6c1e206f846a664958484b1de93d60a731e985
Greg Wong <gregorywong@box.com>
2023-02-14 18:20:03 -0500

    chore(answers): feedback

--- src/elements/index.js
+++ src/elements/index.js
@@ -1,5 +1,6 @@
 // @flow
 
+export { default as ContentAnswers } from './content-answers';
 export { default as ContentExplorer } from './content-explorer';
 export { default as ContentPreview } from './content-preview';
 export { default as ContentPicker, ContentPickerPopup } from './content-picker';
@@ -7,4 +8,3 @@ export { default as ContentUploader, ContentUploaderPopup } from './content-uplo
 export { default as ContentOpenWith } from './content-open-with';
 export { default as ContentSidebar } from './content-sidebar';
 export { default as ContentSharing } from './content-sharing';
-export { default as ContentAnswers } from './content-answers';
