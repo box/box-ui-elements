18cdf3fdbd757aacfa261dd6c0242be700272aff
Trevor <7311041+tjuanitas@users.noreply.github.com>
2023-02-27 09:50:10 -0800

    fix(avatar): Add aria-label to external user icon (#3263)
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- src/components/avatar/__tests__/Avatar.test.tsx
+++ src/components/avatar/__tests__/Avatar.test.tsx
@@ -1,65 +1,70 @@
 import React from 'react';
 import { shallow, mount } from 'enzyme';
 import { act } from 'react-dom/test-utils';
-import Avatar from '../Avatar';
+import { IntlShape } from 'react-intl';
+import { AvatarBase as Avatar } from '../Avatar';
 import AvatarImage from '../AvatarImage';
 
 const testDataURI = 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7';
 
 describe('components/avatar/Avatar', () => {
+    const intl = {
+        formatMessage: jest.fn().mockImplementation(message => message.defaultMessage) as unknown,
+    } as IntlShape;
+
+    const getWrapper = (props = {}) => shallow(<Avatar intl={intl} {...props} />);
+
     test('should render an avatar container', () => {
-        const wrapper = shallow(<Avatar className="test-avatar" name="hello" />);
+        const wrapper = getWrapper({ className: 'test-avatar', name: 'box' });
         expect(wrapper.is('.avatar.test-avatar')).toBe(true);
     });
 
     test('should add small size class based on prop', () => {
-        const wrapper = shallow(<Avatar name="hello" size="small" />);
+        const wrapper = getWrapper({ name: 'box', size: 'small' });
         expect(wrapper.is('.avatar.avatar--small')).toBe(true);
     });
 
     test('should add large size class based on prop', () => {
-        const wrapper = shallow(<Avatar name="hello" size="large" />);
+        const wrapper = getWrapper({ name: 'box', size: 'large' });
         expect(wrapper.is('.avatar.avatar--large')).toBe(true);
     });
 
     test('should not allow unknown sizes', () => {
-        // eslint-disable-next-line
-        // @ts-ignore testing a wrong value for the size prop
-        const wrapper = shallow(<Avatar name="hello" size="WRONG" />);
+        const wrapper = getWrapper({ name: 'box', size: 'WRONG' });
         expect(wrapper.is('.avatar.avatar--WRONG')).toBe(false);
     });
 
     test('should render an AvatarImage when avatarUrl is passed in', () => {
-        const wrapper = shallow(<Avatar avatarUrl={testDataURI} />);
+        const wrapper = getWrapper({ avatarUrl: testDataURI });
         const avatarImage = wrapper.find(AvatarImage);
-        expect(avatarImage.length).toEqual(1);
-        expect(avatarImage.prop('url')).toEqual(testDataURI);
+        expect(avatarImage.length).toBe(1);
+        expect(avatarImage.prop('url')).toBe(testDataURI);
     });
 
     test('should render an AvatarInitials when name is passed in', () => {
-        const wrapper = shallow(<Avatar id="1" name="hello world" />);
+        const wrapper = getWrapper({ id: '1', name: 'hello world' });
         const avatarInitials = wrapper.find('AvatarInitials');
-        expect(avatarInitials.length).toEqual(1);
-        expect(avatarInitials.prop('name')).toEqual('hello world');
-        expect(avatarInitials.prop('id')).toEqual('1');
+        expect(avatarInitials.length).toBe(1);
+        expect(avatarInitials.prop('name')).toBe('hello world');
+        expect(avatarInitials.prop('id')).toBe('1');
     });
 
     test('should render an UnknownUserAvatar when no params are passed', () => {
-        const wrapper = shallow(<Avatar />);
+        const wrapper = getWrapper();
         const avatarIcon = wrapper.find('UnknownUserAvatar');
-        expect(avatarIcon.length).toEqual(1);
-        expect(avatarIcon.prop('className')).toEqual('avatar-icon');
+        expect(avatarIcon.length).toBe(1);
+        expect(avatarIcon.prop('className')).toBe('avatar-icon');
     });
 
     test('should render an UnknownUserAvatar when empty name and url are passed', () => {
-        const wrapper = shallow(<Avatar avatarUrl={null} id={2} name="" />);
+        const wrapper = getWrapper({ avatarUrl: null, id: 2, name: '' });
         const avatarIcon = wrapper.find('UnknownUserAvatar');
-        expect(avatarIcon.length).toEqual(1);
-        expect(avatarIcon.prop('className')).toEqual('avatar-icon');
+        expect(avatarIcon.length).toBe(1);
+        expect(avatarIcon.prop('className')).toBe('avatar-icon');
     });
 
     test('should fall back to AvatarInitials when there is an error in AvatarImage', () => {
-        const wrapper = shallow(<Avatar avatarUrl="http://foo.bar/baz123_invalid" id="1" name="hello world" />);
+        const wrapper = getWrapper({ avatarUrl: 'http://foo.bar/baz123_invalid', id: '1', name: 'hello world' });
 
         const avatarImage = wrapper.find(AvatarImage);
         const onError = avatarImage.prop('onError') as Function;
@@ -69,9 +74,14 @@ describe('components/avatar/Avatar', () => {
         expect(avatarInitials.length).toEqual(1);
     });
 
-    test('should show external user icon only when requested', () => {
-        const externalWrapper = shallow(<Avatar id="2" name="External User 1" isExternal shouldShowExternal />);
-        const nonExternalWrapper = shallow(<Avatar id="2" name="External User 2" isExternal />);
+    test('should show external user icon when isExternal and shouldShowExternal are passed', () => {
+        const externalWrapper = getWrapper({
+            id: '2',
+            name: 'External User 1',
+            isExternal: true,
+            shouldShowExternal: true,
+        });
+        const nonExternalWrapper = getWrapper({ id: '2', name: 'External User 2', isExternal: true });
 
         expect(externalWrapper.is('.avatar.avatar--isExternal')).toBe(true);
         expect(nonExternalWrapper.is('.avatar.avatar--isExternal')).toBe(false);
@@ -84,8 +94,8 @@ describe('components/avatar/Avatar', () => {
             avatarUrl: 'http://foo.bar/baz123_invalid',
         };
 
-        const wrapper = mount(<Avatar {...props} />);
-        expect(wrapper.find(AvatarImage).length).toEqual(1);
+        const wrapper = mount(<Avatar intl={intl} {...props} />);
+        expect(wrapper.find(AvatarImage).length).toBe(1);
 
         act(() => {
             const avatarImage = wrapper.find(AvatarImage);
@@ -93,8 +103,8 @@ describe('components/avatar/Avatar', () => {
             onError();
         });
         wrapper.update();
-        expect(wrapper.find(AvatarImage).length).toEqual(0);
-        expect(wrapper.find('AvatarInitials').length).toEqual(1);
+        expect(wrapper.find(AvatarImage).length).toBe(0);
+        expect(wrapper.find('AvatarInitials').length).toBe(1);
 
         act(() => {
             wrapper.setProps({
@@ -103,6 +113,6 @@ describe('components/avatar/Avatar', () => {
             });
         });
         wrapper.update();
-        expect(wrapper.find(AvatarImage).length).toEqual(1);
+        expect(wrapper.find(AvatarImage).length).toBe(1);
     });
 });
