b7f7f8533486b8731852756298ed91b1f53cf83e
John Camden <jcamden@box.com>
2023-05-10 11:16:34 -0400

    chore(node): upgrade node from 14 to 20

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,7 @@
 import { StorybookConfig } from '@storybook/react-webpack5';
+import path from 'path';
+
+const language = process.env.LANGUAGE;
 
 const config: StorybookConfig = {
     stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
@@ -15,7 +18,7 @@ const config: StorybookConfig = {
                 },
             },
         },
-        'storybook-react-intl',
+        'storybook-addon-intl',
     ],
     framework: {
         name: '@storybook/react-webpack5',
@@ -23,6 +26,21 @@ const config: StorybookConfig = {
     },
     docs: {
         autodocs: 'tag',
+    },
+    webpackFinal: async (config, { configType }) => {
+        // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
+        // You can change the configuration based on that.
+        // 'PRODUCTION' is used when building the static version of storybook.
+
+        // Make whatever fine-grained changes you need
+        config.resolve.alias = {
+            ...config.resolve.alias,
+            'box-ui-elements-locale-data': path.resolve(`i18n/${language}`),
+            'box-locale-data': path.resolve(`node_modules/@box/cldr-data/locale-data/${language}`),
+        };
+
+        // Return the altered config
+        return config;
     },
 };
 export default config;
--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,14 +1,23 @@
 import { Preview } from '@storybook/react';
-import { reactIntl } from './reactIntl';
+// import { reactIntl } from './reactIntl';
+import enUS from '../i18n/en-US';
+import deDE from '../i18n/de-DE';
+import frFR from '../i18n/fr-FR';
+import jaJP from '../i18n/ja-JP';
+import zhCN from '../i18n/zh-CN';
+
+// Enable translations in stories
+const messages = {
+    en: { ...enUS },
+    de: { ...deDE },
+    fr: { ...frFR },
+    jp: { ...jaJP },
+    zh: { ...zhCN },
+};
+
+const getMessages = locale => messages[locale];
 
 const preview: Preview = {
-    globals: {
-        locale: reactIntl.defaultLocale,
-        locales: {
-            en: 'English',
-            de: 'Deutsche',
-        },
-    },
     parameters: {
         actions: { argTypesRegex: '^on[A-Z].*' },
         controls: {
@@ -17,7 +26,11 @@ const preview: Preview = {
                 date: /Date$/,
             },
         },
-        reactIntl,
+        intl: {
+            locales: Object.keys(messages),
+            defaultLocale: 'en',
+            getMessages,
+        },
     },
 };
 
