0ed3c27bfa7519577b0e2256392266a103c62cb8
Greg Wong <gregorywong@box.com>
2023-06-09 12:28:48 -0400

    chore(ci): add more descriptive log for buie release script

--- scripts/cut_release.sh
+++ scripts/cut_release.sh
@@ -185,8 +185,9 @@ check_branch_dirty() {
 }
 
 check_npm_registry() {
-    if [[ ! $(npm config get registry) =~ (${YARN_PUBLIC_REGISTRY_REGEX}|${NPM_PUBLIC_REGISTRY_REGEX}) ]] ; then
-        printf "${red}Not pointing at the right npm registry! Make sure ~/.npmrc points to ${NPM_PUBLIC_REGISTRY}${end}"
+    npm_registry = $(npm config get registry)
+    if [[ ! $npm_registry =~ (${YARN_PUBLIC_REGISTRY_REGEX}|${NPM_PUBLIC_REGISTRY_REGEX}) ]] ; then
+        printf "${red}${$npm_registry} is not the correct registry! Make sure ~/.npmrc points to ${NPM_PUBLIC_REGISTRY}${end}"
         return 1
     fi
 }
