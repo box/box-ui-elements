446c7dc8461ad03c2068d5d40c049ce1b0c72d3a
Greg Wong <gregorywong@box.com>
2023-09-21 13:00:04 -0500

    feat(content-answers): feedback

--- src/features/content-answers/__tests__/ContentAnswersOpenButton.test.tsx
+++ src/features/content-answers/__tests__/ContentAnswersOpenButton.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { fireEvent, getByText, render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 
 import ContentAnswersOpenButton from '../ContentAnswersOpenButton';
 
