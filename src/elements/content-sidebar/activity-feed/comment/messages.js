efe0c2f6ae165c9b95b3074e27ca8a60a7b108f3
aostaszewskiBox <145002104+aostaszewskiBox@users.noreply.github.com>
2023-09-27 09:11:37 +0200

    Update src/elements/content-sidebar/activity-feed/comment/messages.js
    Co-authored-by: Trevor <7311041+tjuanitas@users.noreply.github.com>

--- src/elements/content-sidebar/activity-feed/comment/messages.js
+++ src/elements/content-sidebar/activity-feed/comment/messages.js
@@ -58,7 +58,7 @@ const messages = defineMessages({
         description: 'Text to show to get more replies of comment or annotation',
     },
     inlineCommentAnnotationAccessibleSVGTitle: {
-        id: 'be.contentSidebar.activityFeed.comment.IconAnnotation',
+        id: 'be.contentSidebar.activityFeed.comment.inlineCommentAnnotationAccessibleSVGTitle',
         defaultMessage: 'Inline Comment',
         description: 'Text for aria label of circle type icon',
     },
