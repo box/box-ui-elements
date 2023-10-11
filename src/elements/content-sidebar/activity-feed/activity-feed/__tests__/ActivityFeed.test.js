444370216cee89ab4a57ec90bde6ab55128734c6
Joshua Arnold <jarnld2@gmail.com>
2023-10-11 16:08:55 -0400

    fix(activity-thread): refactor to not use context and refs

--- src/elements/content-sidebar/activity-feed/activity-feed/__tests__/ActivityFeed.test.js
+++ src/elements/content-sidebar/activity-feed/activity-feed/__tests__/ActivityFeed.test.js
@@ -127,19 +127,15 @@ describe('elements/content-sidebar/ActivityFeed/activity-feed/ActivityFeed', () 
     //         currentUser: undefined,
     //         feedItems: undefined,
     //     });
-
     //     expect(wrapper.find('EmptyState').exists()).toBe(false);
     //     expect(wrapper.find('LoadingIndicator').exists()).toBe(true);
     // });
-
     // test('should correctly render empty state', () => {
     //     const wrapper = getWrapper({
     //         feedItems: [],
     //     });
-
     //     expect(wrapper.find('EmptyState').exists()).toBe(true);
     // });
-
     // test('should render empty state when there is 1 version (current version from file)', () => {
     //     const wrapper = getWrapper({
     //         currentUser,
@@ -148,7 +144,6 @@ describe('elements/content-sidebar/ActivityFeed/activity-feed/ActivityFeed', () 
     //     });
     //     expect(wrapper.find('EmptyState').exists()).toBe(true);
     // });
-
     // test('should render empty state when UAA is enabled and there is 1 version event (current version of file) and the start and end version are the same', () => {
     //     const wrapper = getWrapper({
     //         currentUser,
@@ -157,7 +152,6 @@ describe('elements/content-sidebar/ActivityFeed/activity-feed/ActivityFeed', () 
     //     });
     //     expect(wrapper.find('EmptyState').exists()).toBe(true);
     // });
-
     // test('should not render empty state when UAA is enabled and there is 1 version event (current version of file) and the start and end version are different', () => {
     //     const wrapper = getWrapper({
     //         currentUser,
@@ -166,20 +160,17 @@ describe('elements/content-sidebar/ActivityFeed/activity-feed/ActivityFeed', () 
     //     });
     //     expect(wrapper.find('EmptyState').exists()).toBe(false);
     // });
-
     // test('should render approval comment form if comment submit handler is passed in and comment permissions', () => {
     //     const wrapper = getWrapper({
     //         onCommentCreate: jest.fn(),
     //     });
     //     expect(wrapper).toMatchSnapshot();
     // });
-
     // test('should not render approval comment form if only comment submit handler is not passed in', () => {
     //     file.permissions.can_comment = true;
     //     const wrapper = getWrapper();
     //     expect(wrapper).toMatchSnapshot();
     // });
-
     // test('should not render approval comment form if comment permissions are not present', () => {
     //     file.permissions.can_comment = false;
     //     const wrapper = getWrapper({
@@ -187,16 +178,13 @@ describe('elements/content-sidebar/ActivityFeed/activity-feed/ActivityFeed', () 
     //     });
     //     expect(wrapper).toMatchSnapshot();
     // });
-
     // test('shouold render correct data-testid if has been scrolled', () => {
     //     const wrapper = getWrapper({
     //         feedItems: [{ type: FEED_ITEM_TYPE_COMMENT }],
     //     });
     //     wrapper.setState({ isScrolled: true });
-
     //     expect(wrapper.find('[data-testid="activityfeedscrolled"]').exists()).toBe(true);
     // });
-
     // test('should correctly render active state', () => {
     //     const wrapper = getWrapper({
     //         feedItems,
@@ -204,16 +192,13 @@ describe('elements/content-sidebar/ActivityFeed/activity-feed/ActivityFeed', () 
     //     expect(wrapper.find('ActiveState')).toHaveLength(1);
     //     expect(wrapper.find('ActiveState').prop('currentFileVersionId')).toBe(987);
     // });
-
     // test('should not expose add approval ui if task submit handler is not passed', () => {
     //     file.permissions.can_comment = true;
     //     const wrapper = getWrapper({
     //         onCommentCreate: jest.fn(),
     //     });
-
     //     expect(wrapper.find('[name="addApproval"]').length).toEqual(0);
     // });
-
     // test('should set scrollTop to be the scrollHeight if feedContainer ref is set', () => {
     //     const wrapper = getWrapper();
     //     const instance = wrapper.instance();
@@ -222,10 +207,8 @@ describe('elements/content-sidebar/ActivityFeed/activity-feed/ActivityFeed', () 
     //         scrollHeight: 100,
     //     };
     //     instance.componentDidMount();
-
     //     expect(instance.feedContainer.scrollTop).toEqual(100);
     // });
-
     // describe('componentDidUpdate()', () => {
     //     test('should set scrollTop to be the scrollHeight if feedContainer exists and prevProps feedItems is undefined and this.props.feedItems is defined', () => {
     //         const wrapper = getWrapper({
@@ -236,7 +219,6 @@ describe('elements/content-sidebar/ActivityFeed/activity-feed/ActivityFeed', () 
     //             scrollTop: 0,
     //             scrollHeight: 100,
     //         };
-
     //         instance.componentDidUpdate(
     //             {
     //                 feedItems: undefined,
@@ -244,21 +226,17 @@ describe('elements/content-sidebar/ActivityFeed/activity-feed/ActivityFeed', () 
     //             },
     //             { isInputOpen: false },
     //         );
-
     //         expect(instance.feedContainer.scrollTop).toEqual(100);
     //     });
-
     //     test('should set scrollTop to be the scrollHeight if more feedItems are added', () => {
     //         const wrapper = getWrapper({
     //             feedItems: [{ type: FEED_ITEM_TYPE_COMMENT }, { type: FEED_ITEM_TYPE_COMMENT }],
     //         });
-
     //         const instance = wrapper.instance();
     //         instance.feedContainer = {
     //             scrollTop: 0,
     //             scrollHeight: 100,
     //         };
-
     //         instance.componentDidUpdate(
     //             {
     //                 feedItems: [{ type: FEED_ITEM_TYPE_COMMENT }],
@@ -266,10 +244,8 @@ describe('elements/content-sidebar/ActivityFeed/activity-feed/ActivityFeed', () 
     //             },
     //             { isInputOpen: false },
     //         );
-
     //         expect(instance.feedContainer.scrollTop).toEqual(100);
     //     });
-
     //     test('should set scrollTop to be the scrollHeight if the user becomes defined', () => {
     //         const wrapper = getWrapper({
     //             feedItems: [{ type: FEED_ITEM_TYPE_COMMENT }],
@@ -279,7 +255,6 @@ describe('elements/content-sidebar/ActivityFeed/activity-feed/ActivityFeed', () 
     //             scrollTop: 0,
     //             scrollHeight: 100,
     //         };
-
     //         instance.componentDidUpdate(
     //             {
     //                 feedItems: [{ type: FEED_ITEM_TYPE_COMMENT }],
@@ -287,10 +262,8 @@ describe('elements/content-sidebar/ActivityFeed/activity-feed/ActivityFeed', () 
     //             },
     //             { isInputOpen: false },
     //         );
-
     //         expect(instance.feedContainer.scrollTop).toEqual(100);
     //     });
-
     //     test('should set scrollTop to be the scrollHeight if input opens', () => {
     //         const wrapper = getWrapper({
     //             feedItems: [{ type: FEED_ITEM_TYPE_COMMENT }],
@@ -303,7 +276,6 @@ describe('elements/content-sidebar/ActivityFeed/activity-feed/ActivityFeed', () 
     //             scrollTop: 0,
     //             scrollHeight: 100,
     //         };
-
     //         instance.componentDidUpdate(
     //             {
     //                 feedItems: [{ type: FEED_ITEM_TYPE_COMMENT }],
@@ -311,56 +283,45 @@ describe('elements/content-sidebar/ActivityFeed/activity-feed/ActivityFeed', () 
     //             },
     //             { isInputOpen: false },
     //         );
-
     //         expect(instance.feedContainer.scrollTop).toEqual(100);
     //     });
-
     //     test('should call scrollToActiveFeedItemOrErrorMessage if feed items loaded', () => {
     //         const wrapper = getWrapper({ feedItems: [{ type: FEED_ITEM_TYPE_COMMENT }] });
     //         const instance = wrapper.instance();
     //         instance.scrollToActiveFeedItemOrErrorMessage = jest.fn();
-
     //         instance.componentDidUpdate(
     //             {
     //                 feedItems: undefined,
     //             },
     //             { isInputOpen: false },
     //         );
-
     //         expect(instance.scrollToActiveFeedItemOrErrorMessage).toHaveBeenCalled();
     //     });
-
     //     test('should call scrollToActiveFeedItemOrErrorMessage if activeFeedEntryId changed', () => {
     //         const wrapper = getWrapper({ activeFeedEntryId: '123' });
     //         const instance = wrapper.instance();
     //         instance.scrollToActiveFeedItemOrErrorMessage = jest.fn();
-
     //         instance.componentDidUpdate(
     //             {
     //                 activeFeedEntryId: '456',
     //             },
     //             { isInputOpen: false },
     //         );
-
     //         expect(instance.scrollToActiveFeedItemOrErrorMessage).toHaveBeenCalled();
     //     });
-
     //     test('should not call scrollToActiveFeedItemOrErrorMessage if activeFeedEntryId changed', () => {
     //         const wrapper = getWrapper({ activeFeedEntryId: '456' });
     //         const instance = wrapper.instance();
     //         instance.scrollToActiveFeedItemOrErrorMessage = jest.fn();
-
     //         instance.componentDidUpdate(
     //             {
     //                 activeFeedEntryId: '456',
     //             },
     //             { isInputOpen: false },
     //         );
-
     //         expect(instance.scrollToActiveFeedItemOrErrorMessage).not.toHaveBeenCalled();
     //     });
     // });
-
     // test('should pass activeFeedItemRef to the ActiveState', () => {
     //     const wrapper = getWrapper({
     //         activeFeedEntryId: comments.entries[0].id,
@@ -369,10 +330,8 @@ describe('elements/content-sidebar/ActivityFeed/activity-feed/ActivityFeed', () 
     //     wrapper.setProps({
     //         feedItems: [{ type: FEED_ITEM_TYPE_COMMENT }],
     //     });
-
     //     expect(wrapper.find('ActiveState').prop('activeFeedItemRef')).toEqual(instance.activeFeedItemRef);
     // });
-
     // test('should scroll to active feed item when activeFeedItemRef has a value', () => {
     //     const wrapper = getWrapper({
     //         activeFeedEntryId: comments.entries[0].id,
@@ -385,60 +344,47 @@ describe('elements/content-sidebar/ActivityFeed/activity-feed/ActivityFeed', () 
     //     });
     //     expect(scrollIntoView).toHaveBeenCalledWith(li);
     // });
-
     // test('should not scroll to active feed item when activeFeedItemRef is null', () => {
     //     const wrapper = getWrapper({
     //         activeFeedEntryId: comments.entries[0].id,
     //     });
     //     const instance = wrapper.instance();
-
     //     instance.activeFeedItemRef.current = null;
     //     wrapper.setProps({
     //         feedItems: [{ type: FEED_ITEM_TYPE_COMMENT }],
     //     });
     //     expect(scrollIntoView).not.toHaveBeenCalled();
     // });
-
     // test('should show input when commentFormFocusHandler is called', () => {
     //     const wrapper = getWrapper();
-
     //     const instance = wrapper.instance();
     //     instance.commentFormFocusHandler();
-
     //     expect(wrapper.state('isInputOpen')).toBe(true);
     // });
-
     // test('should hide input when commentFormCancelHandler is called', () => {
     //     const wrapper = getWrapper({
     //         onCommentCreate: jest.fn(),
     //     });
-
     //     const instance = wrapper.instance();
     //     instance.commentFormFocusHandler();
     //     expect(wrapper.state('isInputOpen')).toBe(true);
-
     //     instance.commentFormCancelHandler();
     //     expect(wrapper.state('isInputOpen')).toBe(false);
     // });
-
     // test('should call create comment handler and close input on valid comment submit', () => {
     //     const createCommentSpy = jest.fn().mockReturnValue(Promise.resolve({}));
     //     const wrapper = getWrapper({
     //         feedItems,
     //         onCommentCreate: createCommentSpy,
     //     });
-
     //     const instance = wrapper.instance();
     //     const commentForm = wrapper.find('CommentForm').first();
-
     //     instance.commentFormFocusHandler();
     //     expect(wrapper.state('isInputOpen')).toBe(true);
-
     //     commentForm.prop('createComment')({ text: 'foo' });
     //     expect(wrapper.state('isInputOpen')).toBe(false);
     //     expect(createCommentSpy).toHaveBeenCalledTimes(1);
     // });
-
     // test('should stop event propagation onKeyDown', () => {
     //     const wrapper = getWrapper({
     //         onCommentCreate: jest.fn(),
@@ -451,7 +397,6 @@ describe('elements/content-sidebar/ActivityFeed/activity-feed/ActivityFeed', () 
     //     });
     //     expect(stopPropagationSpy).toHaveBeenCalled();
     // });
-
     // test('should correctly handle an inline error for a comment id being invalid', () => {
     //     const wrapper = getWrapper({
     //         feedItems,
@@ -460,7 +405,6 @@ describe('elements/content-sidebar/ActivityFeed/activity-feed/ActivityFeed', () 
     //     });
     //     expect(wrapper.exists('InlineError')).toBe(true);
     // });
-
     // test('should correctly handle an inline error for a task id being invalid', () => {
     //     const wrapper = getWrapper({
     //         feedItems,
@@ -469,7 +413,6 @@ describe('elements/content-sidebar/ActivityFeed/activity-feed/ActivityFeed', () 
     //     });
     //     expect(wrapper.exists('InlineError')).toBe(true);
     // });
-
     // test('should correctly handle an inline error for an annotation id being invalid', () => {
     //     const wrapper = getWrapper({
     //         feedItems,
@@ -478,17 +421,14 @@ describe('elements/content-sidebar/ActivityFeed/activity-feed/ActivityFeed', () 
     //     });
     //     expect(wrapper.exists('InlineError')).toBe(true);
     // });
-
     // test('should not render inline error if the type is invalid', () => {
     //     const wrapper = getWrapper({
     //         feedItems,
     //         activeFeedEntryId: 0,
     //         activeFeedEntryType: 'tasksss',
     //     });
-
     //     expect(wrapper.exists('InlineError')).toBe(false);
     // });
-
     // describe('hasLoaded()', () => {
     //     test.each`
     //         prevCurrentUser | prevFeedItems | expected | description
@@ -505,7 +445,6 @@ describe('elements/content-sidebar/ActivityFeed/activity-feed/ActivityFeed', () 
     //         expect(instance.hasLoaded(prevCurrentUser, prevFeedItems)).toBe(expected);
     //     });
     // });
-
     // describe('setSelectedItem()', () => {
     //     test('should call setState if hasReplies = true', () => {
     //         const wrapper = getWrapper({
@@ -513,12 +452,9 @@ describe('elements/content-sidebar/ActivityFeed/activity-feed/ActivityFeed', () 
     //         });
     //         const instance = wrapper.instance();
     //         instance.setState = jest.fn();
-
     //         instance.setSelectedItem('123');
-
     //         expect(instance.setState).toBeCalledWith({ selectedItemId: '123' });
     //     });
-
     //     test.each`
     //         hasReplies
     //         ${false}
@@ -529,13 +465,10 @@ describe('elements/content-sidebar/ActivityFeed/activity-feed/ActivityFeed', () 
     //         });
     //         const instance = wrapper.instance();
     //         instance.setState = jest.fn();
-
     //         instance.setSelectedItem('123');
-
     //         expect(instance.setState).not.toBeCalled();
     //     });
     // });
-
     // describe('isFeedItemActive()', () => {
     //     test.each`
     //         id       | type            | selectedItemId | expected
@@ -552,11 +485,9 @@ describe('elements/content-sidebar/ActivityFeed/activity-feed/ActivityFeed', () 
     //         });
     //         const instance = wrapper.instance();
     //         instance.state.selectedItemId = selectedItemId;
-
     //         expect(instance.isFeedItemActive({ id, type })).toBe(expected);
     //     });
     // });
-
     // describe('isCommentFeedItemActive()', () => {
     //     test.each`
     //         replies            | isFeedItemActiveResult | expected
