a01c2b7ad306d155ceda262c76e2950ded266183
tjuanitas <7311041+tjuanitas@users.noreply.github.com>
2023-12-14 09:49:40 -0800

    fix(search-form): extract search actions component

--- src/components/loading-indicator/makeLoadable.tsx
+++ src/components/loading-indicator/makeLoadable.tsx
@@ -6,10 +6,8 @@ export interface MakeLoadableProps {
     loadingIndicatorProps?: LoadingIndicatorProps;
 }
 
-const makeLoadable = <P extends MakeLoadableProps>(
-    BaseComponent: React.ComponentType<P>,
-): React.FC<P & MakeLoadableProps> => {
-    const LoadableComponent = ({ isLoading = false, loadingIndicatorProps = {}, ...rest }: MakeLoadableProps) =>
+const makeLoadable = <P extends object>(BaseComponent: React.ComponentType<P>): React.FC<P & MakeLoadableProps> => {
+    const LoadableComponent = ({ isLoading = false, loadingIndicatorProps = {}, ...rest }: P & MakeLoadableProps) =>
         isLoading ? <LoadingIndicator {...loadingIndicatorProps} /> : <BaseComponent {...(rest as P)} />;
 
     LoadableComponent.displayName = `Loadable${BaseComponent.displayName || BaseComponent.name || 'Component'}`;
