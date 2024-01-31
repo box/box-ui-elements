20945a58aef223493d261b27062be497693987ff
plizon <plizon@box.com>
2024-01-31 16:27:03 +0100

    fix: use inclusive inequalities in relativeTime helper

--- src/utils/relativeTime.ts
+++ src/utils/relativeTime.ts
@@ -9,19 +9,19 @@ const YEAR_IN_MS = 3.154e10;
 
 const timeFromNow = (ms: number) => {
     const diff = ms - Date.now();
-    if (Math.abs(diff) > YEAR_IN_MS) {
+    if (Math.abs(diff) >= YEAR_IN_MS) {
         return { value: Math.trunc(diff / YEAR_IN_MS), unit: 'year' };
     }
-    if (Math.abs(diff) > WEEK_IN_MS) {
+    if (Math.abs(diff) >= WEEK_IN_MS) {
         return { value: Math.trunc(diff / WEEK_IN_MS), unit: 'week' };
     }
-    if (Math.abs(diff) > DAY_IN_MS) {
+    if (Math.abs(diff) >= DAY_IN_MS) {
         return { value: Math.trunc(diff / DAY_IN_MS), unit: 'day' };
     }
-    if (Math.abs(diff) > HOUR_IN_MS) {
+    if (Math.abs(diff) >= HOUR_IN_MS) {
         return { value: Math.trunc((diff % DAY_IN_MS) / HOUR_IN_MS), unit: 'hour' };
     }
-    if (Math.abs(diff) > MIN_IN_MS) {
+    if (Math.abs(diff) >= MIN_IN_MS) {
         return { value: Math.trunc((diff % HOUR_IN_MS) / MIN_IN_MS), unit: 'minute' };
     }
     return { value: Math.trunc((diff % MIN_IN_MS) / SEC_IN_MS), unit: 'second' };
