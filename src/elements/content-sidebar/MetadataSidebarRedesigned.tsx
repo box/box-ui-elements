14da29fea5afe0c0a0219943b06f5205d6f0097b
Dawid Jankowiak <jankowiak.dawid@gmail.com>
2024-07-15 18:15:52 +0200

    feat(content-sidebar): Remove lint disabling comments

--- src/elements/content-sidebar/MetadataSidebarRedesigned.tsx
+++ src/elements/content-sidebar/MetadataSidebarRedesigned.tsx
@@ -5,18 +5,10 @@
 import * as React from 'react';
 import flow from 'lodash/flow';
 import { FormattedMessage } from 'react-intl';
-// @ts-ignore Module is written in Flow
-// eslint-disable-next-line import/named
 import { withAPIContext } from '../common/api-context';
-// @ts-ignore Module is written in Flow
-// eslint-disable-next-line import/named
 import { withErrorBoundary } from '../common/error-boundary';
-// @ts-ignore Module is written in Flow
-// eslint-disable-next-line import/named
 import { withLogger } from '../common/logger';
-// @ts-ignore Module is written in Flow
 import { ORIGIN_METADATA_SIDEBAR_REDESIGN } from '../../constants';
-// @ts-ignore Module is written in Flow
 import messages from '../common/messages';
 
 import './MetadataSidebarRedesigned.scss';
