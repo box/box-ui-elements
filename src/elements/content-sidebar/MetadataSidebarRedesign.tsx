96102a5cc30897e19c9995a4e212066c90ddef17
Dawid Jankowiak <jankowiak.dawid@gmail.com>
2024-08-09 20:43:50 +0200

    feat(metadata-sidebar): add AddMetadataTemplateDropdown
    To MetadataSidebarRedesign

--- src/elements/content-sidebar/MetadataSidebarRedesign.tsx
+++ src/elements/content-sidebar/MetadataSidebarRedesign.tsx
@@ -4,18 +4,36 @@
  */
 import * as React from 'react';
 import flow from 'lodash/flow';
-import { FormattedMessage } from 'react-intl';
+import getProp from 'lodash/get';
+import { FormattedMessage, type MessageDescriptor } from 'react-intl';
 
 import { Text } from '@box/blueprint-web';
 import { AddMetadataTemplateDropdown } from '@box/metadata-editor';
 
+import API from '../../api';
+import { type ElementsXhrError } from '../../common/types/api';
+import { type ElementOrigin } from '../common/flowTypes';
+import { isUserCorrectableError } from '../../utils/error';
 import { withAPIContext } from '../common/api-context';
 import { withErrorBoundary } from '../common/error-boundary';
 import { withLogger } from '../common/logger';
-import { ORIGIN_METADATA_SIDEBAR_REDESIGN } from '../../constants';
+import {
+    FIELD_IS_EXTERNALLY_OWNED,
+    FIELD_PERMISSIONS,
+    FIELD_PERMISSIONS_CAN_UPLOAD,
+    IS_ERROR_DISPLAYED,
+    ORIGIN_METADATA_SIDEBAR_REDESIGN,
+} from '../../constants';
 import { EVENT_JS_READY } from '../common/logger/constants';
 import { mark } from '../../utils/performance';
+import LoadingIndicator from '../../components/loading-indicator/LoadingIndicator';
+import InlineError from '../../components/inline-error/InlineError';
+
 import messages from '../common/messages';
+
+import { type BoxItem } from '../../common/types/core';
+import { MetadataTemplate, type MetadataEditor } from '../../common/types/metadata';
+import { type WithLoggerProps } from '../../common/types/logging';
 
 import './MetadataSidebarRedesign.scss';
 
@@ -23,23 +41,160 @@ const MARK_NAME_JS_READY = `${ORIGIN_METADATA_SIDEBAR_REDESIGN}_${EVENT_JS_READY
 
 mark(MARK_NAME_JS_READY);
 
-function MetadataSidebarRedesign() {
+interface ExternalProps {
+    isFeatureEnabled: boolean;
+}
+
+interface PropsWithoutContext extends ExternalProps {
+    elementId: string;
+    fileId: string;
+    hasSidebarInitialized?: boolean;
+}
+
+interface ErrorContextProps {
+    onError: (error: ElementsXhrError | Error, code: string, contextInfo?: Object, origin?: ElementOrigin) => void;
+}
+
+export interface MetadataSidebarRedesignProps extends PropsWithoutContext, ErrorContextProps, WithLoggerProps {
+    api: API;
+}
+
+type Status = 'idle' | 'loading' | 'error' | 'success';
+interface DataFetcher {
+    status: Status;
+    file: BoxItem | null;
+    errorMessage: MessageDescriptor | null;
+    templates: Array<MetadataTemplate>;
+}
+
+function useDataFetching(
+    api: API,
+    fileId: string,
+    onError: ErrorContextProps['onError'],
+    isFeatureEnabled: ExternalProps['isFeatureEnabled'],
+): DataFetcher {
+    const [status, setStatus] = React.useState<Status>('idle');
+    const [file, setFile] = React.useState<BoxItem>(null);
+    const [templates, setTemplates] = React.useState(null);
+    const [errorMessage, setErrorMessage] = React.useState<MessageDescriptor | null>(null);
+
+    const onApiError = React.useCallback(
+        (error: ElementsXhrError, code: string, message: MessageDescriptor) => {
+            const { status: errorStatus } = error;
+            const isValidError = isUserCorrectableError(errorStatus);
+            setStatus('error');
+            setErrorMessage(message);
+            onError(error, code, {
+                error,
+                [IS_ERROR_DISPLAYED]: isValidError,
+            });
+        },
+        [onError],
+    );
+
+    const fetchMetadataSuccessCallback = React.useCallback(
+        ({ templates: fetchedTemplates }: { editors: Array<MetadataEditor>; templates: Array<MetadataTemplate> }) => {
+            setErrorMessage(null);
+            setStatus('success');
+            setTemplates(fetchedTemplates);
+        },
+        [],
+    );
+
+    const fetchMetadataErrorCallback = React.useCallback(
+        (e: ElementsXhrError, code: string) => {
+            setTemplates(null);
+            onApiError(e, code, messages.sidebarMetadataFetchingErrorContent);
+        },
+        [onApiError],
+    );
+
+    const fetchMetadata = React.useCallback(
+        (fetchedFile: BoxItem) => {
+            api.getMetadataAPI(false).getMetadata(
+                fetchedFile,
+                fetchMetadataSuccessCallback,
+                fetchMetadataErrorCallback,
+                isFeatureEnabled,
+                { refreshCache: true },
+            );
+        },
+        [api, fetchMetadataErrorCallback, fetchMetadataSuccessCallback, isFeatureEnabled],
+    );
+
+    const fetchFileSuccessCallback = React.useCallback(
+        (fetchedFile: BoxItem) => {
+            const { currentFile } = file ?? {};
+            const currentCanUpload = getProp(currentFile, FIELD_PERMISSIONS_CAN_UPLOAD, false);
+            const newCanUpload = getProp(fetchedFile, FIELD_PERMISSIONS_CAN_UPLOAD, false);
+            const shouldFetchMetadata = !currentFile || currentCanUpload !== newCanUpload;
+            setFile(fetchedFile);
+            if (shouldFetchMetadata && fetchedFile) {
+                fetchMetadata(fetchedFile);
+            } else {
+                setStatus('success');
+            }
+        },
+        [fetchMetadata, file],
+    );
+
+    const fetchFileErrorCallback = React.useCallback(
+        (e: ElementsXhrError, code: string) => {
+            setFile(undefined);
+            onApiError(e, code, messages.sidebarMetadataEditingErrorContent);
+        },
+        [onApiError],
+    );
+
+    React.useEffect(() => {
+        setStatus('loading');
+        api.getFileAPI().getFile(fileId, fetchFileSuccessCallback, fetchFileErrorCallback, {
+            fields: [FIELD_IS_EXTERNALLY_OWNED, FIELD_PERMISSIONS],
+            refreshCache: true,
+        });
+        // this effect is suppose to run only once when component is loaded for a given file
+        // when all dependencies are listed components goes into infinite rerender loop
+        // even if functions are wrapped in useCallback
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [fileId]);
+
+    return {
+        status,
+        file,
+        errorMessage,
+        templates,
+    };
+}
+
+function MetadataSidebarRedesign({ api, fileId, onError, isFeatureEnabled }: MetadataSidebarRedesignProps) {
+    const [selectedTemplates, setSelectedTemplates] = React.useState<Array<MetadataTemplate>>([]);
+
+    const { templates, errorMessage, status } = useDataFetching(api, fileId, onError, isFeatureEnabled);
+
     return (
         <div className="bcs-MetadataSidebarRedesign bcs-content">
             <div className="bcs-MetadataSidebarRedesign-header">
                 <Text as="h3" variant="titleLarge">
                     <FormattedMessage {...messages.sidebarMetadataTitle} />
                 </Text>
-
-                <AddMetadataTemplateDropdown
-                    availableTemplates={[]}
-                    selectedTemplates={[]}
-                    onSelect={(): void => {
-                        // nothing here yet
-                    }}
-                />
+                {status === 'success' && templates && (
+                    <AddMetadataTemplateDropdown
+                        availableTemplates={templates}
+                        selectedTemplates={selectedTemplates}
+                        onSelect={(selectedTemplate): void => {
+                            setSelectedTemplates([...selectedTemplates, selectedTemplate]);
+                        }}
+                    />
+                )}
             </div>
-            <p>Hello from Metadata Sidebar redesign</p>
+            <div className="bcs-MetadataSidebarRedesign-content">
+                {status === 'error' && errorMessage && (
+                    <InlineError title={<FormattedMessage {...messages.error} />}>
+                        <FormattedMessage {...errorMessage} />
+                    </InlineError>
+                )}
+                {status === 'loading' && <LoadingIndicator />}
+            </div>
         </div>
     );
 }
--- src/elements/content-sidebar/__tests__/MetadataSidebarRedesigned.test.tsx
+++ src/elements/content-sidebar/__tests__/MetadataSidebarRedesigned.test.tsx
@@ -1,23 +1,75 @@
 import React from 'react';
-import { IntlProvider } from 'react-intl';
+import { userEvent } from '@testing-library/user-event';
+import { FIELD_PERMISSIONS_CAN_UPLOAD } from '../../../constants';
+import { screen, render } from '../../../test-utils/testing-library';
 
-import { screen, render } from '@testing-library/react';
-
-import { MetadataSidebarRedesignComponent as MetadataSidebar } from '../MetadataSidebarRedesign';
-
-jest.unmock('react-intl');
+import {
+    MetadataSidebarRedesignComponent as MetadataSidebar,
+    type MetadataSidebarRedesignProps,
+} from '../MetadataSidebarRedesign';
 
 describe('elements/content-sidebar/Metadata/MetadataSidebarRedesigned', () => {
-    const renderComponent = (props = {}) =>
-        render(<MetadataSidebar {...props} />, {
-            wrapper: ({ children }: { children: React.ReactNode }) => (
-                <IntlProvider locale="en-US">{children}</IntlProvider>
-            ),
-        });
+    const mockAPI = {
+        getFile: jest.fn((id, successCallback) => {
+            successCallback({ id, [FIELD_PERMISSIONS_CAN_UPLOAD]: true });
+        }),
+        getMetadata: jest.fn((_file, successCallback) => {
+            successCallback({
+                editors: [],
+                templates: [
+                    {
+                        id: 'metadata_template_custom_1',
+                        scope: 'global',
+                        templateKey: 'properties',
+                        hidden: false,
+                    },
+                ],
+            });
+        }),
+    };
+    const api = {
+        getFileAPI: jest.fn().mockReturnValue(mockAPI),
+        getMetadataAPI: jest.fn().mockReturnValue(mockAPI),
+    };
+
+    const defaultProps = {
+        api,
+        fileId: 'test-file-id-1',
+        elementId: 'element-1',
+        isFeatureEnabled: true,
+        onError: jest.fn(),
+    } satisfies MetadataSidebarRedesignProps;
+
+    const renderComponent = (props: Partial<MetadataSidebarRedesignProps> = {}) => {
+        const ps = { ...defaultProps, ...props } as MetadataSidebarRedesignProps;
+        return render(<MetadataSidebar {...ps} />);
+    };
 
     test('should render title', () => {
         renderComponent();
 
-        expect(screen.getByRole('heading', { level: 3, name: 'Metadata' })).toBeVisible();
+        expect(screen.getByRole('heading', { level: 3, name: 'Metadata' })).toBeInTheDocument();
+    });
+
+    test('should have accessible "Add template" button', () => {
+        renderComponent();
+
+        expect(screen.getByRole('button', { name: /Add template/i })).toBeInTheDocument();
+    });
+
+    test('should have selectable "Custom Metadata" template in dropdown', async () => {
+        renderComponent();
+
+        const addTemplateButton = screen.getByRole('button', { name: /Add template/i });
+        await userEvent.click(addTemplateButton);
+
+        const customMetadataOption = screen.getByRole('option', { name: /Custom Metadata/i });
+        expect(customMetadataOption).toBeInTheDocument();
+        userEvent.click(customMetadataOption);
+
+        // instead of below assertions check if template was added when MetadataInstanceList will be implemented
+        await userEvent.click(addTemplateButton);
+
+        expect(customMetadataOption).toHaveAttribute('aria-disabled', 'true');
     });
 });
