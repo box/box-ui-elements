ccf6d7373d1d6f8c92b8799d8bd249c3ab1ea019
Dawid Jankowiak <jankowiak.dawid@gmail.com>
2024-08-23 13:58:55 -0300

    feat(metadata-sidebar): Add metadata add template dropdown menu (#3606)
    * feat(metadata-sidebar): Add AddMetadataTemplateDropdown
    
    and improve basic styling
    
    * feat(metadata-sidebar): Fix failing tests
    
    Extend Jest configuration to not transforming metadata-editor code
    
    * feat(metadata-sidebar): add AddMetadataTemplateDropdown
    
    To MetadataSidebarRedesign
    
    * feat(content-sidebar): Bring back changes to mockServiceWorker.js
    
    No idea why they got there in the first place
    
    * feat(metadata-sidebar): update storybook
    
    * feat(metadata-sidebar): PR comments
    
    * feat(metadata-sidebar): simplify storybook
    
    * feat(metadata-sidebar): enum status
    
    * feat(metadata-sidebar): global variables and enum upper case change
    
    * feat(metadata-sidebar): useSidebarMetadataFetcher tests
    
    * feat(metadata-sidebar): useSidebarMetadataFetcher tests
    
    * feat(metadata-sidebar): PR comments
    
    * feat(metadata-sidebar): PR comments
    
    * feat(metadata-sidebar): loading status test
    
    * feat(metadata-sidebar): use SidebarContent + tests
    
    * feat(metadata-sidebar): template dropdown menu nit fixes
    
    ---------
    
    Co-authored-by: Karolina Rusek-Bieniek <krusek@box.com>
    Co-authored-by: Wiola <wpiesiak@box.com>

---
+++ src/elements/content-sidebar/hooks/useSidebarMetadataFetcher.ts
@@ -0,0 +1,125 @@
+import * as React from 'react';
+import getProp from 'lodash/get';
+import { type MessageDescriptor } from 'react-intl';
+import API from '../../../api';
+import { type ElementsXhrError } from '../../../common/types/api';
+import { isUserCorrectableError } from '../../../utils/error';
+import { FIELD_IS_EXTERNALLY_OWNED, FIELD_PERMISSIONS, FIELD_PERMISSIONS_CAN_UPLOAD } from '../../../constants';
+
+import messages from '../../common/messages';
+
+import { type BoxItem } from '../../../common/types/core';
+import { MetadataTemplate, type MetadataEditor } from '../../../common/types/metadata';
+import { type ErrorContextProps, type ExternalProps } from '../MetadataSidebarRedesign';
+
+export enum STATUS {
+    IDLE = 'idle',
+    LOADING = 'loading',
+    ERROR = 'error',
+    SUCCESS = 'success',
+}
+interface DataFetcher {
+    status: STATUS;
+    file: BoxItem | null;
+    errorMessage: MessageDescriptor | null;
+    templates: Array<MetadataTemplate>;
+}
+
+function useSidebarMetadataFetcher(
+    api: API,
+    fileId: string,
+    onError: ErrorContextProps['onError'],
+    isFeatureEnabled: ExternalProps['isFeatureEnabled'],
+): DataFetcher {
+    const [status, setStatus] = React.useState<STATUS>(STATUS.IDLE);
+    const [file, setFile] = React.useState<BoxItem>(null);
+    const [templates, setTemplates] = React.useState(null);
+    const [errorMessage, setErrorMessage] = React.useState<MessageDescriptor | null>(null);
+
+    const onApiError = React.useCallback(
+        (error: ElementsXhrError, code: string, message: MessageDescriptor) => {
+            const { status: errorStatus } = error;
+            const isValidError = isUserCorrectableError(errorStatus);
+            setStatus(STATUS.ERROR);
+            setErrorMessage(message);
+            onError(error, code, {
+                error,
+                isErrorDisplayed: isValidError,
+            });
+        },
+        [onError],
+    );
+
+    const fetchMetadataSuccessCallback = React.useCallback(
+        ({ templates: fetchedTemplates }: { editors: Array<MetadataEditor>; templates: Array<MetadataTemplate> }) => {
+            setErrorMessage(null);
+            setStatus(STATUS.SUCCESS);
+            setTemplates(fetchedTemplates);
+        },
+        [],
+    );
+
+    const fetchMetadataErrorCallback = React.useCallback(
+        (e: ElementsXhrError, code: string) => {
+            setTemplates(null);
+            onApiError(e, code, messages.sidebarMetadataFetchingErrorContent);
+        },
+        [onApiError],
+    );
+
+    const fetchMetadata = React.useCallback(
+        (fetchedFile: BoxItem) => {
+            api.getMetadataAPI(false).getMetadata(
+                fetchedFile,
+                fetchMetadataSuccessCallback,
+                fetchMetadataErrorCallback,
+                isFeatureEnabled,
+                { refreshCache: true },
+            );
+        },
+        [api, fetchMetadataErrorCallback, fetchMetadataSuccessCallback, isFeatureEnabled],
+    );
+
+    const fetchFileSuccessCallback = React.useCallback(
+        (fetchedFile: BoxItem) => {
+            const { currentFile } = file ?? {};
+            const currentFileCanUpload = getProp(currentFile, FIELD_PERMISSIONS_CAN_UPLOAD, false);
+            const newFileCanUpload = getProp(fetchedFile, FIELD_PERMISSIONS_CAN_UPLOAD, false);
+            const shouldFetchMetadata = !currentFile || currentFileCanUpload !== newFileCanUpload;
+            setFile(fetchedFile);
+            if (shouldFetchMetadata && fetchedFile) {
+                fetchMetadata(fetchedFile);
+            } else {
+                setStatus(STATUS.SUCCESS);
+            }
+        },
+        [fetchMetadata, file],
+    );
+
+    const fetchFileErrorCallback = React.useCallback(
+        (e: ElementsXhrError, code: string) => {
+            setFile(undefined);
+            onApiError(e, code, messages.sidebarMetadataEditingErrorContent);
+        },
+        [onApiError],
+    );
+
+    React.useEffect(() => {
+        if (status === STATUS.IDLE) {
+            setStatus(STATUS.LOADING);
+            api.getFileAPI().getFile(fileId, fetchFileSuccessCallback, fetchFileErrorCallback, {
+                fields: [FIELD_IS_EXTERNALLY_OWNED, FIELD_PERMISSIONS],
+                refreshCache: true,
+            });
+        }
+    }, [api, fetchFileErrorCallback, fetchFileSuccessCallback, fileId, status]);
+
+    return {
+        status,
+        file,
+        errorMessage,
+        templates,
+    };
+}
+
+export default useSidebarMetadataFetcher;
