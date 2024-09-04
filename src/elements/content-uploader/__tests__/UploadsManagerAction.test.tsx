42cc092b40fd5cddfb4a3e14d274af36f4b2f8ca
Jerry Jiang <zmxjtbox@gmail.com>
2024-09-03 20:30:59 -0500

    chore(content-uploader): migrate upload state (#3624)
    * chore(content-uploader): migrate upload state
    
    * chore(content-uploader): add aria-label to svgs
    
    * chore(content-uploader): update test
    
    * chore(content-uploader): resolve nits

--- src/elements/content-uploader/__tests__/UploadsManagerAction.test.tsx
+++ src/elements/content-uploader/__tests__/UploadsManagerAction.test.tsx
@@ -4,17 +4,17 @@ import { render, screen, fireEvent } from '../../../test-utils/testing-library';
 import UploadsManagerAction, { UploadsManagerActionProps } from '../UploadsManagerAction';
 
 describe('elements/content-uploader/UploadsManagerAction', () => {
-    const getWrapper = (props: Partial<UploadsManagerActionProps>) =>
+    const renderComponent = (props: Partial<UploadsManagerActionProps>) =>
         render(<UploadsManagerAction hasMultipleFailedUploads={false} onClick={jest.fn()} {...props} />);
 
     test('should render correctly with hasMultipleFailedUploads as true', () => {
-        getWrapper({ hasMultipleFailedUploads: true });
+        renderComponent({ hasMultipleFailedUploads: true });
 
         expect(screen.getByText('Resume All')).toBeInTheDocument();
     });
 
     test('should render correctly with hasMultipleFailedUploads as false', () => {
-        getWrapper({ hasMultipleFailedUploads: false });
+        renderComponent({ hasMultipleFailedUploads: false });
 
         expect(screen.getByText('Resume')).toBeInTheDocument();
         expect(screen.queryByText('Resume All')).not.toBeInTheDocument();
@@ -22,7 +22,7 @@ describe('elements/content-uploader/UploadsManagerAction', () => {
 
     test('should call onClick when resume button is clicked', () => {
         const handleResumeClick = jest.fn();
-        getWrapper({ onClick: handleResumeClick });
+        renderComponent({ onClick: handleResumeClick });
 
         fireEvent.click(screen.getByText('Resume'));
         expect(handleResumeClick).toHaveBeenCalledTimes(1);
