963626bd1dc25fbcbdcc5ee285f376bf2e3e8d9f
Danila Rubleuski <daniilrublevskiy@gmail.com>
2024-12-02 14:38:55 +0100

    feat(boxai-sidebar): Add Agent selector and clear button to sidebar header

--- src/elements/content-sidebar/__tests__/BoxAISidebar.test.tsx
+++ src/elements/content-sidebar/__tests__/BoxAISidebar.test.tsx
@@ -1,18 +1,31 @@
 import React from 'react';
 import { userEvent } from '@testing-library/user-event';
+import { BoxAiAgentSelector } from '@box/box-ai-agent-selector';
 import { screen, render } from '../../../test-utils/testing-library';
 import BoxAISidebarComponent, { BoxAISidebarProps } from '../BoxAISidebar';
 
 const mockOnExpandClick = jest.fn();
+const mockOnClearClick = jest.fn();
+
+jest.mock('@box/box-ai-agent-selector', () => ({
+    BoxAiAgentSelector: jest.fn(),
+}));
 
 describe('elements/content-sidebar/BoxAISidebar', () => {
     const renderComponent = (props = {}) => {
         const defaultProps = {
             onExpandClick: mockOnExpandClick,
+            onClearCLick: mockOnClearClick,
         } satisfies BoxAISidebarProps;
 
         render(<BoxAISidebarComponent {...defaultProps} {...props} />);
     };
+
+    beforeEach(() => {
+        (BoxAiAgentSelector as jest.MockedFunction<typeof BoxAiAgentSelector>).mockImplementation(() => (
+            <div>BoxAiAgentSelector</div>
+        ));
+    });
 
     afterEach(() => {
         jest.clearAllMocks();
@@ -22,6 +35,21 @@ describe('elements/content-sidebar/BoxAISidebar', () => {
         renderComponent();
 
         expect(screen.getByRole('heading', { level: 3, name: 'Box AI' })).toBeInTheDocument();
+    });
+
+    test('should have accessible "Clear" button', () => {
+        renderComponent();
+
+        expect(screen.getByRole('button', { name: 'Clear' })).toBeInTheDocument();
+    });
+
+    test('should call onExpandClick when click "Clear" button', async () => {
+        renderComponent();
+
+        const expandButton = screen.getByRole('button', { name: 'Clear' });
+        await userEvent.click(expandButton);
+
+        expect(mockOnClearClick).toHaveBeenCalled();
     });
 
     test('should have accessible "Expand" button', () => {
