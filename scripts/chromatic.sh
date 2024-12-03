21d1eca3bc16fd3de92677507ad6336a5f253174
Greg Wong <gregorywong@box.com>
2024-12-03 16:26:11 -0500

    chore(chromatic): broken snapshots

--- scripts/chromatic.sh
+++ scripts/chromatic.sh
@@ -3,7 +3,7 @@
 if [ "${CIRCLE_BRANCH}" != "master" ];
 then
     echo "Detected branch is not master, running chromatic with manual acceptance"
-    yarn chromatic
+    yarn chromatic --debug --diagnostics-file=true
 else
     echo "Detected branch is master, auto-accepting changes"
     yarn chromatic --auto-accept-changes
