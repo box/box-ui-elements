166b6a5d17da24aac781add386c99725bbf32dac
Adam Horowitz <ahorowi4@gmail.com>
2024-12-17 10:57:37 -0500

    fix(content-uploader): fix bug in assigning new item ids

--- src/elements/content-uploader/ContentUploader.tsx
+++ src/elements/content-uploader/ContentUploader.tsx
@@ -295,7 +295,12 @@ class ContentUploader extends Component<ContentUploaderProps, State> {
     getNewFiles = (files: Array<UploadFileWithAPIOptions | File>): Array<UploadFileWithAPIOptions | File> => {
         const { rootFolderId } = this.props;
 
-        return Array.from(files).filter(file => !this.itemIdsRef.current[getFileId(file, rootFolderId)]);
+        return Array.from(files).filter(file => {
+            console.log('getFileId(item, rootFolderId)', getFileId(file, rootFolderId));
+            console.log('this.itemIdsRef.current', this.itemIdsRef.current);
+            console.log('filterOutcome', !this.itemIdsRef.current[getFileId(file, rootFolderId)]);
+            return !this.itemIdsRef.current[getFileId(file, rootFolderId)];
+        });
     };
 
     /**
@@ -308,7 +313,12 @@ class ContentUploader extends Component<ContentUploaderProps, State> {
     ): Array<DataTransferItem | UploadDataTransferItemWithAPIOptions> => {
         const { rootFolderId } = this.props;
 
-        return Array.from(items).filter(item => !this.itemIdsRef.current[getDataTransferItemId(item, rootFolderId)]);
+        return Array.from(items).filter(item => {
+            console.log('getDataTransferItemId(item, rootFolderId)', getDataTransferItemId(item, rootFolderId));
+            console.log('this.itemIdsRef.current', this.itemIdsRef.current);
+            console.log('filterOutcome', !this.itemIdsRef.current[getDataTransferItemId(item, rootFolderId)]);
+            return !this.itemIdsRef.current[getDataTransferItemId(item, rootFolderId)];
+        });
     };
 
     /**
@@ -345,7 +355,7 @@ class ContentUploader extends Component<ContentUploaderProps, State> {
 
         const firstFile = getFile(newFiles[0]);
 
-        const newItemIdsState = { ...this.itemsRef.current, ...newItemIds };
+        const newItemIdsState = { ...this.itemIdsRef.current, ...newItemIds };
 
         this.itemIdsRef.current = newItemIdsState;
 
