dabaae2a675873ce6e683a772878338737732617
Jerry Jiang <zmxjtbox@gmail.com>
2025-02-07 14:30:18 -0600

    feat(content-explorer): migrate subheader to pageheader (#3903)
    * feat(content-explorer): migrate subheader to pageheader
    
    * feat(content-explorer): add aria labels for add and sort
    
    * feat(content-explorer): update breadcrumb style and comments

--- src/elements/common/sub-header/__tests__/Sort.test.js
+++ src/elements/common/sub-header/__tests__/Sort.test.js
@@ -1,69 +1,36 @@
 import * as React from 'react';
-import DropdownMenu from '../../../../components/dropdown-menu/DropdownMenu';
-import Menu from '../../../../components/menu/Menu';
-import SelectMenuItem from '../../../../components/menu/SelectMenuItem';
+import { DropdownMenu, IconButton } from '@box/blueprint-web';
 import Sort from '../Sort';
-import SortButton from '../SortButton';
-import messages from '../../messages';
 import { SORT_ASC, SORT_DESC } from '../../../../constants';
+
+import messages from '../../messages';
 
 describe('elements/common/sub-header/Sort', () => {
     test('should render a button and menu with 4 select menu items', () => {
         const wrapper = shallow(<Sort onSortChange={jest.fn()} sortBy="name" sortDirection={SORT_ASC} />);
 
-        expect(wrapper.find(SortButton)).toHaveLength(1);
-        expect(wrapper.find(DropdownMenu)).toHaveLength(1);
-        expect(wrapper.find(Menu)).toHaveLength(1);
-        expect(wrapper.find(SelectMenuItem)).toHaveLength(6);
+        expect(wrapper.find(IconButton)).toHaveLength(1);
+        expect(wrapper.find(DropdownMenu.Content)).toHaveLength(1);
+        expect(wrapper.find(DropdownMenu.Item)).toHaveLength(6);
     });
 
     test('should render a select with 6 options', () => {
         const wrapper = shallow(<Sort onSortChange={jest.fn()} sortBy="name" sortDirection={SORT_ASC} />);
-        const options = wrapper.find(SelectMenuItem);
+        const options = wrapper.find(DropdownMenu.Item);
 
         expect(options).toHaveLength(6);
-        expect(
-            options
-                .at(0)
-                .childAt(0)
-                .prop('id'),
-        ).toBe(messages.nameASC.id);
-        expect(
-            options
-                .at(1)
-                .childAt(0)
-                .prop('id'),
-        ).toBe(messages.nameDESC.id);
-        expect(
-            options
-                .at(2)
-                .childAt(0)
-                .prop('id'),
-        ).toBe(messages.dateASC.id);
-        expect(
-            options
-                .at(3)
-                .childAt(0)
-                .prop('id'),
-        ).toBe(messages.dateDESC.id);
-        expect(
-            options
-                .at(4)
-                .childAt(0)
-                .prop('id'),
-        ).toBe(messages.sizeASC.id);
-        expect(
-            options
-                .at(5)
-                .childAt(0)
-                .prop('id'),
-        ).toBe(messages.sizeDESC.id);
+        expect(options.at(0).childAt(0).prop('id')).toBe(messages.nameASC.id);
+        expect(options.at(1).childAt(0).prop('id')).toBe(messages.nameDESC.id);
+        expect(options.at(2).childAt(0).prop('id')).toBe(messages.dateASC.id);
+        expect(options.at(3).childAt(0).prop('id')).toBe(messages.dateDESC.id);
+        expect(options.at(4).childAt(0).prop('id')).toBe(messages.sizeASC.id);
+        expect(options.at(5).childAt(0).prop('id')).toBe(messages.sizeDESC.id);
     });
 
     test('should pass correct parameters when clicked', () => {
         const sort = jest.fn();
         const wrapper = shallow(<Sort onSortChange={sort} sortBy="name" sortDirection={SORT_ASC} />);
-        const options = wrapper.find(SelectMenuItem);
+        const options = wrapper.find(DropdownMenu.Item);
 
         options.at(0).simulate('click');
         expect(sort).toHaveBeenCalledWith('name', SORT_ASC);
@@ -86,7 +53,7 @@ describe('elements/common/sub-header/Sort', () => {
 
     test('should only have one option selected', () => {
         const wrapper = shallow(<Sort onSortChange={jest.fn()} sortBy="date" sortDirection={SORT_DESC} />);
-        const options = wrapper.find(SelectMenuItem);
+        const options = wrapper.find(DropdownMenu.Item);
 
         expect(options.at(3).prop('isSelected')).toBe(true);
         options.forEach((option, i) => {
