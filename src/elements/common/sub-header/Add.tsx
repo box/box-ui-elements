80cb2e595eec443b2c782bf6accb9b6762775676
Jerry Jiang <zmxjtbox@gmail.com>
2025-03-04 11:00:10 -0600

    feat(content-explorer): migrate subHeader to typescript (#3999)
    * feat(content-explorer): migrate subHeader to typescript
    
    * feat(content-explorer): update test
    
    * feat(content-explorer): update variant prop in pageHeader

---
+++ src/elements/common/sub-header/Add.tsx
@@ -0,0 +1,45 @@
+import * as React from 'react';
+import { FormattedMessage, useIntl } from 'react-intl';
+import { DropdownMenu, IconButton } from '@box/blueprint-web';
+import { Plus } from '@box/blueprint-web-assets/icons/Fill';
+
+import messages from '../messages';
+
+export interface AddProps {
+    isDisabled: boolean;
+    onCreate: () => void;
+    onUpload: () => void;
+    showCreate: boolean;
+    showUpload: boolean;
+}
+
+const Add = ({ isDisabled, onUpload, onCreate, showUpload = true, showCreate = true }: AddProps) => {
+    const { formatMessage } = useIntl();
+
+    return (
+        <DropdownMenu.Root>
+            <DropdownMenu.Trigger>
+                <IconButton
+                    aria-label={formatMessage(messages.add)}
+                    className="be-btn-add"
+                    disabled={isDisabled}
+                    icon={Plus}
+                />
+            </DropdownMenu.Trigger>
+            <DropdownMenu.Content>
+                {showUpload && (
+                    <DropdownMenu.Item onClick={onUpload}>
+                        <FormattedMessage {...messages.upload} />
+                    </DropdownMenu.Item>
+                )}
+                {showCreate && (
+                    <DropdownMenu.Item onClick={onCreate}>
+                        <FormattedMessage {...messages.newFolder} />
+                    </DropdownMenu.Item>
+                )}
+            </DropdownMenu.Content>
+        </DropdownMenu.Root>
+    );
+};
+
+export default Add;
