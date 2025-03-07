f46ed68eecc22535de2a522d3e2207575e463bd8
Greg Wong <gregorywong@box.com>
2025-03-07 16:29:19 -0500

    chore(share-access-select): Migrate ShareAccessSelect

--- src/elements/common/share-access-select/ShareAccessSelect.tsx
+++ src/elements/common/share-access-select/ShareAccessSelect.tsx
@@ -4,21 +4,23 @@
  */
 
 import * as React from 'react';
-import { injectIntl, type IntlShape } from 'react-intl';
-import messages from '../messages';
+import { useIntl } from 'react-intl';
 import { ACCESS_NONE, ACCESS_OPEN, ACCESS_COLLAB, ACCESS_COMPANY } from '../../../constants';
 import type { BoxItem } from '../../../common/types/core';
+
 import './ShareAccessSelect.scss';
+
+import messages from '../messages';
 
 export interface ShareAccessSelectProps {
     canSetShareAccess: boolean;
     className: string;
-    intl: IntlShape;
     item: BoxItem;
     onChange: (value: string, item: BoxItem) => void;
 }
 
-const ShareAccessSelect = ({ className, canSetShareAccess, onChange, item, intl }: ShareAccessSelectProps) => {
+const ShareAccessSelect = ({ className, canSetShareAccess, onChange, item }: ShareAccessSelectProps) => {
+    const { formatMessage } = useIntl();
     const { allowed_shared_link_access_levels: allowedSharedAccessLevels, permissions, shared_link: sharedLink } = item;
 
     if (!allowedSharedAccessLevels) {
@@ -41,20 +43,16 @@ const ShareAccessSelect = ({ className, canSetShareAccess, onChange, item, intl 
     /* eslint-disable jsx-a11y/no-onchange */
     return (
         <select className={`be-share-access-select ${className}`} onChange={changeHandler} value={access}>
-            {allowOpen ? <option value={ACCESS_OPEN}>{intl.formatMessage(messages.shareAccessOpen)}</option> : null}
-            {allowCollab ? (
-                <option value={ACCESS_COLLAB}>{intl.formatMessage(messages.shareAccessCollab)}</option>
-            ) : null}
-            {allowCompany ? (
-                <option value={ACCESS_COMPANY}>{intl.formatMessage(messages.shareAccessCompany)}</option>
-            ) : null}
+            {allowOpen ? <option value={ACCESS_OPEN}>{formatMessage(messages.shareAccessOpen)}</option> : null}
+            {allowCollab ? <option value={ACCESS_COLLAB}>{formatMessage(messages.shareAccessCollab)}</option> : null}
+            {allowCompany ? <option value={ACCESS_COMPANY}>{formatMessage(messages.shareAccessCompany)}</option> : null}
             <option value={ACCESS_NONE}>
                 {access === ACCESS_NONE
-                    ? intl.formatMessage(messages.shareAccessNone)
-                    : intl.formatMessage(messages.shareAccessRemove)}
+                    ? formatMessage(messages.shareAccessNone)
+                    : formatMessage(messages.shareAccessRemove)}
             </option>
         </select>
     );
 };
 
-export default injectIntl(ShareAccessSelect);
+export default ShareAccessSelect;
