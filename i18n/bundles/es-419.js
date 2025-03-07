493d34f3683be35b5a1874d606f5ab98e43b0a9d
Jenkins CI <jenkins@box.com>
2025-03-06 18:22:52 -0800

    build(examples): v22.1.0-beta.71

---
+++ i18n/bundles/es-419.js
@@ -0,0 +1,26 @@
+/**
+ * Auto-generated Language Bundle File for es-419
+ * @IMPORTANT: DO NOT MODIFY THIS FILE DIRECTLY
+ */
+
+
+import boxCldrData from '@box/cldr-data/locale-data/es-419';
+
+import messageData0 from '@box/box-ai-agent-selector/i18n/es-419';
+import messageData1 from '@box/box-ai-content-answers/i18n/es-419';
+import messageData2 from '@box/item-icon/i18n/es-419';
+import messageData3 from '@box/metadata-editor/i18n/es-419';
+import appMessageData from '../es-419';
+
+const language = 'es-419';
+const locale = 'es';
+
+const messages = {
+    ...messageData0,
+    ...messageData1,
+    ...messageData2,
+    ...messageData3,
+    ...appMessageData,
+};
+
+export { language, locale, messages, boxCldrData };
