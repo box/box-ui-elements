e5ceffa13c3d5e1c776d74dc95aec8db374458ec
Danila Rubleuski <daniilrublevskiy@gmail.com>
2025-03-11 12:25:31 +0100

    feat(boxai-sidebar): Tests cleanup

--- src/elements/common/content-answers/__tests__/ContentAnswersModal.test.tsx
+++ src/elements/common/content-answers/__tests__/ContentAnswersModal.test.tsx
@@ -7,10 +7,8 @@ import { render, screen } from '../../../../test-utils/testing-library';
 import ContentAnswersModal from '../ContentAnswersModal';
 import {
     mockApi,
-    mockApiReturnError,
     mockFile,
     mockQuestionsWithAnswer,
-    mockQuestionsWithError,
 } from '../__mocks__/mocks';
 // @ts-ignore: no ts definition
 import APIContext from '../../api-context';
@@ -39,69 +37,6 @@ describe('elements/common/content-answers/ContentAnswersModal', () => {
 
         const headerIcon = screen.queryByTestId('content-answers-icon-color');
         expect(headerIcon).toBeInTheDocument();
-    });
-
-    test('should ask for answer when prompt is submitted', async () => {
-        const onAskMock = jest.fn();
-        const { answer = '', prompt } = mockQuestionsWithAnswer[0];
-        renderComponent(mockApi, { onAsk: onAskMock });
-
-        const textArea = screen.getByRole('textbox', { name: 'Ask Box AI' });
-        await userEvent.type(textArea, prompt);
-
-        const submitButton = screen.getByRole('button', { name: 'Ask' });
-        await userEvent.click(submitButton);
-
-        expect(mockApi.getIntelligenceAPI().ask).toBeCalledWith(mockQuestion, [{ id: mockFile.id, type: 'file' }], [], {
-            include_citations: true,
-        });
-
-        expect(onAskMock).toBeCalled();
-        expect(screen.getByText(answer)).toBeInTheDocument();
-    });
-
-    test('should render inlineError when ask function failed', async () => {
-        const { prompt } = mockQuestionsWithError[0];
-        renderComponent(mockApiReturnError);
-
-        const textArea = screen.getByRole('textbox', { name: 'Ask Box AI' });
-        await userEvent.type(textArea, prompt);
-
-        const submitButton = screen.getByRole('button', { name: 'Ask' });
-        await userEvent.click(submitButton);
-
-        expect(screen.getByText('The Box AI service was unavailable.')).toBeInTheDocument();
-    });
-
-    test('should render retry button when ask request fails', async () => {
-        const { prompt } = mockQuestionsWithError[0];
-        const apiMock = {
-            ...mockApi,
-            getIntelligenceAPI: jest.fn().mockReturnValue({
-                ask: jest
-                    .fn()
-                    .mockImplementationOnce(() => {
-                        throw new Error('error');
-                    })
-                    .mockResolvedValueOnce({
-                        data: mockQuestionsWithAnswer[0],
-                    }),
-            }),
-        };
-        renderComponent(apiMock);
-
-        const textArea = screen.getByRole('textbox', { name: 'Ask Box AI' });
-        await userEvent.type(textArea, prompt);
-
-        const submitButton = screen.getByRole('button', { name: 'Ask' });
-        await userEvent.click(submitButton);
-
-        const retryButton = screen.getByRole('button', { name: 'Retry' });
-        await userEvent.click(retryButton);
-
-        expect(screen.getByTestId('content-answers-question')).toBeInTheDocument();
-        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-        expect(screen.getByText(mockQuestionsWithAnswer[0].answer!)).toBeInTheDocument();
     });
 
     test('should ask with dialogue history when prompt is submitted', async () => {
