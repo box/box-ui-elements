15bea4f5c9530ed4e4cc11929254db0e3db99f8d
Jenkins CI <jenkins@box.com>
2025-03-17 18:23:09 -0700

    build(examples): v22.1.0-beta.77

--- test/support/commands.js
+++
@@ -1,4 +0,0 @@
-Cypress.Commands.add('getByTestId', testId => cy.get(`[data-testid="${testId}"]`));
-Cypress.Commands.add('getByTitle', title => cy.get(`[title="${title}"]`));
-Cypress.Commands.add('getTooltip', () => cy.get('[role="tooltip"]'));
-Cypress.Commands.add('getByAriaLabel', ariaLabel => cy.get(`[aria-label="${ariaLabel}"]`));
