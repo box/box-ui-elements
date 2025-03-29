066419ba2aca0392cd338ad357a3948c14694a2c
Jenkins CI <jenkins@box.com>
2025-03-28 18:24:45 -0700

    build(examples): v23.0.1-beta.1

---
+++ i18n/bundles/bn-IN.js
@@ -0,0 +1,26 @@
+/**
+ * Auto-generated Language Bundle File for bn-IN
+ * @IMPORTANT: DO NOT MODIFY THIS FILE DIRECTLY
+ */
+
+
+import boxCldrData from '@box/cldr-data/locale-data/bn-IN';
+
+import messageData0 from '@box/box-ai-agent-selector/i18n/bn-IN';
+import messageData1 from '@box/box-ai-content-answers/i18n/bn-IN';
+import messageData2 from '@box/item-icon/i18n/bn-IN';
+import messageData3 from '@box/metadata-editor/i18n/bn-IN';
+import appMessageData from '../bn-IN';
+
+const language = 'bn-IN';
+const locale = 'bn';
+
+const messages = {
+    ...messageData0,
+    ...messageData1,
+    ...messageData2,
+    ...messageData3,
+    ...appMessageData,
+};
+
+export { language, locale, messages, boxCldrData };
