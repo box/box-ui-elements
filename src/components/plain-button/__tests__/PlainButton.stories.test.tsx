2fbba14c846f4e7a904b2839fecd3a7974556aca
Greg Wong <gregorywong@box.com>
2025-04-02 13:46:52 -0500

    chore(jest-imagesnapshot): Remove all image-snapshots and configuration

--- src/components/plain-button/__tests__/PlainButton.stories.test.tsx
+++
@@ -1,9 +0,0 @@
-describe('components/plain-button/PlainButton', () => {
-    test.each(['components-buttons-plainbutton--regular', 'components-buttons-plainbutton--disabled'])(
-        'looks visually correct when using story %s',
-        async id => {
-            const image = await BoxVisualTestUtils.takeScreenshot(id);
-            return expect(image).toMatchImageSnapshot();
-        },
-    );
-});
