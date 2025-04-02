715cb4d41beff552ac3a8075abafb265b8d29369
greg-in-a-box <103291617+greg-in-a-box@users.noreply.github.com>
2025-04-02 17:39:47 -0500

    chore(jest-imagesnapshot): Remove all image-snapshots and configuration (#4046)

--- .storybook/typings.d.ts
+++ .storybook/typings.d.ts
@@ -12,27 +12,3 @@ declare module '*.md' {
     const content: string;
     export = content;
 }
-
-namespace BoxVisualTestUtils {
-    import { Page } from 'puppeteer';
-
-    async function gotoStory(id: string): Promise<Page>;
-
-    async function takeScreenshot(id: string): Promise<Buffer>;
-
-    async function takeScreenshotAfterInput(
-        id: string,
-        selector: string,
-        action?: string,
-        userInput?: string,
-        afterInputSelector?: string,
-    ): Promise<Buffer>;
-
-    async function takeModalScreenshot(id: string, width?: number, height?: number): Promise<Buffer>;
-
-    async function blurInput(selector: string): Promise<void>;
-
-    async function clearInput(selector: string, page: Page): Promise<void>;
-
-    async function sleep(time?: number): Promise<void>;
-}
--- src/components/breadcrumb/__tests__/Breadcrumb.stories.test.js
+++
@@ -1,9 +0,0 @@
-describe('components/breadcrumb/Breadcrumb', () => {
-    test.each([['components-breadcrumb--regular'], ['components-breadcrumb--with-multiple-crumbs']])(
-        'looks visually correct when using story %s',
-        async id => {
-            const image = await BoxVisualTestUtils.takeScreenshot(id);
-            return expect(image).toMatchImageSnapshot();
-        },
-    );
-});
