dd345227b8cb864cb42eabdbefd4208cfdd690c2
Trevor <7311041+tjuanitas@users.noreply.github.com>
2025-05-13 11:00:34 -0700

    fix(gh-pages): deploy storybook from docs folder (#4100)
    * fix(gh-pages): deploy storybook from docs folder
    
    * fix: ensure docs folder does not exist
    
    ---------
    
    Co-authored-by: greg-in-a-box <103291617+greg-in-a-box@users.noreply.github.com>

--- scripts/cut_release.sh
+++ scripts/cut_release.sh
@@ -143,10 +143,10 @@ push_to_gh_pages() {
         printf "${green}Deleted existing gh-pages branch!${end}"
     fi
     git checkout -b gh-pages || return 1
-    rm -rf build
+    rm -rf docs
     cp .storybook/gitignore .gitignore || return 1
+    mv storybook docs || return 1
     git rm -rf --cached . || return 1
-    cp -R storybook/. ./ || return 1
     git add -A || return 1
     git commit --no-verify -am "build(storybook): v$VERSION" || return 1
     git push release gh-pages --force --no-verify || return 1
