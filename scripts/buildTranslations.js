d75daae6622cf80a76e337bb135a6d86c9d983ea
tjuanitas <7311041+tjuanitas@users.noreply.github.com>
2025-06-13 17:29:53 -0700

    fix: order is important

--- scripts/buildTranslations.js
+++ scripts/buildTranslations.js
@@ -4,5 +4,5 @@ const buildLanguageBundles = require('@box/frontend/i18n/buildLanguageBundles');
 const buildTranslations = require('@box/frontend/i18n/buildTranslations');
 const { translationDependencies } = require('./i18n.config');
 
-buildLanguageBundles(translationDependencies.map(pkg => `${pkg}/i18n/[language]`));
 buildTranslations();
+buildLanguageBundles(translationDependencies.map(pkg => `${pkg}/i18n/[language]`));
