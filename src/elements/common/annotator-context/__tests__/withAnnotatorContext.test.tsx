6558a743ce2d7457f8283f076f44d8672266c37d
rafalmaksymiuk <rafal.maksymiuk@gmail.com>
2025-06-14 23:35:41 +0200

    feat: Small updates after review

--- src/elements/common/annotator-context/__tests__/withAnnotatorContext.test.tsx
+++ src/elements/common/annotator-context/__tests__/withAnnotatorContext.test.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { render } from '@testing-library/react';
+import { render } from '../../../../test-utils/testing-library';
 import withAnnotatorContext, { WithAnnotatorContextProps } from '../withAnnotatorContext';
 import { Action } from '../types';
 
