80cc0c7a75ccce0e08317e2a8721514657614d13
rafalmaksymiuk <rafal.maksymiuk@gmail.com>
2025-06-25 16:59:58 +0200

    feat(router): optional router in NavRouter and withNavRouter

--- src/elements/common/nav-router/__tests__/withNavRouter.test.tsx
+++ src/elements/common/nav-router/__tests__/withNavRouter.test.tsx
@@ -1,41 +1,57 @@
 import * as React from 'react';
-import { shallow } from 'enzyme';
-import { createMemoryHistory } from 'history';
-import NavRouter from '../NavRouter';
+import { render } from '@testing-library/react';
 import withNavRouter from '../withNavRouter';
 import { WithNavRouterProps } from '../types';
+
+jest.mock('../NavRouter', () => ({ children }: { children: React.ReactNode }) => (
+    <div data-testid="nav-router-wrapper">{children}</div>
+));
 
 type Props = {
     value?: string;
 };
 
 describe('src/eleemnts/common/nav-router/withNavRouter', () => {
-    const TestComponent = ({ value }: Props) => <div>{`Test ${value}`}</div>;
+    const TestComponent = ({ value }: Props) => <div data-testid="test-component">{`Test ${value}`}</div>;
     const WrappedComponent = withNavRouter(TestComponent);
 
-    const getWrapper = (props?: Props & WithNavRouterProps) => shallow(<WrappedComponent {...props} />);
+    const renderComponent = (props?: Props & WithNavRouterProps) => 
+        render(<WrappedComponent {...props} />);
 
     test('should wrap component with NavRouter', () => {
-        const wrapper = getWrapper();
+        const { getByTestId } = renderComponent();
 
-        expect(wrapper.find(NavRouter)).toBeTruthy();
-        expect(wrapper.find(TestComponent)).toBeTruthy();
+        expect(getByTestId('test-component')).toBeInTheDocument();
+        expect(getByTestId('test-component')).toHaveTextContent('Test undefined');
+        expect(getByTestId('nav-router-wrapper')).toBeInTheDocument();
     });
 
-    test('should provide the appropriate props to NavRouter and the wrapped component', () => {
-        const history = createMemoryHistory();
-        const initialEntries = ['foo'];
-        const value = 'foo';
-        const wrapper = getWrapper({
-            history,
-            initialEntries,
-            value,
+    test('should pass props to wrapped component', () => {
+        const { getByTestId } = renderComponent({ value: 'test-value' });
+
+        expect(getByTestId('test-component')).toBeInTheDocument();
+        expect(getByTestId('test-component')).toHaveTextContent('Test test-value');
+    });
+
+    describe('when routerDisabled feature flag is enabled', () => {
+        test('should return unwrapped component when feature flag is true', () => {
+            const features = { routerDisabled: { value: true } };
+            const { getByTestId, queryByTestId } = renderComponent({ features });
+
+            expect(getByTestId('test-component')).toBeInTheDocument();
+            expect(getByTestId('test-component')).toHaveTextContent('Test undefined');
+            expect(queryByTestId('nav-router-wrapper')).not.toBeInTheDocument();
         });
 
-        const navRouter = wrapper.find(NavRouter);
-        expect(navRouter.prop('history')).toEqual(history);
-        expect(navRouter.prop('initialEntries')).toEqual(initialEntries);
+        test('should pass features prop to NavRouter when router is enabled', () => {
+            const features = { routerDisabled: { value: false } };
+            const { getByTestId } = renderComponent({ features });
 
-        expect(wrapper.find(TestComponent).prop('value')).toEqual(value);
+            expect(getByTestId('test-component')).toBeInTheDocument();
+            expect(getByTestId('test-component')).toHaveTextContent('Test undefined');
+            expect(getByTestId('nav-router-wrapper')).toBeInTheDocument();
+        });
+
+
     });
 });
--- src/elements/common/nav-router/withNavRouter.tsx
+++ src/elements/common/nav-router/withNavRouter.tsx
@@ -1,11 +1,18 @@
 import * as React from 'react';
 import NavRouter from './NavRouter';
 import { WithNavRouterProps } from './types';
+import { isFeatureEnabled } from '../feature-checking';
 
 const withNavRouter = <P extends object>(Component: React.ComponentType<P>): React.FC<P & WithNavRouterProps> => {
-    function WithNavRouter({ history, initialEntries, ...rest }: P & WithNavRouterProps) {
+    function WithNavRouter({ history, initialEntries, features, ...rest }: P & WithNavRouterProps) {
+        const isRouterDisabled = isFeatureEnabled(features, 'routerDisabled.value');
+        
+        if (isRouterDisabled) {
+            return <Component {...(rest as P)} />;
+        }
+
         return (
-            <NavRouter history={history} initialEntries={initialEntries}>
+            <NavRouter history={history} initialEntries={initialEntries} features={features}>
                 <Component {...(rest as P)} />
             </NavRouter>
         );
