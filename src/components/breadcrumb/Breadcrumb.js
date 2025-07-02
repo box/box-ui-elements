92d7a55d6546c7d31f86927c7612e900add7ebe0
Bartosz Kepka <bkepka@box.com>
2025-07-02 12:49:25 +0200

    fix(breadcrumb): Add missing key property

--- src/components/breadcrumb/Breadcrumb.js
+++ src/components/breadcrumb/Breadcrumb.js
@@ -71,6 +71,7 @@ const Breadcrumb = ({
                 <Crumb
                     className={classNames({ 'no-shrink': hasEllipsis && i === numItemsBeforeOverflow })}
                     isLastCrumb={isLastCrumb}
+                    key={`${item.props.id}-${i}`}
                 >
                     {item}
                 </Crumb>
