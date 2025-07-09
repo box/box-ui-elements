d1d126e16173f42ec06d8f05dc37a2c11d351476
Greg Wong <gregorywong@box.com>
2025-07-09 19:38:35 -0400

    feat(metadata-view): Added Metadata V2 button

---
+++ src/elements/common/sub-header/messages.ts
@@ -0,0 +1,11 @@
+import { defineMessages } from 'react-intl';
+
+const messages = defineMessages({
+    metadata: {
+        defaultMessage: 'Metadata',
+        description: 'Text for metadata button that will open the metadata side panel',
+        id: 'boxui.subHeader.metadata',
+    },
+});
+
+export default messages;
