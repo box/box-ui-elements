485137ed526d1cab0572445b424a463dd9fe7743
rafalmaksymiuk <rafal.maksymiuk@gmail.com>
2025-07-15 15:46:46 +0200

    feat(router): noop commit to fix merge queue

--- src/elements/content-sidebar/__tests__/AddTaskButton.test.js
+++ src/elements/content-sidebar/__tests__/AddTaskButton.test.js
@@ -26,7 +26,7 @@ jest.mock('../TaskModal', () => ({ isTaskFormOpen, onModalClose, taskType }) => 
 
 describe('elements/content-sidebar/AddTaskButton', () => {
     /*
-    1. Pushing the  open state into history keeps the sidebar open upon resize and refresh
+    1. Pushing the open state into history keeps the sidebar open upon resize and refresh
     2. Preventing the sidebar from closing keeps the task modal open upon edit and resize
     */
 
