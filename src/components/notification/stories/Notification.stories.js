33c4b0f55c1359878a874548d1a56a719098041e
Greg Wong <gregorywong@box.com>
2025-07-28 18:58:06 -0400

    fix(blueprint): Bump blueprint

--- src/components/notification/stories/Notification.stories.js
+++ src/components/notification/stories/Notification.stories.js
@@ -40,6 +40,39 @@ export const error = () => (
     </IntlProvider>
 );
 
+export const basicV2 = () => (
+    <IntlProvider locale="en">
+        <Notification useV2Icons>This is a default notification.</Notification>
+    </IntlProvider>
+);
+
+export const infoV2 = () => (
+    <IntlProvider locale="en">
+        <Notification type="info" useV2Icons>
+            <span>This is an info notification with a button.</span>
+            <Button>Click me</Button>
+        </Notification>
+    </IntlProvider>
+);
+
+export const warnV2 = () => (
+    <IntlProvider locale="en">
+        <Notification type="warn" useV2Icons>
+            <span>This is a warning notification with two buttons.</span>
+            <Button>Click me</Button>
+            <Button>Click me again</Button>
+        </Notification>
+    </IntlProvider>
+);
+
+export const errorV2 = () => (
+    <IntlProvider locale="en">
+        <Notification type="error" useV2Icons>
+            <span>This is an error notification.</span>
+        </Notification>
+    </IntlProvider>
+);
+
 export default {
     title: 'Components/Notifications/Notification',
     component: Notification,
