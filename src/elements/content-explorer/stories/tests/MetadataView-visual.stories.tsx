330ae3398998c7e05778a52f15f2e5d0f815a0b3
jfox-box <jonathanfox@box.com>
2025-08-15 16:10:55 -0700

    fix(content-explorer): Fix flaky SubheaderV2 folder title (#4229)

--- src/elements/content-explorer/stories/tests/MetadataView-visual.stories.tsx
+++ src/elements/content-explorer/stories/tests/MetadataView-visual.stories.tsx
@@ -8,6 +8,7 @@ import noop from 'lodash/noop';
 import ContentExplorer from '../../ContentExplorer';
 import { DEFAULT_HOSTNAME_API } from '../../../../constants';
 import { mockMetadata, mockSchema } from '../../../common/__mocks__/mockMetadata';
+import { mockRootFolder } from '../../../common/__mocks__/mockRootFolder';
 
 // The intent behind relying on mockMetadata is to allow a developer to paste in their own metadata template schema for use with live API calls.
 const { scope: templateScope, templateKey } = mockSchema;
@@ -179,6 +180,9 @@ const meta: Meta<typeof ContentExplorer> = {
                 }),
                 http.get(`${DEFAULT_HOSTNAME_API}/2.0/metadata_templates/enterprise/templateName/schema`, () => {
                     return HttpResponse.json(mockSchema);
+                }),
+                http.get(`${DEFAULT_HOSTNAME_API}/2.0/folders/:id`, () => {
+                    return HttpResponse.json(mockRootFolder);
                 }),
             ],
         },
