face14fa04ed0d26fcb1211c16513cf7e0555971
bfoxx <bfoxx@box.com>
2025-08-15 16:36:41 -0400

    feat(timestamped-comments): fixing flow errors

--- src/elements/content-sidebar/activity-feed/comment-form/CommentForm.js
+++ src/elements/content-sidebar/activity-feed/comment-form/CommentForm.js
@@ -51,6 +51,26 @@ export type CommentFormProps = {
     user?: User,
     features?: FeatureConfig,
 };
+// flow does not recognise FILE_EXTENSIONS in ../common/item/constants.ts as it is ts so
+// we need to define them here
+export const VIDEO_EXTENSIONS = [
+    '3g2',
+    '3gp',
+    'avi',
+    'flv',
+    'm2ts',
+    'm2v',
+    'm4v',
+    'mkv',
+    'mov',
+    'mp4',
+    'mpeg',
+    'mpg',
+    'mts',
+    'ogg',
+    'qt',
+    'wmv',
+];
 
 const getEditorState = (shouldFocusOnOpen: boolean, message?: string): EditorState =>
     shouldFocusOnOpen
