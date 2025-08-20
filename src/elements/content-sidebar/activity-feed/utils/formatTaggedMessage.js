d61a3ed9e6d0ddc1af76451e4fff81ca4fec0c86
bfoxx <bfoxx@box.com>
2025-08-20 10:55:31 -0400

    feat(timestamped-comments): reverting tagged mesage formatting

--- src/elements/content-sidebar/activity-feed/utils/formatTaggedMessage.js
+++ src/elements/content-sidebar/activity-feed/utils/formatTaggedMessage.js
@@ -13,55 +13,7 @@ import UserLink from '../common/user-link';
 // urls: (?:\b)((?:(?:ht|f)tps?:\/\/)[\w\._\-]+(:\d+)?(\/[\w\-_\.~\+\/#\?&%=:\[\]@!$'\(\)\*;,]*)?)
 // NOTE: There are useless escapes in the regex below, should probably remove them when safe
 // eslint-disable-next-line
-const splitRegex =
-    /((?:[@＠﹫]\[[0-9]+:[^\]]+])|(?:\b(?:(?:ht|f)tps?:\/\/)[\w\._\-]+(?::\d+)?(?:\/[\w\-_\.~\+\/#\?&%=:\[\]@!$'\(\)\*;,]*)?))/gim;
-
-/**
- * Converts a timestamp string (hh:mm:ss) to total seconds
- * @param {string} timestamp The timestamp string in format "hh:mm:ss"
- * @returns {number} The total seconds
- */
-const convertTimestampToSeconds = (timestamp: string): number => {
-    const timeParts = timestamp.split(':');
-    const hours = parseInt(timeParts[0], 10) || 0;
-    const minutes = parseInt(timeParts[1], 10) || 0;
-    const seconds = parseInt(timeParts[2], 10) || 0;
-    const totalSeconds = hours * 3600 + minutes * 60 + seconds;
-    return totalSeconds;
-};
-
-/**
- * Formats text containing a timestamp by wrapping the timestamp in a Link component
- * @param {string} text The text containing the timestamp
- * @param {RegExpMatchArray} timestampMatch The regex match result for the timestamp
- * @param {string} contentKey The key to use for the React Fragment
- * @returns {React.Node} A React Fragment with formatted timestamp
- */
-const formatTimestamp = (text: string, timestampMatch: RegExpMatchArray, contentKey: string): React.Node => {
-    const [, timestamp] = timestampMatch;
-    const beforeTimestamp = text.substring(0, timestampMatch.index);
-    const afterTimestamp = text.substring(timestampMatch.index + timestamp.length);
-    const handleClick = e => {
-        e.preventDefault();
-        const videoContainer = document.querySelector('.bp-media-dash');
-        if (videoContainer) {
-            const video = videoContainer.querySelector('video');
-            if (video) {
-                const totalSeconds = convertTimestampToSeconds(timestamp);
-                video.currentTime = totalSeconds;
-            }
-        }
-    };
-    return (
-        <React.Fragment key={contentKey}>
-            {beforeTimestamp}
-            <Link href={`#${timestamp}`} onClick={handleClick}>
-                {timestamp}
-            </Link>
-            {afterTimestamp}
-        </React.Fragment>
-    );
-};
+const splitRegex = /((?:[@＠﹫]\[[0-9]+:[^\]]+])|(?:\b(?:(?:ht|f)tps?:\/\/)[\w\._\-]+(?::\d+)?(?:\/[\w\-_\.~\+\/#\?&%=:\[\]@!$'\(\)\*;,]*)?))/gim;
 
 /**
  * Formats a message a string and replaces the following:
@@ -102,14 +54,6 @@ const formatTaggedMessage = (
             );
         }
 
-        // Check for timestamp in first item only
-        if (contentIndex === 0 && !shouldReturnString) {
-            const timestampMatch = text.match(/(\d{1,2}:\d{2}:\d{2})/);
-            if (timestampMatch) {
-                return formatTimestamp(text, timestampMatch, contentKey);
-            }
-        }
-
         if (!shouldReturnString) {
             // attempt url match
             // NOTE: There are useless escapes in the regex below, should probably remove them when safe
@@ -130,11 +74,7 @@ const formatTaggedMessage = (
         return text;
     });
 
-    if (shouldReturnString) {
-        return contentItems.join('');
-    }
-
-    return contentItems;
+    return shouldReturnString ? contentItems.join('') : contentItems;
 };
 
 export default formatTaggedMessage;
