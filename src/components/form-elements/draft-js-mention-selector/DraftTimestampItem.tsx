b9ceda424f31d289f5589fc94ea06b150a9bfdb4
bfoxx <bfoxx@box.com>
2025-09-05 12:29:17 -0400

    feat(timestamped-comments): removing timestamp from aria label

--- src/components/form-elements/draft-js-mention-selector/DraftTimestampItem.tsx
+++ src/components/form-elements/draft-js-mention-selector/DraftTimestampItem.tsx
@@ -1,29 +1,19 @@
 import * as React from 'react';
 import { useIntl } from 'react-intl';
-import { ContentState, EntityInstance } from 'draft-js';
 import messages from './messages';
 import './DraftTimestamp.scss';
 
 interface Props {
     children: React.ReactNode;
-    entityKey: string;
-    contentState: ContentState;
 }
 
-interface TimestampEntityData {
-    timestamp: string;
-}
-
-const DraftTimestampItem: React.FC<Props> = ({ children, entityKey, contentState }) => {
+const DraftTimestampItem: React.FC<Props> = ({ children }) => {
     const { formatMessage } = useIntl();
-    const entity: EntityInstance = contentState.getEntity(entityKey);
-    const { timestamp }: TimestampEntityData = entity.getData();
     const videoTimestampLabel = formatMessage(messages.commentTimestampLabel);
-    const videoTimestampLabelText = `${videoTimestampLabel}: ${timestamp}`;
     return (
         <div
             className="bcs-CommentTimestamp-entity"
-            aria-label={videoTimestampLabelText}
+            aria-label={videoTimestampLabel}
             contentEditable={false}
             suppressContentEditableWarning={true}
         >
