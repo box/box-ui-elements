59b4a639a84756056d05c9c8b58456c9a9e66b5e
Jason Pan <jpan@box.com>
2025-10-06 14:12:03 -0400

    fix(content-picker): broken folder navigation
    test

--- src/elements/content-picker/stories/tests/ContentPicker-e2e.test.stories.tsx
+++ src/elements/content-picker/stories/tests/ContentPicker-e2e.test.stories.tsx
@@ -1,3 +1,5 @@
+import { expect, userEvent, waitFor, within } from 'storybook/test';
+import sleep from '../../../../utils/sleep';
 import ContentPicker from '../../ContentPicker';
 
 export const basic = {
@@ -9,6 +11,30 @@ export const basic = {
 export const withPagination = {
     args: {
         initialPageSize: 3,
+    },
+};
+
+// This test covers a regression where different values for type would sometimes result in broken folder navigation
+// where clicking on a folder would trigger the API call but not refresh the UI
+export const singleSelectWithItemTypes = {
+    args: {
+        maxSelectable: 1,
+        type: 'file,folder',
+    },
+    play: async ({ canvasElement }) => {
+        const canvas = within(canvasElement);
+        await waitFor(
+            () => {
+                expect(canvas.getByRole('button', { name: 'An Ordered Folder' })).toBeInTheDocument();
+            },
+            { timeout: 10000 },
+        );
+        await sleep(1000);
+        userEvent.click(canvas.getByRole('button', { name: 'An Ordered Folder' }));
+
+        await waitFor(() => {
+            expect(canvas.getByRole('gridcell', { name: 'Audio.mp3' })).toBeInTheDocument();
+        });
     },
 };
 
