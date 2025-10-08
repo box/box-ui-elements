c3b5e4222718f531a3cb6ef8f686bcc0eb0523f6
Jenkins CI <jenkins@box.com>
2025-10-08 12:00:10 -0700

    build(storybook): v25.2.0-beta.2

--- src/elements/content-sharing/utils/index.ts
+++
@@ -1,4 +0,0 @@
-export { convertItemResponse } from './convertItemResponse';
-export { convertCollabsResponse } from './convertCollaborators';
-export { getAllowedAccessLevels } from './getAllowedAccessLevels';
-export { getAllowedPermissionLevels } from './getAllowedPermissionLevels';
--- src/elements/content-uploader/nameCellRenderer.tsx
+++
@@ -1,6 +0,0 @@
-import * as React from 'react';
-import IconName from './IconName';
-import { cellRendererProps } from './ItemList';
-
-export default (isResumableUploadsEnabled: boolean) =>
-    ({ rowData }: cellRendererProps) => <IconName isResumableUploadsEnabled={isResumableUploadsEnabled} {...rowData} />;
