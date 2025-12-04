15f0f8e2bc0053117d85e403a8403b2ebbd5ee60
zhirongwang <rong131030001@gmail.com>
2025-12-03 23:17:42 -0800

    feat(presence-avatar-list): add test (WEBAPP-41740)

--- src/features/presence/__tests__/PresenceAvatarList.test.tsx
+++ src/features/presence/__tests__/PresenceAvatarList.test.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { shallow, ShallowWrapper } from 'enzyme';
+import { Tooltip as BPTooltip } from '@box/blueprint-web';
 import collaboratorList from '../__mocks__/collaborators';
 // @ts-ignore flow import
 import PresenceAvatar from '../PresenceAvatar';
@@ -20,136 +21,132 @@ describe('features/presence/PresenceAvatarList', () => {
             expect(wrapper.exists('.bdl-PresenceAvatarList')).toBe(false);
         });
 
-        test('should correctly render collaborators without additional count when number of collaborators is less than or equal to maxDisplayedAvatars', () => {
-            const maxDisplayedAvatars = 3;
-            const wrapper = getWrapper({
-                collaborators: collaboratorList.slice(0, maxDisplayedAvatars),
-                maxDisplayedAvatars,
-            });
-
-            expect(wrapper.find(PresenceAvatar).length).toBe(maxDisplayedAvatars);
-            expect(wrapper.exists('.bdl-PresenceAvatarList-count')).toBe(false);
-        });
-
-        test('should correctly render collaborators with additional count when number of collaborators is greater than maxDisplayedAvatars', () => {
-            const maxDisplayedAvatars = 3;
-            const wrapper = getWrapper({
-                maxDisplayedAvatars,
-            });
-
-            expect(wrapper.find(PresenceAvatar).length).toBe(maxDisplayedAvatars);
-            expect(wrapper.exists('.bdl-PresenceAvatarList-count')).toBe(true);
-        });
-
-        test('should hide additional count if hideAdditionalCount is specified', () => {
-            const maxDisplayedAvatars = 3;
-            const wrapper = getWrapper({
-                hideAdditionalCount: true,
-                maxDisplayedAvatars,
-            });
-
-            expect(wrapper.exists('.bdl-PresenceAvatarList')).toBe(true);
-            expect(wrapper.find(PresenceAvatar).length).toBe(maxDisplayedAvatars);
-            expect(wrapper.exists('.bdl-PresenceAvatarList-count')).toBe(false);
-        });
-
-        test.each(['focus', 'mouseenter'])(
-            'should show tooltip when correponding avatar encounters %s event',
-            event => {
-                const onAvatarMouseEnter = jest.fn();
+        describe.each([true, false])('when isPreviewModernizationEnabled is %s', isPreviewModernizationEnabled => {
+            test('should correctly render collaborators without additional count when number of collaborators is less than or equal to maxDisplayedAvatars', () => {
+                const maxDisplayedAvatars = 3;
                 const wrapper = getWrapper({
-                    onAvatarMouseEnter,
+                    collaborators: collaboratorList.slice(0, maxDisplayedAvatars),
+                    maxDisplayedAvatars,
+                    isPreviewModernizationEnabled,
                 });
 
-                expect(
-                    wrapper
-                        .find(Tooltip)
-                        .first()
-                        .prop('isShown'),
-                ).toBe(false);
-
-                // Trigger event on the first avatar
-                wrapper
-                    .find(PresenceAvatar)
-                    .first()
-                    .simulate(event);
-
-                expect(onAvatarMouseEnter).toHaveBeenCalledWith('1');
-                expect(
-                    wrapper
-                        .find(Tooltip)
-                        .first()
-                        .prop('isShown'),
-                ).toBe(true);
-            },
-        );
-
-        test.each(['blur', 'mouseleave'])('should hide tooltip when correponding avatar encounters %s event', event => {
-            const onAvatarMouseLeave = jest.fn();
-            const wrapper = getWrapper({
-                onAvatarMouseLeave,
+                expect(wrapper.find(PresenceAvatar).length).toBe(maxDisplayedAvatars);
+                expect(wrapper.exists('.bdl-PresenceAvatarList-count')).toBe(false);
             });
 
-            // Cause the tooltip to show
-            wrapper
-                .find(PresenceAvatar)
-                .first()
-                .simulate('focus');
+            test('should correctly render collaborators with additional count when number of collaborators is greater than maxDisplayedAvatars', () => {
+                const maxDisplayedAvatars = 3;
+                const wrapper = getWrapper({
+                    maxDisplayedAvatars,
+                    isPreviewModernizationEnabled,
+                });
 
-            expect(
-                wrapper
-                    .find(Tooltip)
-                    .first()
-                    .prop('isShown'),
-            ).toBe(true);
-
-            // Trigger event on the first avatar
-            wrapper
-                .find(PresenceAvatar)
-                .first()
-                .simulate(event);
-
-            expect(onAvatarMouseLeave).toHaveBeenCalled();
-            expect(
-                wrapper
-                    .find(Tooltip)
-                    .first()
-                    .prop('isShown'),
-            ).toBe(false);
-        });
-
-        test('should pass through additional attributes when specified', () => {
-            const avatarAttr = { 'data-resin-target': 'avatar' };
-            const wrapper = getWrapper({
-                avatarAttributes: avatarAttr,
-                'data-resin-feature': 'presence',
+                expect(wrapper.find(PresenceAvatar).length).toBe(maxDisplayedAvatars);
+                expect(wrapper.exists('.bdl-PresenceAvatarList-count')).toBe(true);
             });
 
-            expect(wrapper.find('.bdl-PresenceAvatarList').prop('data-resin-feature')).toEqual('presence');
-            expect(
-                wrapper
-                    .find(PresenceAvatar)
-                    .first()
-                    .prop('data-resin-target'),
-            ).toEqual('avatar');
-        });
+            test('should hide additional count if hideAdditionalCount is specified', () => {
+                const maxDisplayedAvatars = 3;
+                const wrapper = getWrapper({
+                    hideAdditionalCount: true,
+                    maxDisplayedAvatars,
+                    isPreviewModernizationEnabled,
+                });
 
-        test('should correctly render collaborators with additional count when number of collaborators is greater than maxAddionalCollaboratorsNum + maxDisplayedAvatars', () => {
-            const maxDisplayedAvatars = 2;
-            const maxAdditionalCollaborators = 1;
-            const wrapper = getWrapper({ maxAdditionalCollaborators, maxDisplayedAvatars });
+                expect(wrapper.exists('.bdl-PresenceAvatarList')).toBe(true);
+                expect(wrapper.find(PresenceAvatar).length).toBe(maxDisplayedAvatars);
+                expect(wrapper.exists('.bdl-PresenceAvatarList-count')).toBe(false);
+            });
 
-            expect(wrapper.find(PresenceAvatar).length).toBe(maxDisplayedAvatars);
-            expect(wrapper.find('.bdl-PresenceAvatarList-count').text()).toBe('1+');
-        });
+            test.each(['focus', 'mouseenter'])(
+                'should show tooltip when correponding avatar encounters %s event',
+                event => {
+                    const onAvatarMouseEnter = jest.fn();
+                    const wrapper = getWrapper({
+                        onAvatarMouseEnter,
+                        isPreviewModernizationEnabled,
+                    });
 
-        test('should correctly render collaborators with additional count when number of collaborators is less than maxAddionalCollaboratorsNum + maxDisplayedAvatars', () => {
-            const maxDisplayedAvatars = 2;
-            const maxAdditionalCollaborators = 10;
-            const wrapper = getWrapper({ maxAdditionalCollaborators, maxDisplayedAvatars });
+                    if (isPreviewModernizationEnabled) {
+                        // Blueprint tooltip doesn't use isShown prop, just verify it exists
+                        expect(wrapper.find(BPTooltip).length).toBeGreaterThan(0);
+                    } else {
+                        expect(wrapper.find(Tooltip).first().prop('isShown')).toBe(false);
+                    }
 
-            expect(wrapper.find(PresenceAvatar).length).toBe(maxDisplayedAvatars);
-            expect(wrapper.find('.bdl-PresenceAvatarList-count').text()).toBe('+3');
+                    // Trigger event on the first avatar
+                    wrapper.find(PresenceAvatar).first().simulate(event);
+
+                    expect(onAvatarMouseEnter).toHaveBeenCalledWith('1');
+                    if (!isPreviewModernizationEnabled) {
+                        expect(wrapper.find(Tooltip).first().prop('isShown')).toBe(true);
+                    }
+                },
+            );
+
+            test.each(['blur', 'mouseleave'])(
+                'should hide tooltip when correponding avatar encounters %s event',
+                event => {
+                    const onAvatarMouseLeave = jest.fn();
+                    const wrapper = getWrapper({
+                        onAvatarMouseLeave,
+                        isPreviewModernizationEnabled,
+                    });
+
+                    // Cause the tooltip to show
+                    wrapper.find(PresenceAvatar).first().simulate('focus');
+
+                    if (!isPreviewModernizationEnabled) {
+                        expect(wrapper.find(Tooltip).first().prop('isShown')).toBe(true);
+                    }
+
+                    // Trigger event on the first avatar
+                    wrapper.find(PresenceAvatar).first().simulate(event);
+
+                    expect(onAvatarMouseLeave).toHaveBeenCalled();
+                    if (!isPreviewModernizationEnabled) {
+                        expect(wrapper.find(Tooltip).first().prop('isShown')).toBe(false);
+                    }
+                },
+            );
+
+            test('should pass through additional attributes when specified', () => {
+                const avatarAttr = { 'data-resin-target': 'avatar' };
+                const wrapper = getWrapper({
+                    avatarAttributes: avatarAttr,
+                    'data-resin-feature': 'presence',
+                    isPreviewModernizationEnabled,
+                });
+
+                expect(wrapper.find('.bdl-PresenceAvatarList').prop('data-resin-feature')).toEqual('presence');
+                expect(wrapper.find(PresenceAvatar).first().prop('data-resin-target')).toEqual('avatar');
+            });
+
+            test('should correctly render collaborators with additional count when number of collaborators is greater than maxAddionalCollaboratorsNum + maxDisplayedAvatars', () => {
+                const maxDisplayedAvatars = 2;
+                const maxAdditionalCollaborators = 1;
+                const wrapper = getWrapper({
+                    maxAdditionalCollaborators,
+                    maxDisplayedAvatars,
+                    isPreviewModernizationEnabled,
+                });
+
+                expect(wrapper.find(PresenceAvatar).length).toBe(maxDisplayedAvatars);
+                expect(wrapper.find('.bdl-PresenceAvatarList-count').text()).toBe('1+');
+            });
+
+            test('should correctly render collaborators with additional count when number of collaborators is less than maxAddionalCollaboratorsNum + maxDisplayedAvatars', () => {
+                const maxDisplayedAvatars = 2;
+                const maxAdditionalCollaborators = 10;
+                const wrapper = getWrapper({
+                    maxAdditionalCollaborators,
+                    maxDisplayedAvatars,
+                    isPreviewModernizationEnabled,
+                });
+
+                expect(wrapper.find(PresenceAvatar).length).toBe(maxDisplayedAvatars);
+                expect(wrapper.find('.bdl-PresenceAvatarList-count').text()).toBe('+3');
+            });
         });
     });
 });
