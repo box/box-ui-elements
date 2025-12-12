8a3e853ec34f7a73cde6a8864b06407ced378826
Polina Yaromchyk <pyaromchyk@box.com>
2025-12-12 13:25:50 +0100

    fix(docgen-sidebar): fix file extension (DOCGEN-7872)

--- src/elements/content-sidebar/DocGenSidebar/stories/DocGenSidebar.stories.tsx
+++ src/elements/content-sidebar/DocGenSidebar/stories/DocGenSidebar.stories.tsx
@@ -1,5 +1,7 @@
-// @flow
+import noop from 'lodash/noop';
 import { http, HttpResponse } from 'msw';
+import type { HttpHandler } from 'msw';
+import type { Meta } from '@storybook/react';
 import ContentSidebar from '../../ContentSidebar';
 import { mockFileRequest } from '../../stories/__mocks__/ContentSidebarMocks';
 import mockDocGenTags from '../../__mocks__/DocGenSidebar.mock';
@@ -12,7 +14,7 @@ const defaultArgs = {
         hasClassification: true,
         hasRetentionPolicy: true,
     },
-    features: global.FEATURES,
+    features: global.FEATURE_FLAGS,
     fileId: global.FILE_ID,
     hasActivityFeed: true,
     hasMetadata: true,
@@ -21,19 +23,20 @@ const defaultArgs = {
     token: global.TOKEN,
 };
 
+const docGenSidebarProps = {
+    enabled: true,
+    isDocGenTemplate: true,
+    checkDocGenTemplate: noop,
+    getDocGenTags: async () => ({
+        pagination: {},
+        data: mockDocGenTags,
+    }),
+};
+
 export const basic = {
     args: {
         defaultView: 'docgen',
-        docGenSidebarProps: {
-            enabled: true,
-            isDocGenTemplate: true,
-            checkDocGenTemplate: () => {},
-            getDocGenTags: () =>
-                Promise.resolve({
-                    pagination: {},
-                    data: mockDocGenTags,
-                }),
-        },
+        docGenSidebarProps,
     },
 };
 
@@ -41,20 +44,11 @@ export const withModernizedBlueprint = {
     args: {
         enableModernizedComponents: true,
         defaultView: 'docgen',
-        docGenSidebarProps: {
-            enabled: true,
-            isDocGenTemplate: true,
-            checkDocGenTemplate: () => {},
-            getDocGenTags: () =>
-                Promise.resolve({
-                    pagination: {},
-                    data: mockDocGenTags,
-                }),
-        },
+        docGenSidebarProps,
     },
 };
 
-export default {
+const meta: Meta<typeof ContentSidebar> & { parameters: { msw: { handlers: HttpHandler[] } } } = {
     title: 'Elements/ContentSidebar/DocGenSidebar',
     component: ContentSidebar,
     args: defaultArgs,
@@ -68,3 +62,5 @@ export default {
         },
     },
 };
+
+export default meta;
