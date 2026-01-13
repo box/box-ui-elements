6231a0c9a963605ceb71032d06f66ec767076795
Duncan Hsu <duncanhsu999@gmail.com>
2026-01-12 17:13:10 -0800

    refactor(metadata-instance-editor): migrate edit button to BP

--- src/features/metadata-instance-editor/Instance.js
+++ src/features/metadata-instance-editor/Instance.js
@@ -6,15 +6,15 @@ import isEqual from 'lodash/isEqual';
 import cloneDeep from 'lodash/cloneDeep';
 import noop from 'lodash/noop';
 
+import { IconButton, TooltipProvider, Tooltip } from '@box/blueprint-web';
+import { Pencil } from '@box/blueprint-web-assets/icons/Medium';
+
 import type { AgentType } from '@box/box-ai-agent-selector';
 import Collapsible from '../../components/collapsible/Collapsible';
 import Form from '../../components/form-elements/form/Form';
 import LoadingIndicatorWrapper from '../../components/loading-indicator/LoadingIndicatorWrapper';
-import PlainButton from '../../components/plain-button/PlainButton';
-import Tooltip from '../../components/tooltip';
 import IconMetadataColored from '../../icons/general/IconMetadataColored';
 import IconAlertCircle from '../../icons/general/IconAlertCircle';
-import IconEdit from '../../icons/general/IconEdit';
 import { bdlWatermelonRed } from '../../styles/variables';
 import { scrollIntoView } from '../../utils/dom';
 
@@ -621,18 +621,19 @@ class Instance extends React.PureComponent<Props, State> {
         if (canEdit && !isDirty && !isBusy) {
             const metadataLabelEditText = intl.formatMessage(messages.metadataEditTooltip);
             return (
-                <Tooltip position="top-left" text={metadataLabelEditText}>
-                    <PlainButton
-                        aria-label={metadataLabelEditText}
-                        aria-pressed={isEditing}
-                        className={editClassName}
-                        data-resin-target="metadata-instanceedit"
-                        onClick={this.toggleIsEditing}
-                        type="button"
-                    >
-                        <IconEdit />
-                    </PlainButton>
-                </Tooltip>
+                <TooltipProvider>
+                    <Tooltip content={metadataLabelEditText}>
+                        <IconButton
+                            aria-label={metadataLabelEditText}
+                            aria-pressed={isEditing}
+                            className={editClassName}
+                            data-resin-target="metadata-instanceedit"
+                            icon={Pencil}
+                            onClick={this.toggleIsEditing}
+                            size="x-small"
+                        />
+                    </Tooltip>
+                </TooltipProvider>
             );
         }
         return null;
