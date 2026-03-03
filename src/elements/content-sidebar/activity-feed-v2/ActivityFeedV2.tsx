0d6ef56b1ffb1a32d0520f0e8b4fc2ca5f4482e2
Jackie Jou <jackiejou@users.noreply.github.com>
2026-03-02 17:01:20 -0800

    feat(activity-feed): add new v2 placeholder

---
+++ src/elements/content-sidebar/activity-feed-v2/ActivityFeedV2.tsx
@@ -0,0 +1,10 @@
+/**
+ * @file Activity Feed V2 adapter placeholder
+ * @author Box
+ */
+
+import * as React from 'react';
+
+const ActivityFeedV2 = () => <div data-testid="activity-feed-adapter-v2">Activity Feed V2</div>;
+
+export default ActivityFeedV2;
