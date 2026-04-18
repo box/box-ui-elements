6418d9b67aa34ae70247bab492a4ca86f38d0995
Jenkins CI <jenkins@box.com>
2026-04-17 18:21:52 -0700

    build(storybook): v27.0.0-beta.7

--- src/components/flyout/FlyoutContext.js
+++
@@ -1,9 +0,0 @@
-// @flow
-import * as React from 'react';
-import noop from 'lodash/noop';
-
-type FlyoutContextValues = {
-    closeOverlay: Function,
-};
-
-export default React.createContext<FlyoutContextValues>({ closeOverlay: noop });
