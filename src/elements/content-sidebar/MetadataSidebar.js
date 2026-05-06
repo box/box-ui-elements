ed8c9837b6917e0c585004e05aecee4cb76f965f
Oleh Rybak <olegryboleg@gmail.com>
2026-05-06 13:28:58 +0200

    fix(metadata): fix types

--- src/elements/content-sidebar/MetadataSidebar.js
+++ src/elements/content-sidebar/MetadataSidebar.js
@@ -48,7 +48,7 @@ type ExternalProps = {
     registerOpenWarningModalCallback?: (handleWarningModalOpen: (isOpen: boolean) => void) => void,
     onWarningModalDiscard?: () => void,
     onWarningModalClose?: () => void,
-    trackEvent?: (eventName: string, data?: Record<string, unknown>) => void,
+    trackEvent?: (eventName: string, data?: { [string]: any }) => void,
 };
 
 type PropsWithoutContext = {
