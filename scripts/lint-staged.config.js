3185e8edae02361a341ad7b4baabc5b9d20ace35
jfox-box <jonathanfox@box.com>
2026-06-16 11:59:11 -0700

    fix(deps): bump husky and lint-staged

--- scripts/lint-staged.config.js
+++ scripts/lint-staged.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    '*.{js,ts,tsx}': ['prettier --write', 'eslint --fix', 'git add'],
-    '*.md': ['prettier --write --parser=markdown', 'git add'],
-    '*.json': ['prettier --write --parser=json', 'git add'],
-    '*.html': ['prettier --write --parser=html', 'git add'],
-    '*.scss': ['prettier --write --parser=scss', 'stylelint --fix', 'git add'],
-    '*.css': ['prettier --write --parser=css', 'stylelint --fix', 'git add'],
+    '*.{js,ts,tsx}': ['prettier --write', 'eslint --fix'],
+    '*.md': ['prettier --write --parser=markdown'],
+    '*.json': ['prettier --write --parser=json'],
+    '*.html': ['prettier --write --parser=html'],
+    '*.scss': ['prettier --write --parser=scss', 'stylelint --fix'],
+    '*.css': ['prettier --write --parser=css', 'stylelint --fix'],
 };
