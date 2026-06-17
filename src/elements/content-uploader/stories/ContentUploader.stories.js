f7ecc3a8f0f05bd82b9e3e614fe7a103b3926763
Oleh Rybak <100802720+olehrybak@users.noreply.github.com>
2026-06-18 00:26:44 +0200

    feat: implement open/closing logic for modernized content uploader (#4628)
    * feat: implement open/closing logic for modernized content uploader
    
    * test: update storybook
    
    * fix: avoid starting timer on every re-render

--- src/elements/content-uploader/stories/ContentUploader.stories.js
+++ src/elements/content-uploader/stories/ContentUploader.stories.js
@@ -1,6 +1,8 @@
 // @flow
+import { http, HttpResponse } from 'msw';
 import ContentUploader from '../ContentUploader';
 import mockTheme from '../../common/__mocks__/mockTheme';
+import { DEFAULT_HOSTNAME_API, DEFAULT_HOSTNAME_UPLOAD } from '../../../constants';
 
 export const basic = {};
 
@@ -13,6 +15,24 @@ export const withTheming = {
 export const withModernizedUploads = {
     args: {
         enableModernizedUploads: true,
+        useUploadsManager: true,
+        files: [
+            new File(['contents'], 'upload_file_1.txt', { type: 'text/plain' }),
+            new File(['contents'], 'upload_file_2.txt', { type: 'text/plain' }),
+            new File(['contents'], 'upload_file_3.txt', { type: 'text/plain' }),
+        ],
+    },
+    parameters: {
+        msw: {
+            handlers: [
+                http.options(`${DEFAULT_HOSTNAME_API}/2.0/files/content`, () => {
+                    return HttpResponse.json({});
+                }),
+                http.post(`${DEFAULT_HOSTNAME_UPLOAD}/api/2.0/files/content`, () => {
+                    return HttpResponse.json({});
+                }),
+            ],
+        },
     },
 };
 
