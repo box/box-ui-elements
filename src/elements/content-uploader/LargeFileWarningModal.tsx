81c3d704b99ab3f1072d24507b6997d76fa342b3
Oleh Rybak <olegryboleg@gmail.com>
2026-06-29 21:44:31 +0200

    fix: code review changes

--- src/elements/content-uploader/LargeFileWarningModal.tsx
+++ src/elements/content-uploader/LargeFileWarningModal.tsx
@@ -1,17 +1,14 @@
 import * as React from 'react';
-import { useVirtualizer } from '@tanstack/react-virtual';
 import { FormattedMessage, useIntl } from 'react-intl';
 import { Modal } from '@box/blueprint-web';
 
 import getFileSize from '../../utils/getFileSize';
 import messages from './messages';
+import OversizeFileList, { type OversizeFile } from './OversizeFileList';
 
 import './LargeFileWarningModal.scss';
 
-export type OversizeFile = {
-    name: string;
-    size: number;
-};
+export type { OversizeFile };
 
 export interface LargeFileWarningModalProps {
     eligibleCount: number;
@@ -32,17 +29,9 @@ const LargeFileWarningModal = ({
     onUpgradeCTAClick,
     oversizeFiles,
 }: LargeFileWarningModalProps) => {
-    const intl = useIntl();
+    const { formatMessage, locale } = useIntl();
     const oversizeCount = oversizeFiles.length;
-    const maxFileSizeLabel = maxFileSize ? getFileSize(maxFileSize, intl.locale) : null;
-    const scrollRef = React.useRef<HTMLDivElement>(null);
-    const rowVirtualizer = useVirtualizer({
-        count: oversizeFiles.length,
-        getScrollElement: () => scrollRef.current,
-        estimateSize: () => 20,
-        overscan: 1,
-        gap: 12,
-    });
+    const maxFileSizeLabel = maxFileSize ? getFileSize(maxFileSize, locale) : null;
 
     const handleOpenChange = (open: boolean) => {
         if (!open) {
@@ -56,54 +45,14 @@ const LargeFileWarningModal = ({
         onCancel();
     };
 
-    const renderFileList = () => {
-        return (
-            <div
-                aria-label={intl.formatMessage(messages.largeFileWarningFileListAriaLabel)}
-                className="bcu-large-file-warning-modal-fileListContainer"
-                ref={scrollRef}
-                role="list"
-            >
-                <div
-                    className="bcu-large-file-warning-modal-fileListInner"
-                    style={{ height: rowVirtualizer.getTotalSize() }}
-                >
-                    {rowVirtualizer.getVirtualItems().map(item => {
-                        const file = oversizeFiles[item.index];
-
-                        return (
-                            <div
-                                key={`${file.name}-${file.size}-${item.index}`}
-                                ref={rowVirtualizer.measureElement}
-                                className="bcu-large-file-warning-modal-fileListRow"
-                                data-index={item.index}
-                                role="listitem"
-                                style={{ transform: `translateY(${item.start}px)` }}
-                            >
-                                <span className="bcu-large-file-warning-modal-fileName" title={file.name}>
-                                    {file.name}
-                                </span>
-                                <span className="bcu-large-file-warning-modal-fileSize">
-                                    {getFileSize(file.size, intl.locale)}
-                                </span>
-                            </div>
-                        );
-                    })}
-                </div>
-            </div>
-        );
-    };
-
     return (
         <Modal open={isOpen} onOpenChange={handleOpenChange}>
             <Modal.Content size="small">
-                <Modal.Header>
-                    {intl.formatMessage(messages.largeFileWarningHeading, { count: oversizeCount })}
-                </Modal.Header>
+                <Modal.Header>{formatMessage(messages.largeFileWarningHeading, { count: oversizeCount })}</Modal.Header>
                 <Modal.ScrollableContainer>
                     <Modal.Body>
-                        <div className="bcu-large-file-warning-modal-body">
-                            <p className="bcu-large-file-warning-modal-description">
+                        <div className="bcu-LargeFileWarningModal-body">
+                            <p className="bcu-LargeFileWarningModal-description">
                                 <FormattedMessage
                                     {...messages.largeFileWarningBody}
                                     values={{
@@ -112,7 +61,7 @@ const LargeFileWarningModal = ({
                                         link: chunks =>
                                             onUpgradeCTAClick ? (
                                                 <button
-                                                    className="bcu-large-file-warning-modal-upgradeLink"
+                                                    className="bcu-LargeFileWarningModal-upgradeLink"
                                                     onClick={handleUpgradeCTAClick}
                                                     type="button"
                                                 >
@@ -124,19 +73,19 @@ const LargeFileWarningModal = ({
                                     }}
                                 />
                             </p>
-                            {renderFileList()}
+                            <OversizeFileList oversizeFiles={oversizeFiles} />
                         </div>
                     </Modal.Body>
                 </Modal.ScrollableContainer>
                 <Modal.Footer>
                     <Modal.Footer.SecondaryButton onClick={onCancel}>
-                        {intl.formatMessage(messages.largeFileWarningCancelButton)}
+                        {formatMessage(messages.largeFileWarningCancelButton)}
                     </Modal.Footer.SecondaryButton>
                     <Modal.Footer.PrimaryButton disabled={eligibleCount <= 0} onClick={onConfirm}>
-                        {intl.formatMessage(messages.largeFileWarningUploadTheRestButton)}
+                        {formatMessage(messages.largeFileWarningUploadTheRestButton)}
                     </Modal.Footer.PrimaryButton>
                 </Modal.Footer>
-                <Modal.Close aria-label={intl.formatMessage(messages.largeFileWarningCloseAriaLabel)} />
+                <Modal.Close aria-label={formatMessage(messages.largeFileWarningCloseAriaLabel)} />
             </Modal.Content>
         </Modal>
     );
