38e7e9b47585aea2a26a00edd13477a6c590cc92
roman <bonchevskyi@gmail.com>
2026-08-10 15:03:22 +0200

    refactor(thumbnail-card): migrate ThumbnailCard from Flow to TypeScript

--- src/components/thumbnail-card/__tests__/ThumbnailCardDetails.test.tsx
+++ src/components/thumbnail-card/__tests__/ThumbnailCardDetails.test.tsx
@@ -1,4 +1,3 @@
-// @flow
 import * as React from 'react';
 import userEvent from '@testing-library/user-event';
 
--- src/components/thumbnail-card/__tests__/ThumbnailCardThumbnail.test.tsx
+++ src/components/thumbnail-card/__tests__/ThumbnailCardThumbnail.test.tsx
@@ -1,5 +1,5 @@
-// @flow
 import * as React from 'react';
+import { shallow } from 'enzyme';
 
 import ThumbnailCardThumbnail from '../ThumbnailCardThumbnail';
 
