cfdcc8ac9a04cf7fa66b2939e95080730cd00fc8
roman <bonchevskyi@gmail.com>
2026-08-18 14:42:00 +0200

    refactor(select-field): migrate SelectField from Flow to TypeScript

--- src/components/select-field/__tests__/MultiSelectField.test.tsx
+++ src/components/select-field/__tests__/MultiSelectField.test.tsx
@@ -1,7 +1,13 @@
 import * as React from 'react';
+import { shallow } from 'enzyme';
+import type { IntlShape } from 'react-intl';
 
 import { MultiSelectFieldBase } from '../MultiSelectField';
 import CLEAR from '../constants';
+
+const intl = {
+    formatMessage: jest.fn().mockImplementation(() => 'Clear All'),
+} as unknown as IntlShape;
 
 describe('components/select-field/MultiSelectField', () => {
     const options = [
@@ -12,20 +18,17 @@ describe('components/select-field/MultiSelectField', () => {
 
     describe('MultiSelectField', () => {
         test('should render a BaseSelectField with a selectedValues prop matching passed in selected value when called', () => {
-            const wrapper = shallow(<MultiSelectFieldBase onChange={() => {}} options={options} />);
+            const wrapper = shallow(<MultiSelectFieldBase intl={intl} onChange={jest.fn()} options={options} />);
 
             const baseSelectFieldWrapper = wrapper.find('BaseSelectField');
-            expect(baseSelectFieldWrapper.length).toBe(1);
+            expect(baseSelectFieldWrapper).toHaveLength(1);
             expect(baseSelectFieldWrapper.prop('options')).toEqual(options);
             expect(baseSelectFieldWrapper.prop('multiple')).toBe(true);
         });
 
         test('should render a BaseSelectField with an options prop containing a clear option if shouldShowClearOption is true', () => {
-            const intl = {
-                formatMessage: jest.fn().mockImplementationOnce(() => 'Clear All'),
-            };
             const wrapper = shallow(
-                <MultiSelectFieldBase intl={intl} onChange={() => {}} options={options} shouldShowClearOption />,
+                <MultiSelectFieldBase intl={intl} onChange={jest.fn()} options={options} shouldShowClearOption />,
             );
             const expectedOptions = [
                 {
@@ -36,7 +39,7 @@ describe('components/select-field/MultiSelectField', () => {
             ];
 
             const baseSelectFieldWrapper = wrapper.find('BaseSelectField');
-            expect(baseSelectFieldWrapper.length).toBe(1);
+            expect(baseSelectFieldWrapper).toHaveLength(1);
             expect(baseSelectFieldWrapper.prop('options')).toEqual(expectedOptions);
         });
     });
---
+++ src/components/select-field/index.ts
@@ -0,0 +1,7 @@
+export { default as SingleSelectField } from './SingleSelectField';
+export type { SingleSelectFieldProps } from './SingleSelectField';
+export { default as MultiSelectField } from './MultiSelectField';
+export type { MultiSelectFieldProps } from './MultiSelectField';
+export { default as SelectField } from './SelectField';
+export type { SelectFieldProps } from './SelectField';
+export type { SelectOptionProp, SelectOptionValueProp } from './props';
