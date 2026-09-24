5fb50a2317f1e6e54e646269ec395af00eb4a4df
Jose Gaston <jmcbgaston@gmail.com>
2026-09-24 15:29:49 -0700

    refactor(activity-sidebar): read rich-text flag through one getter
    The activityFeed.richText.enabled lookup was repeated at five call
    sites. A single getIsRichTextEnabled getter keeps the flag name in one
    place, so the feed GETs and the V2 render path cannot drift apart.

--- src/elements/content-sidebar/ActivitySidebar.js
+++ src/elements/content-sidebar/ActivitySidebar.js
@@ -183,6 +183,11 @@ class ActivitySidebar extends React.PureComponent<Props, State> {
         onVersionHistoryClick: noop,
     };
 
+    getIsRichTextEnabled = (): boolean => {
+        const { features } = this.props;
+        return isFeatureEnabled(features, 'activityFeed.richText.enabled');
+    };
+
     constructor(props: Props) {
         super(props);
         // eslint-disable-next-line react/prop-types
@@ -800,7 +805,7 @@ class ActivitySidebar extends React.PureComponent<Props, State> {
         const shouldShowAppActivity = isFeatureEnabled(features, 'activityFeed.appActivity.enabled');
         const shouldShowAnnotations = isFeatureEnabled(features, 'activityFeed.annotations.enabled');
         const shouldUseUAA = isFeatureEnabled(features, 'activityFeed.uaaIntegration.enabled');
-        const shouldEnableRichText = isFeatureEnabled(features, 'activityFeed.richText.enabled');
+        const shouldEnableRichText = this.getIsRichTextEnabled();
 
         api.getFeedAPI(shouldDestroy).feedItems(
             file,
@@ -906,8 +911,8 @@ class ActivitySidebar extends React.PureComponent<Props, State> {
     });
 
     getFeedItemsWithReplies = (feedItems: FeedItems, id?: string, type?: CommentFeedItemType): Promise<FeedItems> => {
-        const { api, file, features } = this.props;
-        const shouldEnableRichText = isFeatureEnabled(features, 'activityFeed.richText.enabled');
+        const { api, file } = this.props;
+        const shouldEnableRichText = this.getIsRichTextEnabled();
 
         return new Promise((resolve, reject) => {
             if (!id || !type) {
@@ -1182,8 +1187,8 @@ class ActivitySidebar extends React.PureComponent<Props, State> {
      * @return {Promise<{ id: string, type?: FocusableFeedItemType }>}
      */
     getActiveFeedEntryData = (feedItems: FeedItems): Promise<{ id?: string, type?: FeedItemType }> => {
-        const { activeFeedEntryId, activeFeedEntryType, api, file, features } = this.props;
-        const shouldEnableRichText = isFeatureEnabled(features, 'activityFeed.richText.enabled');
+        const { activeFeedEntryId, activeFeedEntryType, api, file } = this.props;
+        const shouldEnableRichText = this.getIsRichTextEnabled();
         return new Promise((resolve, reject) => {
             if (!activeFeedEntryId || !activeFeedEntryType || !this.isItemTypeFocusable(activeFeedEntryType)) {
                 resolve({});
@@ -1247,8 +1252,8 @@ class ActivitySidebar extends React.PureComponent<Props, State> {
      * @return {void}
      */
     getReplies = (id: string, type: CommentFeedItemType): void => {
-        const { api, file, features } = this.props;
-        const shouldEnableRichText = isFeatureEnabled(features, 'activityFeed.richText.enabled');
+        const { api, file } = this.props;
+        const shouldEnableRichText = this.getIsRichTextEnabled();
 
         api.getFeedAPI(false).fetchReplies(
             file,
@@ -1484,7 +1489,6 @@ class ActivitySidebar extends React.PureComponent<Props, State> {
             const timestampedCommentsConfig = getFeatureConfig(features, 'activityFeed.timestampedComments');
             const isTimestampedCommentsEnabled = timestampedCommentsConfig?.enabled === true;
             const isAudioPlayerV2Enabled = isFeatureEnabled(features, 'audioPlayerV2.enabled');
-            const isRichTextEnabled = isFeatureEnabled(features, 'activityFeed.richText.enabled');
             return (
                 <div
                     aria-labelledby={label}
@@ -1508,7 +1512,7 @@ class ActivitySidebar extends React.PureComponent<Props, State> {
                         hasTasks={this.props.hasTasks}
                         isAudioPlayerV2Enabled={isAudioPlayerV2Enabled}
                         isDisabled={isDisabled}
-                        isRichTextEnabled={isRichTextEnabled}
+                        isRichTextEnabled={this.getIsRichTextEnabled()}
                         isTimestampedCommentsEnabled={isTimestampedCommentsEnabled}
                         onAnnotationCopyLink={onAnnotationCopyLink}
                         onAnnotationDelete={this.handleAnnotationDelete}
